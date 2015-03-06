919ea9b05854a9923d7510ab9583fb67fd0eeada
Arthur.WANG <fnghwsj@qq.com>
2015-03-06 13:57:26 +0800

    update:绑定支付的model定义都放到一个文件里面

--- model/bindingpays.go
+++ model/bindingpays.go
@@ -1,20 +1,41 @@
 package model
 
-// Binding 绑定关系
-type Binding struct {
-	cardNum   string
-	bindingId string
-	// ...
+// 解除绑定关系请求
+type BindingRemoveIn struct {
+	BindingId string `json:"bindingId"` //银行卡绑定ID
 }
 
-// BindingCreateIn 绑定支付请求
-type BindingCreateIn struct {
-	merId string // 商户 Id
-	// ...
+// 解除绑定关系响应
+type BindingRemoveOut struct {
+	RespCode string `json:"respCode"` //响应代码
+	RespMsg  string `json:"respMsg"`  //响应信息
 }
 
-// BindingCreateOut 绑定支付响应
-type BindingCreateOut struct {
-	merId string // 商户 Id
-	// ...
+// 绑定关系查询请求
+type BindingEnquiryIn struct {
+	BindingId string `json:"bindingId"` //银行卡绑定ID
+}
+
+// 绑定关系查询响应
+type BindingEnquiryOut struct {
+	RespCode string `json:"respCode"` //响应代码
+	RespMsg  string `json:"respMsg"`  //响应信息
+}
+
+// 绑定支付请求
+type BindingPaymentIn struct {
+	SubMerId    string `json:"subMerId"`    //子商户号
+	MerOrderNum string `json:"merOrderNum"` //商户订单号
+	TransAmt    int    `json:"transAmt"`    //支付金额
+	BindingId   string `json:"bindingId"`   //银行卡绑定ID
+	SendSmsId   string `json:"sendSmsId"`   //申请短信验证码的交易流水
+	SmsCode     string `json:"smsCode"`     //短信验证码
+}
+
+// 绑定支付响应
+type BindingPaymentOut struct {
+	MerOrderNum string `json:"merOrderNum"` //商户订单号
+	OrderNum    string `json:"orderNum"`    //网关订单号
+	RespCode    string `json:"respCode"`    //响应代码
+	RespMsg     string `json:"respMsg"`     //响应信息
 }
