70c7bcab5f6d3230b3c3bffc645d155c77be2c1a
Arthur.WANG <fnghwsj@qq.com>
2015-03-11 11:59:19 +0800

    update:判断是否是银联卡的方法更新，传入卡品牌

--- core/router_test.go
+++ core/router_test.go
@@ -42,12 +42,12 @@ func doCardBrandTest(cardNum string, t *testing.T) (cardBrand string) {
 func TestIsUnionPayCard(t *testing.T) {
 	// 银联卡
 	cardNum := "6222020302062061908"
-	if result := IsUnionPayCard(cardNum); !result {
+	if result := IsUnionPayCard(cardNum, "CUP"); !result {
 		t.Errorf("Expected 'true',but get '%t'", result)
 	}
 	// MCC卡
 	cardNum = "5500521234567891"
-	if result := IsUnionPayCard(cardNum); result {
+	if result := IsUnionPayCard(cardNum, ""); result {
 		t.Errorf("Expected 'false',but get '%t'", result)
 	}
 }
