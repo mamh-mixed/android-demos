1d5cb096bcfb24931028bc3ef1b4fd8dc3a6596e
Arthur.WANG <fnghwsj@qq.com>
2015-03-11 16:51:09 +0800

    update:报错提示信息优化

--- core/bizValidity.go
+++ core/bizValidity.go
@@ -8,15 +8,15 @@ import (
 // 银联卡必须验证证件和手机号
 func UnionPayCardValidity(bc *model.BindingCreate) (ret *model.BindingReturn) {
 	if bc.IdentType == "" {
-		return model.NewBindingReturn("200050", "字段 identType 不能为空")
+		return model.NewBindingReturn("200050", "银联卡 identType 不能为空")
 	}
 
 	if bc.IdentNum == "" {
-		return model.NewBindingReturn("200050", "字段 identNum 不能为空")
+		return model.NewBindingReturn("200050", "银联卡 identNum 不能为空")
 	}
 
 	if bc.PhoneNum == "" {
-		return model.NewBindingReturn("200050", "字段 phoneNum 不能为空")
+		return model.NewBindingReturn("200050", "银联卡 phoneNum 不能为空")
 	}
 
 	if matched, _ := regexp.MatchString(`^[0-9]$|^X$`, bc.IdentType); !matched {
