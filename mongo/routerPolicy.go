5f6b9a0356c170a792ee9576350fc422d44ef978
Arthur.WANG <fnghwsj@qq.com>
2015-03-13 10:42:57 +0800

    update:路由策略的结构体字段改动，查找路由的方法优化

--- mongo/routerPolicy.go
+++ mongo/routerPolicy.go
@@ -8,22 +8,6 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-// FindRouter 根据源商户号和卡品牌在数据库中查找路由策略 [moved]
-func FindRouter(origMerId, cardBrand string) (rp *model.RouterPolicy, err error) {
-	rp = &model.RouterPolicy{}
-	q := bson.M{"origMerId": origMerId, "cardBrand": cardBrand}
-	g.Debug("'FindRouter' condition: %+v", q)
-
-	err = db.routerPolicy.Find(q).One(rp)
-	if err != nil {
-		g.Debug("Error message is ", err.Error())
-		return nil, err
-	}
-	g.Debug("Router Policy is %+v", rp)
-
-	return rp, nil
-}
-
 // InsertRouterPolicy 插入一个路由策略到数据库中
 func InsertRouterPolicy(rp *model.RouterPolicy) error {
 	if err := db.routerPolicy.Insert(rp); err != nil {
@@ -33,11 +17,15 @@ func InsertRouterPolicy(rp *model.RouterPolicy) error {
 }
 
 // FindRouterPolicy 根据源商户Id 和 卡品牌查找路由
-func FindRouterPolicy(origMerId, cardBrand string) (r *model.RouterPolicy) {
+func FindRouterPolicy(merId, cardBrand string) (r *model.RouterPolicy) {
 	r = &model.RouterPolicy{}
-	q := bson.M{"origMerId": origMerId, "cardBrand": cardBrand}
-	db.routerPolicy.Find(q).One(r)
-
+	q := bson.M{"merId": merId, "cardBrand": cardBrand}
+	err := db.routerPolicy.Find(q).One(r)
+	if err != nil {
+		g.Debug("'FindRouter' condition: %+v\n", q)
+		g.Debug("Error message is: \n", err.Error())
+		return nil
+	}
 	g.Debug("'FindRouter' condition: %+v, result %#v", q, r)
 	return r
 }
--- mongo/routerPolicy_test.go
+++ mongo/routerPolicy_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestInsertRouterPolicy(t *testing.T) {
 	rp := &model.RouterPolicy{
-		OrigMerId: "001405",
+		MerId:     "001405",
 		CardBrand: "VIS",
 		ChanCode:  "CFCA",
 		ChanMerId: "10000000007",
@@ -16,4 +16,12 @@ func TestInsertRouterPolicy(t *testing.T) {
 	if err := InsertRouterPolicy(rp); err != nil {
 		t.Errorf("Excepted no erro,but get ,error message is %s", err.Error())
 	}
+}
+
+func TestFindRouterPolicy(t *testing.T) {
+	rp := FindRouterPolicy("001405", "VIS")
+	if rp == nil {
+		t.Error("Excepted one but get 'nil'")
+	}
+	t.Logf("RouterPolicy is: %+v", rp)
 }
