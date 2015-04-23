67bc3b1393589ca9c5965835f8f4900782db808d
O' Migo <shangxuejin@gmail.com>
2015-04-23 17:58:52 +0800

    可以通过环境变量做配置

--- conf/reinit.go
+++ conf/reinit.go
@@ -2,19 +2,22 @@
 package conf
 
 import (
+	"time"
+
 	"github.com/CardInfoLink/quickpay/core"
 	"github.com/CardInfoLink/quickpay/mongo"
 	"github.com/omigo/log"
-	"time"
 )
 
 var defaultVn string
 
+// CheckConf 定时检查数据库配置，如果 卡Bin 变化，重建搜索树
 func CheckConf() {
 	// cardBin
 	version, err := mongo.VersionColl.FindOne("cardBin")
 	if err != nil {
-		log.Panicf("fail to load cardBin version : %s ", err)
+		log.Errorf("fail to load cardBin version : %s ", err)
+		return
 	}
 	defaultVn = version.Vn
 	go doCheckConf()
@@ -25,19 +28,18 @@ func doCheckConf() {
 	tick := time.Tick(30 * time.Second)
 
 	for {
-		select {
-		case <-tick:
-			o, _ := mongo.VersionColl.FindOne("cardBin")
-			if o.Vn != defaultVn {
-				log.Infof("cardBin had been updated (%s -> %s), begin to rebuild the cardBin tree ", defaultVn, o.Vn)
-				// ... rebuild the tree
-				err := core.ReBuildTree()
-				if err != nil {
-					log.Error(err)
-					continue
-				}
-				defaultVn = o.Vn
+		<-tick
+
+		o, _ := mongo.VersionColl.FindOne("cardBin")
+		if o.Vn != defaultVn {
+			log.Infof("cardBin had been updated (%s -> %s), begin to rebuild the cardBin tree ", defaultVn, o.Vn)
+			// ... rebuild the tree
+			err := core.ReBuildTree()
+			if err != nil {
+				log.Error(err)
+				continue
 			}
+			defaultVn = o.Vn
 		}
 	}
 }
