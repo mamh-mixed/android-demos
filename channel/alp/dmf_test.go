262ddfc40804977d72ed7dad3ac4eb667bc37de2
Rui <379630413@qq.com>
2015-04-27 11:58:44 +0800

    修复测试bug

--- channel/alp/dmf_test.go
+++ channel/alp/dmf_test.go
@@ -6,15 +6,15 @@ import (
 	"testing"
 )
 
-var qrCodePay = &model.QrCodePay{
-	GoodsInfo:    "鞋子,1000,2;衣服,1500,3",
-	ChanOrderNum: "awdajwdadn",
-	Key:          "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
-	ScanCodeId:   "23131242413",
-	Txamt:        "0.01",
+var scanPay = &model.ScanPay{
+	GoodsInfo:       "鞋子,1000,2;衣服,1500,3",
+	ChannelOrderNum: "awdajwdadn",
+	Key:             "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
+	ScanCodeId:      "23131242413",
+	Txamt:           "0.01",
 }
 
 func TestProcessBarcodePay(t *testing.T) {
-	resp := DefaultClient.ProcessBarcodePay(qrCodePay)
+	resp := DefaultClient.ProcessBarcodePay(scanPay)
 	log.Debugf("%+v", resp)
 }
