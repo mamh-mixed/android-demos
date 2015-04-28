3e0f45e8d20995e29b1d7ac3305c0acb4e7f4fc0
Arthur.WANG <fnghwsj@qq.com>
2015-04-28 09:39:07 +0800

    无卡直接支付的业务逻辑处理

--- core/bindingpay.go
+++ core/bindingpay.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 
 	"github.com/CardInfoLink/quickpay/channel"
+	"github.com/CardInfoLink/quickpay/channel/cil"
 	"github.com/CardInfoLink/quickpay/model"
 	"github.com/CardInfoLink/quickpay/mongo"
 	"github.com/CardInfoLink/quickpay/tools"
@@ -596,6 +597,124 @@ func ProcessBillingSummary(be *model.BillingSummary) (ret *model.BindingReturn) 
 	ret.SettDate = be.SettDate
 	ret.Data = data
 	return
+}
+
+// ProcessNoTrackPayment 处理无卡直接支付的业务逻辑
+func ProcessNoTrackPayment(be *model.NoTrackPayment) (ret *model.BindingReturn) {
+	// 默认返回
+	ret = mongo.RespCodeColl.Get("000001")
+
+	// 系统唯一的序列号
+	chanOrderNum := mongo.SnColl.GetSysSN()
+
+	// 检查同一个商户的订单号是否重复
+	count, err := mongo.TransColl.Count(be.MerId, be.MerOrderNum)
+	if err != nil {
+		log.Errorf("find trans fail : (%s)", err)
+		return
+	}
+	if count > 0 {
+		return mongo.RespCodeColl.Get("200081")
+	}
+
+	//只要订单号不重复就记录这笔交易
+	errorTrans := &model.Trans{
+		OrderNum:     be.MerOrderNum,
+		ChanOrderNum: chanOrderNum,
+		AcctNum:      be.AcctNum,
+		MerId:        be.MerId,
+		TransAmt:     be.TransAmt,
+		TransCurr:    be.CurrCode,
+		SendSmsId:    be.SendSmsId,
+		SmsCode:      be.SmsCode,
+		SubMerId:     be.SubMerId,
+	}
+
+	// 暂时不支持预授权交易
+	if be.TransType == "AUTH" {
+		errorTrans.RespCode = "100030"
+		saveErrorTran(errorTrans)
+		return mongo.RespCodeColl.Get("100030")
+	}
+
+	// 获取卡bin详情
+	cardBin, err := findCardBin(be.AcctNum)
+	if err != nil {
+		if err.Error() == "not found" {
+			errorTrans.RespCode = "200070"
+			saveErrorTran(errorTrans)
+			return mongo.RespCodeColl.Get("200110")
+		}
+		saveErrorTran(errorTrans)
+		return
+	}
+	log.Debugf("CardBin: %+v", cardBin)
+
+	// 通过路由策略找到渠道和渠道商户
+	rp := mongo.RouterPolicyColl.Find(be.MerId, cardBin.CardBrand)
+	if rp == nil {
+		errorTrans.RespCode = "300030"
+		saveErrorTran(errorTrans)
+		return mongo.RespCodeColl.Get("300030")
+	}
+
+	// 根据绑定关系得到渠道商户信息
+	chanMer, err := mongo.ChanMerColl.Find(rp.ChanCode, rp.ChanMerId)
+	if err != nil {
+		errorTrans.RespCode = "300030"
+		saveErrorTran(errorTrans)
+		log.Errorf("not found any chanMer (%s)", err)
+		return mongo.RespCodeColl.Get("300030")
+	}
+
+	// 下游送来的终端号，如果没有的话，填上渠道商户里面的配置的终端号
+	if be.TerminalId == "" {
+		be.TerminalId = chanMer.TerminalId
+	}
+	// 补充信息
+	be.Chcd = chanMer.InsCode
+	be.Mchntid = chanMer.ChanMerId
+	be.CliSN = mongo.SnColl.GetDaySN(chanMer.ChanMerId, chanMer.TerminalId)
+	be.SysSN = chanOrderNum
+
+	// 记录这笔交易，入库
+	trans := &model.Trans{
+		OrderNum:     be.MerOrderNum,
+		ChanOrderNum: chanOrderNum,
+		AcctNum:      be.AcctNum,
+		MerId:        be.MerId,
+		TransAmt:     be.TransAmt,
+		TransCurr:    be.CurrCode,
+		SendSmsId:    be.SendSmsId,
+		SmsCode:      be.SmsCode,
+		SubMerId:     be.SubMerId,
+		ChanMerId:    chanMer.ChanMerId,
+		ChanCode:     chanMer.ChanCode,
+		TransType:    1, //TODO 预授权不属于支付
+		Remark:       "Apple Pay",
+	}
+	if err := mongo.TransColl.Add(trans); err != nil {
+		log.Errorf("add trans fail: (%s)", err)
+		return
+	}
+
+	ret = cil.Consume(be)
+
+	trans.ChanRespCode = ret.ChanRespCode
+	trans.RespCode = ret.RespCode
+	switch ret.RespCode {
+	case "000000":
+		trans.TransStatus = model.TransSuccess
+	case "000009":
+		trans.TransStatus = model.TransHandling
+	default:
+		trans.TransStatus = model.TransFail
+	}
+	if err = mongo.TransColl.Update(trans); err != nil {
+		log.Errorf("update trans status fail: %s", err)
+	}
+
+	return ret
 }
 
 // todo 校验短信验证码，短信验证通过就返回nil
--- core/bindingpay_test.go
+++ core/bindingpay_test.go
@@ -3,7 +3,9 @@ package core
 import (
 	"github.com/CardInfoLink/quickpay/model"
 	"github.com/omigo/log"
+	"strconv"
 	"testing"
+	"time"
 )
 
 func TestProcessBindingCreate(t *testing.T) {
@@ -140,3 +142,55 @@ func TestProcessBillingSummary(t *testing.T) {
 	}
 	log.Debugf("%+v", ret)
 }
+
+func TestProcessNoTrackPayment(t *testing.T) {
+
+	ntp := &model.NoTrackPayment{
+		MerId:       testMerID,
+		TransType:   "SALE",
+		SubMerId:    "SM123456",
+		MerOrderNum: strconv.FormatInt(time.Now().UnixNano(), 10),
+		TransAmt:    120,
+		CurrCode:    "156",
+		AcctName:    "Peter",
+		AcctNum:     testCUPCard,
+		IdentType:   "0",
+		IdentNum:    testCUPIdentNum,
+		PhoneNum:    testCUPPhone,
+		AcctType:    "10",
+		ValidDate:   testCUPValidDate,
+		Cvv2:        testCUPCVV2,
+	}
+
+	ret := ProcessNoTrackPayment(ntp)
+
+	if ret == nil {
+		t.Error("NoTrackPayment process error")
+	}
+
+	t.Logf("%+v", ret)
+}
+
+// only for test
+var (
+	// 万事达卡测试数据
+	testMSCCard       = "5457210001000019"
+	testMSCCVV2       = "300"
+	testMSCValidDate  = "1412"
+	testMSCTrackdata2 = "5457210001000019=1412101080080748"
+
+	// VISA卡测试数据
+	testVISCard       = "4761340000000019"
+	testVISCVV2       = "830"
+	testVISValidDate  = "1712"
+	testVISTrackdata2 = "4761340000000019=171210114991787"
+
+	// 银联卡测试数据
+	testCUPCard      = "6225220100740059"
+	testCUPCVV2      = "111"
+	testCUPValidDate = "1605"
+	testCUPPhone     = "13611111111"
+	testCUPIdentNum  = "130412"
+
+	testMerID = "APPTEST"
+)
