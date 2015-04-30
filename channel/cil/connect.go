9b07b43de54725dea61340c0a7672a849d7c8155
O' Migo <shangxuejin@gmail.com>
2015-04-30 17:20:29 +0800

    重新组织和线下联机的接口代码

--- channel/cil/connect.go
+++ channel/cil/connect.go
@@ -19,51 +19,7 @@ var (
 	port = "$CIL_PORT | 7823"
 )
 
-// send 方法会同步返回线下处理结果，它最大的好处是把一个异步 TCP 请求响应变成同步的，无需回调。
-// 这对调用者来说是透明的，调用者无需关心与上游网关的通信方式和通信过程，按照正常的顺序流程编写代码，
-// 注意：如果上游请求延迟较大，这个方法会阻塞。
-func send(msg *model.CilMsg, timeout time.Duration) (back *model.CilMsg) {
-	// 串行写入，以免写入错乱
-	sendQueue <- msg
-
-	// 交易唯一流水号
-	sn := fmt.Sprintf("%s_%s_%s_%s", msg.Chcd, msg.Mchntid, msg.Terminalid, msg.Clisn)
-	log.Tracef("send: %s", sn)
-
-	// 结果会异步写入到这个管道中
-	c := make(chan *model.CilMsg)
-
-	mapMutex.Lock()
-	recvMap[sn] = c
-	mapMutex.Unlock()
-
-	// 等待结果返回
-	select {
-	case back = <-c:
-		log.Debug("received request normally")
-	case <-time.After(timeout):
-		// 超时处理
-		log.Warn("request timeout")
-		back = &model.CilMsg{
-			Respcd: reversalFlag,
-		}
-	}
-	log.Debugf("rcvd: %s", sn)
-
-	// 清除这个 key 和 管道
-	mapMutex.Lock()
-	delete(recvMap, sn)
-	mapMutex.Unlock()
-	log.Debugf("delete: %s", sn)
-
-	return back
-}
-
-var sendQueue = make(chan *model.CilMsg, 100)
-var recvMap = make(map[string]chan *model.CilMsg, 400)
-var mapMutex sync.RWMutex
-
-var conn net.Conn
+var defualtClient *CilOnlinePay
 
 // Connect 连接到线下
 func Connect() {
@@ -71,33 +27,60 @@ func Connect() {
 	fevPort := tools.FirstExistValue(port)
 	addr := fevHost + ":" + fevPort
 
+	defualtClient = NewCilOnlinePay(addr, 10000, 100)
+
+	defualtClient.Connect()
+}
+
+// send 方法会同步返回线下处理结果，它最大的好处是把一个异步 TCP 请求响应变成同步的，无需回调。
+// 这对调用者来说是透明的，调用者无需关心与上游网关的通信方式和通信过程，按照正常的顺序流程编写代码，
+// 注意：如果上游请求延迟较大，这个方法会阻塞。
+func send(msg *model.CilMsg, timeout time.Duration) (back *model.CilMsg) {
+	return defualtClient.Send(msg, timeout)
+}
+
+// CilOnlinePay 线下联机系统
+type CilOnlinePay struct {
+	Addr string
+	conn net.Conn
+	open bool
+
+	sendQueue chan *model.CilMsg
+
+	recvMap  map[string]chan *model.CilMsg
+	mapMutex sync.RWMutex
+}
+
+// NewCilOnlinePay 创建一个线下联机系统客户端
+func NewCilOnlinePay(addr string, queueSize, initRecvMapSize int) (c *CilOnlinePay) {
+	c = &CilOnlinePay{Addr: addr}
+	c.sendQueue = make(chan *model.CilMsg, queueSize)
+	c.recvMap = make(map[string]chan *model.CilMsg, initRecvMapSize)
+
+	return c
+}
+
+// Connect 建立连接
+func (c *CilOnlinePay) Connect() {
 	var err error
-	conn, err = net.Dial("tcp", addr)
+	c.conn, err = net.Dial("tcp", c.Addr)
 	if err != nil {
-		log.Errorf("can't connect to cil-online tcp://%s: %s", addr, err)
+		log.Errorf("can't connect to cil-online tcp://%s: %s", c.Addr, err)
 		Connect()
 		return
 	}
 	// defer conn.Close()
-	log.Infof("connect to cil channel %s", addr)
+	log.Infof("connect to cil channel %s", c.Addr)
+	c.open = true
 
-	// 循环接收
-	go recv0()
-
-	// 循环发送
-	go send0()
+	go c.WaitAndReceive()
+	go c.LoopToSend()
 }
 
-func restart() {
-	log.Warn("connection error, connecting...")
-	conn.Close()
-	Connect()
-}
-
-func recv0() {
-	defer restart()
+// WaitAndReceive 等待接收消息
+func (c *CilOnlinePay) WaitAndReceive() {
 	for {
-		msg, err := read()
+		msg, err := c.ReceiveOne()
 		if err != nil {
 			log.Error(err)
 			return
@@ -112,45 +95,97 @@ func recv0() {
 		log.Debugf("read: %s", sn)
 
 		// 根据交易流水号取到存放结果的管道
-		mapMutex.RLock()
-		c := recvMap[sn]
-		mapMutex.RUnlock()
+		c.mapMutex.RLock()
+		retChan := c.recvMap[sn]
+		c.mapMutex.RUnlock()
 
-		c <- msg
+		retChan <- msg
 
 		//  如果读取错误或链接断开 EOF 直接返回
 		if err != nil {
+			c.open = false
 			return
 		}
 	}
 }
 
-func send0() {
-	defer restart()
+// LoopToSend 循环从队列中取报文，发送
+func (c *CilOnlinePay) LoopToSend() {
 	for {
 		select {
-		case msg := <-sendQueue:
-			err := write(msg)
+		case msg := <-c.sendQueue:
+			err := c.SendOne(msg)
 			//  如果写入错误或链接断开 EOF 直接返回
 			if err != nil {
+				c.open = false
 				return
 			}
 			sn := fmt.Sprintf("%s_%s_%s_%s", msg.Chcd, msg.Mchntid, msg.Terminalid, msg.Clisn)
 			log.Debugf("write: %s", sn)
 		case <-time.After(60 * time.Second):
 			log.Info("send keepalive")
-			err := keepalive()
+			err := c.Keepalive()
 			if err != nil {
+				c.open = false
 				return
 			}
 		}
 	}
 }
 
-func read() (back *model.CilMsg, err error) {
+// Close 关闭连接
+func (c *CilOnlinePay) Close() {
+	c.conn.Close()
+}
+
+// Keepalive 避免长时间连接空闲自动断开，每隔一段时间需调用一次这个方法
+func (c *CilOnlinePay) Keepalive() (err error) {
+	if !c.open {
+		return
+	}
+
+	_, err = io.WriteString(c.conn, "0000")
+	if err != nil {
+		log.Error("write len error", err)
+	}
+	return err
+}
+
+// SendOne 发生报文
+func (c *CilOnlinePay) SendOne(msg *model.CilMsg) (err error) {
+	// log.Debugf("%#v", msg)
+
+	jsonBytes, err := json.Marshal(msg)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	mLen := len(jsonBytes)
+	mLenStr := fmt.Sprintf("%04d", mLen)
+
+	_, err = io.WriteString(c.conn, mLenStr)
+	if err != nil {
+		log.Error("write len error", err)
+		return err
+	}
+
+	_, err = c.conn.Write(jsonBytes)
+	if err != nil {
+		log.Error("write len error", err)
+		return err
+	}
+
+	log.Debugf("write message: %s %s", mLenStr, jsonBytes)
+
+	return nil
+}
+
+// ReceiveOne 接收异步返回的报文
+func (c *CilOnlinePay) ReceiveOne() (back *model.CilMsg, err error) {
 	mLenByte := make([]byte, 4)
 
-	_, err = conn.Read(mLenByte)
+	_, err = c.conn.Read(mLenByte)
 	if err != nil {
 		log.Debug("read length error: ", err)
 		return nil, err
@@ -174,8 +209,8 @@ func read() (back *model.CilMsg, err error) {
 	msg := make([]byte, mlen)
 	var size int
 	for size < mlen {
-		log.Debug(size)
-		rlen, err := conn.Read(msg[size:])
+		// log.Debug(size)
+		rlen, err := c.conn.Read(msg[size:])
 		if err != nil {
 			if err == io.EOF {
 				// read end
@@ -198,39 +233,42 @@ func read() (back *model.CilMsg, err error) {
 	return back, err
 }
 
-func write(msg *model.CilMsg) (err error) {
-	// log.Debugf("%#v", msg)
+// Send 方法会同步返回线下处理结果，它最大的好处是把一个异步 TCP 请求响应变成同步的，无需回调。
+// 这对调用者来说是透明的，调用者无需关心与上游网关的通信方式和通信过程，按照正常的顺序流程编写代码，
+// 注意：如果上游请求延迟较大，这个方法会阻塞。
+func (c *CilOnlinePay) Send(msg *model.CilMsg, timeout time.Duration) (back *model.CilMsg) {
+	// 串行写入，以免写入错乱
+	c.sendQueue <- msg
 
-	jsonBytes, err := json.Marshal(msg)
-	if err != nil {
-		log.Error(err)
-		return err
+	// 交易唯一流水号
+	sn := fmt.Sprintf("%s_%s_%s_%s", msg.Chcd, msg.Mchntid, msg.Terminalid, msg.Clisn)
+	log.Tracef("send: %s", sn)
+
+	// 结果会异步写入到这个管道中
+	retChan := make(chan *model.CilMsg)
+
+	c.mapMutex.Lock()
+	c.recvMap[sn] = retChan
+	c.mapMutex.Unlock()
+
+	// 等待结果返回
+	select {
+	case back = <-retChan:
+		log.Debug("received request normally")
+	case <-time.After(timeout):
+		// 超时处理
+		log.Warn("request timeout")
+		back = &model.CilMsg{
+			Respcd: reversalFlag,
+		}
 	}
+	log.Debugf("rcvd: %s", sn)
 
-	mLen := len(jsonBytes)
-	mLenStr := fmt.Sprintf("%04d", mLen)
+	// 清除这个 key 和 管道
+	c.mapMutex.Lock()
+	delete(c.recvMap, sn)
+	c.mapMutex.Unlock()
+	log.Debugf("delete: %s", sn)
 
-	_, err = io.WriteString(conn, mLenStr)
-	if err != nil {
-		log.Error("write len error", err)
-		return err
-	}
-
-	_, err = conn.Write(jsonBytes)
-	if err != nil {
-		log.Error("write len error", err)
-		return err
-	}
-
-	log.Debugf("write message: %s %s", mLenStr, jsonBytes)
-
-	return nil
-}
-
-func keepalive() (err error) {
-	_, err = io.WriteString(conn, "0000")
-	if err != nil {
-		log.Error("write len error", err)
-	}
-	return err
+	return back
 }
