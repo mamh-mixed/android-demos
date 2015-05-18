c11176155726f5d70a46a18b55fb9cd472ae99aa
Arthur.WANG <fnghwsj@qq.com>
2015-05-18 15:34:42 +0800

    切换商户的界面优化以及增加remark的值

--- model/core.go
+++ model/core.go
@@ -80,6 +80,7 @@ type Merchant struct {
 	TransCurr  string `bson:"transCurr,omitempty" json:"transCurr,omitempty"`   //商户交易币种
 	SignKey    string `bson:"signKey,omitempty" json:"signKey,omitempty"`       //商户签名密钥
 	EncryptKey string `bson:"encryptKey,omitempty" json:"encryptKey,omitempty"` //商户加密密钥
+	Remark     string `bson:"remark,omitempty" json:"remark,omitempty"`         //备注信息
 }
 
 // MerDetail 商户详细信息
