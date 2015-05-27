cd18624987e48509a1dd6d0c85b72318ee48c77a
Rui <379630413@qq.com>
2015-05-27 14:29:02 +0800

    导入文件增加注释

--- data/importData_test.go
+++ data/importData_test.go
@@ -1,26 +1,30 @@
 package data
 
 import (
-	// "fmt"
 	"testing"
 )
 
+// 导入系统应答码，存在时跳过，不存在插入
 func xTestAddRespCodeFromCsv(t *testing.T) {
-	// step 1
+	// respCode
 	err := AddSysCodeFromCsv("respCode_quickpay.csv")
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
+}
 
-	// step 2
-	err = AddChanCodeFromScv("cfca", "respCode_cfca.csv")
+// 导入渠道应答码
+// 导入前先确定系统应答码表是否已存在
+// 存在则更新，但不会删除
+func xTestAddChanCodeFromScv(t *testing.T) {
+	// cfca
+	err := AddChanCodeFromScv("cfca", "respCode_cfca.csv")
 	if err != nil {
 		t.Error(err)
 		t.FailNow()
 	}
-
-	// step 3
+	// cil
 	err = AddChanCodeFromScv("cil", "respCode_cil.csv")
 	if err != nil {
 		t.Error(err)
@@ -28,22 +32,15 @@ func xTestAddRespCodeFromCsv(t *testing.T) {
 	}
 }
 
-func TestAddSettSchemeCd(t *testing.T) {
-
-	// import settSchemeCd
-	// err := AddSettSchemeCdFromCsv("settSchemeCd.csv")
-	// if err != nil {
-	// 	t.Error(err)
-	// 	t.FailNow()
-	// }
-}
-
-func TestAddCardBinFromCsv(t *testing.T) {
+// 导入卡bin表，执行单元测试时去掉x
+// false时会更新数据，但不会删除
+// true时会丢掉集合，重新建立
+func xTestAddCardBinFromCsv(t *testing.T) {
 
 	// import cardBin !!!
-	// err := AddCardBinFromCsv("cardBin.csv", false)
-	// if err != nil {
-	// 	t.Error(err)
-	// 	t.FailNow()
-	// }
+	err := AddCardBinFromCsv("cardBin.csv", false)
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
 }
