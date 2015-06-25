5f6acfabd7fb22c64ad411f5731a942d9fac5b0e
Rui <379630413@qq.com>
2015-06-25 11:25:47 +0800

    调试信息

--- channel/alipay/dmf_test.go
+++ channel/alipay/dmf_test.go
@@ -12,25 +12,28 @@ import (
 
 var pay = &model.ScanPay{
 	// GoodsInfo:    "鞋子,1000,2;衣服,1500,3",
-	SysOrderNum: tools.SerialNumber(),
-	Key:         "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
-	ScanCodeId:  "289253580324978839",
-	Txamt:       "0.01",
-	Subject:     "讯联测试",
+	OrderNum:   tools.SerialNumber(),
+	SignCert:   "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
+	ScanCodeId: "289253580324978839",
+	ActTxamt:   "0.01",
+	Subject:    "讯联测试",
+	ChanMerId:  "2088811767473826",
 }
 
 var prePay = &model.ScanPay{
 	// GoodsInfo:    "鞋子,1000,2;衣服,1500,3",
-	SysOrderNum: tools.SerialNumber(),
-	Key:         "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
-	Txamt:       "0.01",
-	Subject:     "讯联测试",
+	OrderNum:  tools.SerialNumber(),
+	SignCert:  "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
+	ActTxamt:  "0.01",
+	Subject:   "讯联测试",
+	ChanMerId: "2088811767473826",
 }
 
 var enquiry = &model.ScanPay{
 	// SysOrderNum: "fc718816621f4bc47fc09ccba1c66304",
-	SysOrderNum: "30723e05292b42df78c0e8fd8acb474e",
-	Key:         "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
+	OrderNum:  "1435200967398",
+	SignCert:  "eu1dr0c8znpa43blzy1wirzmk8jqdaon",
+	ChanMerId: "2088811767473826",
 }
 
 func TestProcessBarcodePay(t *testing.T) {
@@ -38,7 +41,7 @@ func TestProcessBarcodePay(t *testing.T) {
 	// 默认开启调试
 	// Debug = false
 	Convey("支付宝下单", t, func() {
-		resp := DefaultClient.ProcessBarcodePay(pay)
+		resp, _ := DefaultClient.ProcessBarcodePay(pay)
 		Convey("期望", func() {
 			So(resp.Respcd, ShouldEqual, "00")
 		})
@@ -52,7 +55,7 @@ func TestProcessQrCodeOfflinePay(t *testing.T) {
 	log.SetOutputLevel(log.Linfo)
 	log.Infof("%+v", prePay)
 	Convey("支付宝预下单", t, func() {
-		resp := DefaultClient.ProcessQrCodeOfflinePay(prePay)
+		resp, _ := DefaultClient.ProcessQrCodeOfflinePay(prePay)
 		log.Infof("%+v", resp)
 		Convey("期望", func() {
 			So(resp.Respcd, ShouldEqual, "09")
@@ -66,7 +69,7 @@ func TestProcessEnquiry(t *testing.T) {
 	// 默认开启调试
 	// log.SetOutputLevel(log.Linfo)
 	Convey("支付宝订单查询", t, func() {
-		resp := DefaultClient.ProcessEnquiry(enquiry)
+		resp, _ := DefaultClient.ProcessEnquiry(enquiry)
 		Convey("期望", func() {
 			So(resp.Respcd, ShouldEqual, "00")
 		})
