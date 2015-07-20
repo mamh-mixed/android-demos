3bf62d1e1b1bda5b625561beb86af3fa207e83dd
O' Migo <shangxuejin@gmail.com>
2015-07-20 23:16:49 +0800

    纠正单元测试中 tools => util

--- channel/weixin/scanpay/prePay_test.go
+++ channel/weixin/scanpay/prePay_test.go
@@ -3,7 +3,7 @@ package scanpay
 import (
 	"testing"
 
-	"github.com/CardInfoLink/quickpay/tools"
+	"github.com/CardInfoLink/quickpay/util"
 )
 
 func TestPrePay(t *testing.T) {
@@ -12,26 +12,26 @@ func TestPrePay(t *testing.T) {
 		Appid:    "wx25ac886b6dac7dd2", // 公众账号ID
 		MchID:    "1236593202",         // 商户号
 		SubMchId: "1247075201",         // 子商户号（文档没有该字段）
-		NonceStr: tools.Nonce(32),      // 随机字符串
+		NonceStr: util.Nonce(32),       // 随机字符串
 		Sign:     "",                   // 签名
 
 		WeixinMD5Key: "12sdffjjguddddd2widousldadi9o0i1",
 
-		DeviceInfo:     "",                   // 设备号
-		Body:           "product desc",       // 商品描述
-		Detail:         "",                   // 商品详情
-		Attach:         "",                   // 附加数据
-		OutTradeNo:     tools.SerialNumber(), // 商户订单号
-		TotalFee:       "1",                  // 总金额
-		FeeType:        "",                   // 货币类型
-		SpbillCreateIP: tools.LocalIP,        // 终端IP
-		TimeStart:      "",                   // 交易起始时间
-		TimeExpire:     "",                   // 交易结束时间
-		GoodsGag:       "",                   // 商品标记
-		NotifyURL:      weixinNotifyURL,      // 通知地址
-		TradeType:      "NATIVE",             // 交易类型
-		ProductID:      "",                   // 商品ID
-		Openid:         "",                   // 用户标识
+		DeviceInfo:     "",                  // 设备号
+		Body:           "product desc",      // 商品描述
+		Detail:         "",                  // 商品详情
+		Attach:         "",                  // 附加数据
+		OutTradeNo:     util.SerialNumber(), // 商户订单号
+		TotalFee:       "1",                 // 总金额
+		FeeType:        "",                  // 货币类型
+		SpbillCreateIP: util.LocalIP,        // 终端IP
+		TimeStart:      "",                  // 交易起始时间
+		TimeExpire:     "",                  // 交易结束时间
+		GoodsGag:       "",                  // 商品标记
+		NotifyURL:      weixinNotifyURL,     // 通知地址
+		TradeType:      "NATIVE",            // 交易类型
+		ProductID:      "",                  // 商品ID
+		Openid:         "",                  // 用户标识
 	}
 
 	r := &PrePayResp{}
--- channel/weixin/scanpay/refund_test.go
+++ channel/weixin/scanpay/refund_test.go
@@ -3,7 +3,7 @@ package scanpay
 import (
 	"testing"
 
-	"github.com/CardInfoLink/quickpay/tools"
+	"github.com/CardInfoLink/quickpay/util"
 )
 
 func TestRefund(t *testing.T) {
@@ -12,7 +12,7 @@ func TestRefund(t *testing.T) {
 		Appid:    "wx25ac886b6dac7dd2", // 公众账号ID
 		MchID:    "1236593202",         // 商户号
 		SubMchId: "1247075201",         // 子商户号（文档没有该字段）
-		NonceStr: tools.Nonce(32),      // 随机字符串
+		NonceStr: util.Nonce(32),       // 随机字符串
 		Sign:     "",                   // 签名
 
 		WeixinMD5Key: "12sdffjjguddddd2widousldadi9o0i1",
@@ -20,7 +20,7 @@ func TestRefund(t *testing.T) {
 		DeviceInfo:    "",                                 // 设备号
 		TransactionId: "",                                 // 微信的订单号，优先使用
 		OutTradeNo:    "7a5d8c60e1284fe8697af775c60d15d7", // 商户系统内部的订单号，当没提供transaction_id时需要传这个
-		OutRefundNo:   tools.SerialNumber(),               // 商户退款单号
+		OutRefundNo:   util.SerialNumber(),                // 商户退款单号
 		TotalFee:      "3",                                // 总金额
 		RefundFee:     "1",                                // 退款金额
 		RefundFeeType: "",                                 // 货币种类
