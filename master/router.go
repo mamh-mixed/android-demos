29865175ad71c587c4f9b6c04e36523376a16964
Arthur.WANG <fnghwsj@qq.com>
2015-08-06 11:04:01 +0800

    机构商户的查找和列表查询方法重构

--- master/router.go
+++ master/router.go
@@ -1,7 +1,11 @@
 package master
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+
+	"github.com/CardInfoLink/quickpay/model"
 
 	"github.com/omigo/log"
 )
@@ -10,12 +14,34 @@ import (
 func MasterRoute(w http.ResponseWriter, r *http.Request) {
 	log.Infof("url = %s", r.URL.String())
 
+	var ret *model.ResultBody
+
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Errorf("Read all body error: %s", err)
+		w.WriteHeader(501)
+		return
+	}
+
 	switch r.URL.Path {
 	case "/master/trade/report":
 		tradeReport(w, r)
 		return
+	case "/master/merchant/find":
+		merId := r.FormValue("merId")
+		merStatus := r.FormValue("merStatus")
+		ret = Merchant.Find(merId, merStatus)
+	case "/master/merchant/save":
+		ret = Merchant.Save(data)
 	default:
 		w.WriteHeader(404)
 	}
 
+	rdata, err := json.Marshal(ret)
+	if err != nil {
+		w.Write([]byte("mashal data error"))
+	}
+
+	log.Tracef("response message: %s", rdata)
+	w.Write(rdata)
 }
