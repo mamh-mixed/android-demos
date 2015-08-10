1420ebd36a53325aa618ed57f7f6d5f3e95129a0
Rui <379630413@qq.com>
2015-08-10 17:59:09 +0800

    配置

--- master/import.go
+++ master/import.go
@@ -2,9 +2,14 @@ package master
 
 import (
 	"encoding/csv"
+	"github.com/CardInfoLink/quickpay/model"
 	"github.com/omigo/log"
 	"net/http"
 	"strings"
+)
+
+const (
+	totalFieldNum = 22
 )
 
 // importMerchant 接受csv格式文件，导入商户
@@ -31,8 +36,17 @@ func importMerchant(w http.ResponseWriter, r *http.Request) {
 	reader.Comment = '\n'
 	rawCSVdata, err := reader.ReadAll()
 
-	for _, mer := range rawCSVdata {
-		log.Debug(mer)
+	// 顺序 [商户编号 商户名称 机构号 权限（空即默认全部开放） 是否开启验签 签名密钥 商户商品名称 支付宝商户号 支付宝商户名称 讯联跟支付宝费率 商户跟讯联费率(支付宝) 支付宝密钥 微信商户号 微信子商户号 微信商户名称 是否代理商模式 微信AppId 讯联跟微信费率 商户跟讯联费率(微信) 微信签名密钥 微信http证书 微信http密钥]
+	for i, mer := range rawCSVdata {
+		log.Debug(len(mer))
+		// 跳过头行
+		if i == 0 {
+			continue
+		}
+		if len(mer) != totalFieldNum {
+			m := &model.Merchant{}
+			m.MerId = mer[0]
+		}
 	}
 
 }
