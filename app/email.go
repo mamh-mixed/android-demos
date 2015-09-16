499ce8403d62fb517bc21c3f4357cb21f9e0287a
Rui <379630413@qq.com>
2015-09-16 14:02:19 +0800

    邮件发送整理

---
+++ app/email.go
@@ -0,0 +1,36 @@
+package app
+
+var (
+	andyLi      = "andy.li@cardinfolink.com"
+	actTemplate = `	
+	<html>
+		<body>
+			<h3>
+				点击以下链接以激活账户</br>
+				<a href="%s">%s</a>
+			</h3>
+		</body>
+	</html>
+	`
+
+	promoteTemplate = `
+	<html>
+		<body>
+			<h3>
+				Hello,Andy.li:
+			</h3>
+		</body>
+	</html>
+	`
+)
+
+// 邮件模板消息体
+var (
+	activation = emailTemplate{Title: "云收银帐号激活", Body: actTemplate}
+	promote    = emailTemplate{Title: "申请提升限额", Body: promoteTemplate}
+)
+
+type emailTemplate struct {
+	Title string
+	Body  string
+}
--- email/email.go
+++ email/email.go
@@ -2,7 +2,6 @@ package email
 
 import (
 	"crypto/rand"
-	"encoding/base64"
 	"fmt"
 	"github.com/omigo/log"
 	"io"
@@ -14,28 +13,27 @@ const (
 	HOST     = "smtp.qiye.163.com"
 	PASSWORD = "cil1009"
 	USER     = "it.support@cardinfolink.com"
-	AndyLi   = "andy.li@cardinfolink.com"
 )
 
 var auth = smtp.PlainAuth("", USER, PASSWORD, HOST)
-var b64Encoding = base64.StdEncoding
+var contentType = "Content-Type: text/html; charset=UTF-8"
 
 type Email struct {
-	To     string
-	ActUrl string
+	To    string
+	Title string
+	Body  string
 }
 
+// Send 发送邮件
 func (e *Email) Send() error {
 
-	var iv [20]byte
-	if _, err := io.ReadFull(rand.Reader, iv[:]); err != nil {
-		log.Errorf("io.ReadFull error: %s", err)
-	}
-
-	body := fmt.Sprintf(template, e.ActUrl, b64Encoding.EncodeToString(iv[:]))
-	msg := "To: " + e.To + "\r\nFrom: " + USER + ">\r\nSubject: " + subject + "\r\n" + contentType + "\r\n\r\n" + body
+	// var iv [20]byte
+	// if _, err := io.ReadFull(rand.Reader, iv[:]); err != nil {
+	// 	log.Errorf("io.ReadFull error: %s", err)
+	// }
+	// body := fmt.Sprintf(template, e.ActUrl, b64Encoding.EncodeToString(iv[:]))
+	msg := "To: " + e.To + "\r\nFrom: " + USER + ">\r\nSubject: " + e.Title + "\r\n" + contentType + "\r\n\r\n" + e.Body
 	log.Infof("SendBody: %s", msg)
-
 	// send
 	err := smtp.SendMail(ADDR, auth, USER, []string{e.To}, []byte(msg))
 	if err != nil {
@@ -43,29 +41,3 @@ func (e *Email) Send() error {
 	}
 	return err
 }
-
-var (
-	subject     = "云收银帐号激活"
-	contentType = "Content-Type: text/html; charset=UTF-8"
-
-	actTemplate = `	
-	<html>
-		<body>
-			<h3>
-				点击以下链接以激活账户</br>
-				<a href="%s">%s</a>
-			</h3>
-		</body>
-	</html>
-	`
-
-	promoteTemplate = `
-	<html>
-		<body>
-			<h3>
-				Hello,Andy.li:
-			</h3>
-		</body>
-	</html>
-	`
-)
