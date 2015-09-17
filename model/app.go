897250906ca5b088ff31c5628b5ee195a9f97116
Rui <379630413@qq.com>
2015-09-17 18:24:47 +0800

    云收银接口

--- model/app.go
+++ model/app.go
@@ -25,6 +25,8 @@ var (
 	USERNAME_NO_EXIST       = NewAppResult(FAIL, "username_no_exist")
 	OLD_PASSWORD_ERROR      = NewAppResult(FAIL, "old_password_error")
 	PARAMS_EMPTY            = NewAppResult(FAIL, "params_empty")
+	NO_PAY_MER              = NewAppResult(FAIL, "找不到支付商户")
+	NO_TRANS                = NewAppResult(FAIL, "找不到交易")
 )
 
 type AppResult struct {
@@ -34,11 +36,11 @@ type AppResult struct {
 
 	// 可选
 	User         *AppUser    `json:"user,omitempty"`
-	TotalAmt     int         `json:"total,omitempty"`
+	TotalAmt     string      `json:"total,omitempty"`
 	Count        int         `json:"count,omitempty"`
 	Size         int         `json:"size,omitempty"`
 	RefdCount    int         `json:"refdcount,omitempty"`
-	RefdTotalAmt int         `json:"refdtotal,omitempty"`
+	RefdTotalAmt string      `json:"refdtotal,omitempty"`
 	BankOpen     string      `json:"bank_open,omitempty"`
 	Payee        string      `json:"payee,omitempty"`
 	PayeeCard    string      `json:"payee_card,omitempty"`
