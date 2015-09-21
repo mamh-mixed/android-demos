efb5826d6853c16023cb87f896c30f77b428b30a
Rui <379630413@qq.com>
2015-09-21 09:43:38 +0800

    异步通知流程修改

--- app/user.go
+++ app/user.go
@@ -234,7 +234,7 @@ func (u *user) improveInfo(req *reqParams) (result *model.AppResult) {
 	// 创建商户
 	uniqueId := fmt.Sprintf("%d%d", time.Now().Unix(), rand.Int31())
 	randStr := fmt.Sprintf("%d", rand.Int31())
-	permission := []string{"PAUT", "PURC", "CANC", "VOID", "INQY", "REFD", "JSZF", "QYFK"}
+	permission := []string{model.Paut, model.Purc, model.Canc, model.Void, model.Inqy, model.Refd, model.Jszf, model.Qyfk}
 	merchant := &model.Merchant{
 		AgentCode:  "99911888",
 		AgentName:  "讯联O2O机构",
