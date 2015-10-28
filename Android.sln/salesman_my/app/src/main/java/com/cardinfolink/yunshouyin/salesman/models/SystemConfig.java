d6c08ee13c36f92d0a7432cebbc2846e3399580f
“Jiahua” <jiahua.xu@outlook.com>
2015-10-28 16:48:54 +0800

    correct pro url

--- Android.sln/salesman_my/app/src/main/java/com/cardinfolink/yunshouyin/salesman/models/SystemConfig.java
+++ Android.sln/salesman_my/app/src/main/java/com/cardinfolink/yunshouyin/salesman/models/SystemConfig.java
@@ -16,10 +16,7 @@ public class SystemConfig {
 
     // 行号信息
     public static final String bankbase_key = "20e786206dcf4aae8a63fe34553fd274";
-    // testing
-//    public static final String bankbase_url = "http://211.144.213.120:443/bdp";
-    // pro
-    public static final String bankbase_url = "http://211.144.213.118:443/bdp";
+    public static final String bankbase_url = "http://211.144.213.120:443/bdp";
 
     //扫固定码支付网页订单支付
 //    public static final String WEB_PAY_URL ="http://qrcode.cardinfolink.net/agent/pay1.html?merchantCode=";
