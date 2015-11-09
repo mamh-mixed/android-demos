16a967df27aaafee11c2468a6cb906cf748d977e
Jiahua <jiahua.xu@outlook.com>
2015-11-09 15:44:59 +0800

    refactor

--- Android.sln/showmoney_main/showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/SystemConfig.java
+++ Android.sln/showmoney_main/showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/SystemConfig.java
@@ -1,5 +1,6 @@
 package com.cardinfolink.yunshouyin.constant;
 
+import com.cardinfolink.yunshouyin.BuildConfig;
 import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.util.ContextUtil;
 
@@ -9,13 +10,13 @@ public class SystemConfig {
     public static final String APP_KEY = ContextUtil.getInstance().getResources().getString(R.string.app_key);//app用户系统交互key
 
     //用户系统服务器地址
-    public static final String Server = ContextUtil.getInstance().getResources().getString(R.string.user_server);
+    public static final String Server = BuildConfig.URL;
 
     //扫固定码支付网页订单支付
     public static final String WEB_BILL_URL = ContextUtil.getInstance().getResources().getString(R.string.web_bill_url);
 
     // SDK 环境
-    public static final boolean IS_PRODUCE = true;
+    public static final boolean IS_PRODUCE = BuildConfig.IS_PRODUCE;
 
     //公共数据平台秘钥和key
 
