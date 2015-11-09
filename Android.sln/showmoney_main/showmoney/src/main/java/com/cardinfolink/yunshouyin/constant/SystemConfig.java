3968e7c9357cd9a706f0b2fd6a853023ca4de6d6
Jiahua <jiahua.xu@outlook.com>
2015-11-09 16:24:39 +0800

    refactor

--- Android.sln/showmoney_main/showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/SystemConfig.java
+++ Android.sln/showmoney_main/showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/SystemConfig.java
@@ -1,22 +1,21 @@
 package com.cardinfolink.yunshouyin.constant;
 
-import com.cardinfolink.yunshouyin.BuildConfig;
 import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.util.ContextUtil;
 
 public class SystemConfig {
 
 
-    public static final String APP_KEY = ContextUtil.getInstance().getResources().getString(R.string.app_key);//app用户系统交互key
+    public static String APP_KEY = ContextUtil.getInstance().getResources().getString(R.string.app_key);//app用户系统交互key
 
     //用户系统服务器地址
-    public static final String Server = BuildConfig.URL;
+    public static String Server;
 
     //扫固定码支付网页订单支付
-    public static final String WEB_BILL_URL = ContextUtil.getInstance().getResources().getString(R.string.web_bill_url);
+    public static String WEB_BILL_URL;
 
     // SDK 环境
-    public static final boolean IS_PRODUCE = BuildConfig.IS_PRODUCE;
+    public static boolean IS_PRODUCE;
 
     //公共数据平台秘钥和key
 
--- Android.sln/showmoney_main/showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ContextUtil.java
+++ Android.sln/showmoney_main/showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ContextUtil.java
@@ -2,7 +2,12 @@ package com.cardinfolink.yunshouyin.util;
 
 import android.app.Application;
 
+import com.cardinfolink.yunshouyin.BuildConfig;
+import com.cardinfolink.yunshouyin.constant.SystemConfig;
+
 public class ContextUtil extends Application {
+    private static final String ENVIRONMENT = BuildConfig.ENVIRONMENT;
+
     private static ContextUtil instance;
 
     public static ContextUtil getInstance() {
@@ -20,5 +25,34 @@ public class ContextUtil extends Application {
         instance = this;
         CrashHandler handler = CrashHandler.getInstance();
         handler.init(getApplicationContext());
+
+        initEnvironment();
+    }
+
+
+    private void initEnvironment() {
+        //default is pro
+        SystemConfig.IS_PRODUCE =true;
+        SystemConfig.Server="https://api.shou.money/app";
+        SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/payment/trade.html";
+        switch (ENVIRONMENT){
+            case "pro":
+                SystemConfig.IS_PRODUCE =true;
+                SystemConfig.Server="https://api.shou.money/app";
+                SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/payment/trade.html";
+                break;
+            case "test":
+                SystemConfig.IS_PRODUCE =false;
+                SystemConfig.Server="http://test.quick.ipay.so/app";
+                SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/agent/trade.html";
+                break;
+            case "dev":
+                SystemConfig.IS_PRODUCE =false;
+                SystemConfig.Server="http://dev.quick.ipay.so/app";
+                SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/agent/trade.html";
+                break;
+            default:
+                break;
+        }
     }
 }
