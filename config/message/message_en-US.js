44479a9f34630ec485cb70f40dbca3dfc7330b54
Rui <379630413@qq.com>
2015-11-20 10:54:49 +0800

    补充提交

---
+++ config/message/message_en-US.js
@@ -0,0 +1,20 @@
+{
+	"StatReport":{
+		"Title":"merchant trade summary",
+		"Total" :"total: ",
+		"StartDate":"start date: ",
+		"EndDate" :"end date: ",
+		"Remark":"remark：手续费为每笔单笔计算后四舍五入精确到分，跟总额计算手续费略有误差。因本表仅统计了讯联数据系统的数据，数据仅供参考",
+		"MerId":"merchantID",
+		"MerName":"merchant Name",
+		"Summary":"summary",
+		"ALP" :"Alipay",
+		"WXP":"Weixin",
+		"AgentName":"agentName",
+		"TotalCount":"total count",
+		"TotalAmt" :"total amt",
+		"Count":"count",
+		"Amt":"amt",
+		"Fee":"fee"
+	}
+}
