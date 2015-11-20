8649065a2834ef70d063a41716bdbd7cf359eced
Minghui Ma <mamh.ma@cardinfolink.com>
2015-11-20 15:30:22 +0800

    showmoney: use quickPayService to get bill
    使用新的quickPayService这个类来获取账单。
    替代了原来的HttpCommunicationUtil
    
    Change-Id: I9d704ed85204acf8357519bd7364089a241fa6e7
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/model/QRequest.java
@@ -0,0 +1,104 @@
+package com.cardinfolink.yunshouyin.model;
+
+public class QRequest {
+    private String busicd;
+    private String inscd;
+    private String txndir;
+    private String terminalid;
+    private String origOrderNum;
+    private String orderNum;
+    private String mchntid;
+    private String tradeFrom;
+    private String txamt;
+    private String chcd;
+    private String goodsInfo;
+
+
+    public String getBusicd() {
+        return busicd;
+    }
+
+    public void setBusicd(String busicd) {
+        this.busicd = busicd;
+    }
+
+    public String getInscd() {
+        return inscd;
+    }
+
+    public void setInscd(String inscd) {
+        this.inscd = inscd;
+    }
+
+    public String getTxndir() {
+        return txndir;
+    }
+
+    public void setTxndir(String txndir) {
+        this.txndir = txndir;
+    }
+
+    public String getTerminalid() {
+        return terminalid;
+    }
+
+    public void setTerminalid(String terminalid) {
+        this.terminalid = terminalid;
+    }
+
+    public String getOrigOrderNum() {
+        return origOrderNum;
+    }
+
+    public void setOrigOrderNum(String origOrderNum) {
+        this.origOrderNum = origOrderNum;
+    }
+
+    public String getOrderNum() {
+        return orderNum;
+    }
+
+    public void setOrderNum(String orderNum) {
+        this.orderNum = orderNum;
+    }
+
+    public String getMchntid() {
+        return mchntid;
+    }
+
+    public void setMchntid(String mchntid) {
+        this.mchntid = mchntid;
+    }
+
+    public String getTradeFrom() {
+        return tradeFrom;
+    }
+
+    public void setTradeFrom(String tradeFrom) {
+        this.tradeFrom = tradeFrom;
+    }
+
+    public String getTxamt() {
+        return txamt;
+    }
+
+    public void setTxamt(String txamt) {
+        this.txamt = txamt;
+    }
+
+    public String getChcd() {
+        return chcd;
+    }
+
+    public void setChcd(String chcd) {
+        this.chcd = chcd;
+    }
+
+    public String getGoodsInfo() {
+        return goodsInfo;
+    }
+
+    public void setGoodsInfo(String goodsInfo) {
+        this.goodsInfo = goodsInfo;
+    }
+}
--- showmoney/src/main/java/com/cardinfolink/yunshouyin/model/Txn.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/model/Txn.java
@@ -4,6 +4,37 @@ public class Txn {
     private String response;
     private String system_date;
     private String consumerAccount;
+    private QRequest m_request;
 
+    public String getResponse() {
+        return response;
+    }
 
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public String getSystem_date() {
+        return system_date;
+    }
+
+    public void setSystem_date(String system_date) {
+        this.system_date = system_date;
+    }
+
+    public String getConsumerAccount() {
+        return consumerAccount;
+    }
+
+    public void setConsumerAccount(String consumerAccount) {
+        this.consumerAccount = consumerAccount;
+    }
+
+    public QRequest getM_request() {
+        return m_request;
+    }
+
+    public void setM_request(QRequest m_request) {
+        this.m_request = m_request;
+    }
 }
