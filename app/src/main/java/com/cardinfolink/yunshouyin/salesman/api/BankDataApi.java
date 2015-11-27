bcb052158ab2f01b5fa9e675aa40d5d0f8462b07
Minghui Ma <mamh.ma@cardinfolink.com>
2015-11-27 15:41:45 +0800

    salesman: change getProvince return a List<Province>
    改变gerProvince（）返回值类型。现在返回一个Province
    对象的list。类Province对应这个json的一个实体类。
    
    Change-Id: Id158cc52d54b92ebfa462b74dee2dc33c337670a
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- app/src/main/java/com/cardinfolink/yunshouyin/salesman/api/BankDataApi.java
+++ app/src/main/java/com/cardinfolink/yunshouyin/salesman/api/BankDataApi.java
@@ -3,13 +3,14 @@ package com.cardinfolink.yunshouyin.salesman.api;
 
 import com.cardinfolink.yunshouyin.salesman.model.Bank;
 import com.cardinfolink.yunshouyin.salesman.model.City;
+import com.cardinfolink.yunshouyin.salesman.model.Province;
 import com.cardinfolink.yunshouyin.salesman.model.SubBank;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BankDataApi {
-    List<String> getProvince();
+    List<Province> getProvince();
 
     List<City> getCity(String province);
 
