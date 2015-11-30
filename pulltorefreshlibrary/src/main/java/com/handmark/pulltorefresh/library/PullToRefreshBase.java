a65699d2b6746301b1c7e66ba105c9ed4fe54244
Minghui Ma <mamh.ma@cardinfolink.com>
2015-11-30 10:06:04 +0800

    showmoney: pulltorefreshlibrary: disable print debug log
    Change-Id: If49469b3f2802b8436e2ed163e7184a8d3b2e62c
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>
    (cherry picked from commit f5cebd1e3336d16d9394e6c84f31982565b2aa27)

--- pulltorefreshlibrary/src/main/java/com/handmark/pulltorefresh/library/PullToRefreshBase.java
+++ pulltorefreshlibrary/src/main/java/com/handmark/pulltorefresh/library/PullToRefreshBase.java
@@ -49,7 +49,7 @@ public abstract class PullToRefreshBase<T extends View> extends LinearLayout
 
     public static final int SMOOTH_SCROLL_DURATION_MS = 200;
     public static final int SMOOTH_SCROLL_LONG_DURATION_MS = 325;
-    static final boolean DEBUG = true;
+    static final boolean DEBUG = false;
     static final boolean USE_HW_LAYERS = false;
     static final String LOG_TAG = "PullToRefresh";
     static final float FRICTION = 2.0f;
