2147ece6d5ffa11a85813e8942786a938ce8ed26
Minghui Ma <mamh.ma@cardinfolink.com>
2015-11-30 10:16:32 +0800

    showmoney: add BankDataServiceImpl in ShowMoneyApp
    在ShowMoneyApp类里添加了BankDataServiceImpl的实例
    BankDataServiceImpl类增加了构造方法
    
    Change-Id: I56217b1c26b3316580a0f7c11b9b6abb276cfff2
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>
    (cherry picked from commit 1291d6426e5711d0f831759c825c748d18fc4a7a)

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ShowMoneyApp.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ShowMoneyApp.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.cardinfolink.yunshouyin.BuildConfig;
 import com.cardinfolink.yunshouyin.api.QuickPayConfigStorage;
 import com.cardinfolink.yunshouyin.constant.SystemConfig;
+import com.cardinfolink.yunshouyin.core.BankDataService;
+import com.cardinfolink.yunshouyin.core.BankDataServiceImpl;
 import com.cardinfolink.yunshouyin.core.QuickPayService;
 import com.cardinfolink.yunshouyin.core.QuickPayServiceImpl;
 
@@ -12,6 +14,10 @@ public class ShowMoneyApp extends Application {
     private static final String ENVIRONMENT = BuildConfig.ENVIRONMENT;
 
     private static ShowMoneyApp instance;
+
+    private QuickPayConfigStorage quickPayConfigStorage;
+    private QuickPayService quickPayService;
+    private BankDataService bankDataService;
 
     public static ShowMoneyApp getInstance() {
         return instance;
@@ -21,11 +27,13 @@ public class ShowMoneyApp extends Application {
         return instance.getResources().getString(id);
     }
 
-    private QuickPayConfigStorage quickPayConfigStorage;
-    private QuickPayService quickPayService;
 
     public QuickPayService getQuickPayService() {
         return quickPayService;
+    }
+
+    public BankDataService getBankDataService() {
+        return bankDataService;
     }
 
     @Override
@@ -43,34 +51,37 @@ public class ShowMoneyApp extends Application {
         quickPayConfigStorage.setAppKey("eu1dr0c8znpa43blzy1wirzmk8jqdaon");
 
         //default is pro
-        SystemConfig.IS_PRODUCE =true;
-        SystemConfig.Server="https://api.shou.money/app";
-        SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/payment/trade.html";
-        switch (ENVIRONMENT){
+        SystemConfig.IS_PRODUCE = true;
+        SystemConfig.Server = "https://api.shou.money/app";
+        SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/payment/trade.html";
+        switch (ENVIRONMENT) {
             case "pro":
-                SystemConfig.IS_PRODUCE =true;
-                SystemConfig.Server="https://api.shou.money/app";
-                SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/payment/trade.html";
+                SystemConfig.IS_PRODUCE = true;
+                SystemConfig.Server = "https://api.shou.money/app";
+                SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/payment/trade.html";
                 break;
             case "test":
-                SystemConfig.IS_PRODUCE =false;
-                SystemConfig.Server="http://test.quick.ipay.so/app";
-                SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/agent/trade.html";
+                SystemConfig.IS_PRODUCE = false;
+                SystemConfig.Server = "http://test.quick.ipay.so/app";
+                SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/agent/trade.html";
                 break;
             case "dev":
-                SystemConfig.IS_PRODUCE =false;
-                SystemConfig.Server="http://dev.quick.ipay.so/app";
-                SystemConfig.WEB_BILL_URL="http://qrcode.cardinfolink.net/agent/trade.html";
+                SystemConfig.IS_PRODUCE = false;
+                SystemConfig.Server = "http://dev.quick.ipay.so/app";
+                SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/agent/trade.html";
                 break;
             default:
                 break;
         }
 
         quickPayConfigStorage.setUrl(SystemConfig.Server);
-        quickPayService = new QuickPayServiceImpl(quickPayConfigStorage);
 
         quickPayConfigStorage.setBankbaseKey(SystemConfig.bankbase_key);
         quickPayConfigStorage.setBankbaseUrl(SystemConfig.bankbase_url);
+
+        quickPayService = new QuickPayServiceImpl(quickPayConfigStorage);
+        bankDataService = new BankDataServiceImpl(quickPayConfigStorage);
+
 
 
     }
