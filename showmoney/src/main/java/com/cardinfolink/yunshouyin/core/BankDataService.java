2608220921224a0d12580d68d9559043c33a1141
Minghui Ma <mamh.ma@cardinfolink.com>
2015-11-30 11:42:19 +0800

    showmoney: add getProvince() in BankDataServiceImpl
    Change-Id: Ia6bb4cc4752b3ed1a746dcc027a584e1b42dcced
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>
    (cherry picked from commit def4e89757a1f09657a8af40e27ba60a7d975726)

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/core/BankDataService.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/core/BankDataService.java
@@ -2,13 +2,14 @@ package com.cardinfolink.yunshouyin.core;
 
 import com.cardinfolink.yunshouyin.model.Bank;
 import com.cardinfolink.yunshouyin.model.City;
+import com.cardinfolink.yunshouyin.model.Province;
 import com.cardinfolink.yunshouyin.model.SubBank;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BankDataService {
-    void getProvince(QuickPayCallbackListener<List<String>> quickPayCallbackListener);
+    void getProvince(QuickPayCallbackListener<List<Province>> quickPayCallbackListener);
 
     void getCity(String province, QuickPayCallbackListener<List<City>> quickPayCallbackListener);
 
