1558fe67a5735a6cccab7db0549a71c8cdbc6914
Minghui Ma <mamh.ma@cardinfolink.com>
2015-11-30 18:24:52 +0800

    showmone: fixed get city error will use post
    很奇怪为什么post可以。get不行呢？？？
    
    Change-Id: I67114cb911e23f4f72aabaf8007dc8b03182a0fe
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/api/BankDataApiImpl.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/api/BankDataApiImpl.java
@@ -78,7 +78,7 @@ public class BankDataApiImpl implements BankDataApi {
         params.put("sig", createSign(params));
 
         try {
-            String response = postEngine.get(url, params);
+            String response = postEngine.post(url, params);
             Gson gson = new Gson();
             City[] arr = gson.fromJson(response, City[].class);
             return Arrays.asList(arr);
