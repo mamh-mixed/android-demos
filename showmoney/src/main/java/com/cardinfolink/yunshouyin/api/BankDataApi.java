def4e89757a1f09657a8af40e27ba60a7d975726
Minghui Ma <mamh.ma@cardinfolink.com>
2015-11-30 11:42:19 +0800

    showmoney: add getProvince() in BankDataServiceImpl
    Change-Id: Ia6bb4cc4752b3ed1a746dcc027a584e1b42dcced
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/api/BankDataApi.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/api/BankDataApi.java
@@ -2,14 +2,18 @@ package com.cardinfolink.yunshouyin.api;
 
 import com.cardinfolink.yunshouyin.model.Bank;
 import com.cardinfolink.yunshouyin.model.City;
+import com.cardinfolink.yunshouyin.model.Province;
 import com.cardinfolink.yunshouyin.model.SubBank;
 
 import java.util.List;
 import java.util.Map;
 
 public interface BankDataApi {
-    List<String> getProvince();
+    List<Province> getProvince();
+
     List<City> getCity(String province);
+
     Map<String, Bank> getBank();
+
     List<SubBank> getBranchBank(String city_code, String bank_id);
 }
