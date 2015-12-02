fd502cf88ed0bf68eed9ff85a1f5a0c3c4906fd9
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-02 15:20:15 +0800

    cashierSdk: add construct  and setter getter method in InitData.java
    添加了构造方法和set，get方法在InitData.java
    
    Change-Id: I6b710384548d4ba233d525f6ea83380c5f2695cc
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- cashierSdk/src/main/java/com/cardinfolink/cashiersdk/model/InitData.java
+++ cashierSdk/src/main/java/com/cardinfolink/cashiersdk/model/InitData.java
@@ -1,11 +1,62 @@
 package com.cardinfolink.cashiersdk.model;
 
 public class InitData {
-    public String inscd;
-    public String mchntid;
-    public String signKey;
-    public String terminalid;
-    public boolean isProduce = false;
-    ;
+    public String inscd;                // 机构号
+    public String mchntid;              // 商户号
+    public String signKey;              // 秘钥
+    public String terminalid;           // 设备号
+    public boolean isProduce = false;   //是否是生产环境
 
+    public InitData() {
+
+    }
+
+    public InitData(String inscd, String mchntid, String signKey, String terminalid, boolean isProduce) {
+        this.inscd = inscd;
+        this.mchntid = mchntid;
+        this.signKey = signKey;
+        this.terminalid = terminalid;
+        this.isProduce = isProduce;
+    }
+
+
+    public String getInscd() {
+        return inscd;
+    }
+
+    public void setInscd(String inscd) {
+        this.inscd = inscd;
+    }
+
+    public String getMchntid() {
+        return mchntid;
+    }
+
+    public void setMchntid(String mchntid) {
+        this.mchntid = mchntid;
+    }
+
+    public String getSignKey() {
+        return signKey;
+    }
+
+    public void setSignKey(String signKey) {
+        this.signKey = signKey;
+    }
+
+    public String getTerminalid() {
+        return terminalid;
+    }
+
+    public void setTerminalid(String terminalid) {
+        this.terminalid = terminalid;
+    }
+
+    public boolean isProduce() {
+        return isProduce;
+    }
+
+    public void setIsProduce(boolean isProduce) {
+        this.isProduce = isProduce;
+    }
 }
