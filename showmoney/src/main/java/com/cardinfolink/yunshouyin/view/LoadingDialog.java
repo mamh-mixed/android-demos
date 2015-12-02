fd091b5f7cc54043365eb8ae7958da4a19dbadea
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-02 17:54:02 +0800

    showmoney: add onTouchEvent in loadlingDialog view
    添加设置onTouch事件，这样在loading界面的时候就不能
    点击了
    
    Change-Id: Ic62150cf0e50195877f7bb541deadd8b2c7a19d8
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/LoadingDialog.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/LoadingDialog.java
@@ -2,6 +2,7 @@ package com.cardinfolink.yunshouyin.view;
 
 
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -26,8 +27,14 @@ public class LoadingDialog {
         dialogView.setVisibility(View.VISIBLE);
         Animation loadingAnimation = AnimationUtils.loadAnimation(mContext, R.anim.loading_animation);
         loadImg.startAnimation(loadingAnimation);
+        dialogView.setOnTouchListener(new View.OnTouchListener() {
 
-
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                //加入这个 在loading的时候，点击其他任何地方都会没有反应的。
+                return true;
+            }
+        });
     }
 
     public void endLoading() {
