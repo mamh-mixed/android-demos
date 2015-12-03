d5dfbd58e30cc18130143e24aa14eccd78f28ad7
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-03 11:11:14 +0800

    showmoney: remove btnRegisterNextOnClick in layout file
    删除布局文件register_activity.xml 里的
    android:onClick="btnRegisterNextOnClick"点击事件，
    采用统一的button的onclick
    mRegisterNext.setOnClickListener(new View.OnClickListener() {
        @Override
        public void onClick(View v) {
            btnRegisterNextOnClick(v);
        }
    });
    
    Change-Id: I972f7e8e9800ff9872b9b56c75275029b1c2ad6c
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>
    (cherry picked from commit c0cacaf89e07022f03dac547412d32b26e5c9824)

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.cardinfolink.yunshouyin.R;
@@ -11,11 +12,11 @@ import com.cardinfolink.yunshouyin.data.SaveData;
 import com.cardinfolink.yunshouyin.data.SessonData;
 import com.cardinfolink.yunshouyin.data.User;
 import com.cardinfolink.yunshouyin.util.CommunicationListener;
-import com.cardinfolink.yunshouyin.util.ShowMoneyApp;
 import com.cardinfolink.yunshouyin.util.ErrorUtil;
 import com.cardinfolink.yunshouyin.util.HttpCommunicationUtil;
 import com.cardinfolink.yunshouyin.util.JsonUtil;
 import com.cardinfolink.yunshouyin.util.ParamsUtil;
+import com.cardinfolink.yunshouyin.util.ShowMoneyApp;
 import com.cardinfolink.yunshouyin.util.VerifyUtil;
 import com.cardinfolink.yunshouyin.view.ActivateDialog;
 
@@ -26,6 +27,8 @@ public class RegisterActivity extends BaseActivity {
     private EditText mEmailEdit;
     private EditText mPasswordEdit;
     private EditText mQrPasswordEdit;
+
+    private Button mRegisterNext;
 
     /**
      * 验证邮箱
@@ -57,75 +60,84 @@ public class RegisterActivity extends BaseActivity {
     private void initLayout() {
         mEmailEdit = (EditText) findViewById(R.id.register_email);
         VerifyUtil.addEmailLimit(mEmailEdit);
+
         mPasswordEdit = (EditText) findViewById(R.id.register_password);
         VerifyUtil.addEmailLimit(mPasswordEdit);
+
         mQrPasswordEdit = (EditText) findViewById(R.id.register_qr_password);
         VerifyUtil.addEmailLimit(mQrPasswordEdit);
 
-
+        mRegisterNext = (Button) findViewById(R.id.btnregister);
+        mRegisterNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnRegisterNextOnClick(v);
+            }
+        });
     }
 
-    public void BtnRegisterNextOnClick(View view) {
-        if (validate()) {
-            mLoadingDialog.startLoading();
-            final String username = mEmailEdit.getText().toString();
-            final String password = mPasswordEdit.getText().toString();
-            HttpCommunicationUtil.sendDataToServer(ParamsUtil.getRegister(username, password), new CommunicationListener() {
+    public void btnRegisterNextOnClick(View view) {
+        if (!validate()) {
+            return;
+        }
+        mLoadingDialog.startLoading();
+        final String username = mEmailEdit.getText().toString();
+        final String password = mPasswordEdit.getText().toString();
+        HttpCommunicationUtil.sendDataToServer(ParamsUtil.getRegister(username, password), new CommunicationListener() {
 
-                @Override
-                public void onResult(final String result) {
+            @Override
+            public void onResult(final String result) {
 
-                    String state = JsonUtil.getParam(result, "state");
-                    if (state.equals("success")) {
-                        User user = new User();
-                        user.setUsername(username);
-                        user.setPassword(password);
-                        SaveData.setUser(mContext, user);
+                String state = JsonUtil.getParam(result, "state");
+                if (state.equals("success")) {
+                    User user = new User();
+                    user.setUsername(username);
+                    user.setPassword(password);
+                    SaveData.setUser(mContext, user);
 
-                        SessonData.loginUser.setUsername(username);
-                        SessonData.loginUser.setPassword(password);
-                        runOnUiThread(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                //更新UI
-                                mLoadingDialog.endLoading();
-                                ActivateDialog activate_dialog = new ActivateDialog(mContext, RegisterActivity.this.findViewById(R.id.activate_dialog), SessonData.loginUser.getUsername());
-                                activate_dialog.show();
-                            }
-
-                        });
-
-                    } else {
-                        runOnUiThread(new Runnable() {
-
-                            @Override
-                            public void run() {
-                                //更新UI
-                                mLoadingDialog.endLoading();
-                                mAlertDialog.show(ErrorUtil.getErrorString(JsonUtil.getParam(result, "error")), BitmapFactory.decodeResource(mContext.getResources(), R.drawable.wrong));
-                            }
-
-                        });
-
-                    }
-                }
-
-                @Override
-                public void onError(final String error) {
+                    SessonData.loginUser.setUsername(username);
+                    SessonData.loginUser.setPassword(password);
                     runOnUiThread(new Runnable() {
 
                         @Override
                         public void run() {
                             //更新UI
                             mLoadingDialog.endLoading();
-                            mAlertDialog.show(error, BitmapFactory.decodeResource(mContext.getResources(), R.drawable.wrong));
+                            ActivateDialog activate_dialog = new ActivateDialog(mContext, RegisterActivity.this.findViewById(R.id.activate_dialog), SessonData.loginUser.getUsername());
+                            activate_dialog.show();
                         }
 
                     });
+
+                } else {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            //更新UI
+                            mLoadingDialog.endLoading();
+                            mAlertDialog.show(ErrorUtil.getErrorString(JsonUtil.getParam(result, "error")), BitmapFactory.decodeResource(mContext.getResources(), R.drawable.wrong));
+                        }
+
+                    });
+
                 }
-            });
-        }
+            }
+
+            @Override
+            public void onError(final String error) {
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        //更新UI
+                        mLoadingDialog.endLoading();
+                        mAlertDialog.show(error, BitmapFactory.decodeResource(mContext.getResources(), R.drawable.wrong));
+                    }
+
+                });
+            }
+        });
 
     }
 
