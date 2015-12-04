17a285f56cc5b0aa1585df006bc82398a23f0617
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-04 20:18:36 +0800

    showmoney: use new api updatePasswordAsync() in QuickPayService
    Change-Id: I5f1faa7807dd69eb1e27e2186163d419c404245c
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/PasswordUpdateView.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/PasswordUpdateView.java
@@ -1,25 +1,21 @@
 package com.cardinfolink.yunshouyin.view;
 
-import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.activity.BaseActivity;
-import com.cardinfolink.yunshouyin.data.SaveData;
+import com.cardinfolink.yunshouyin.api.QuickPayException;
+import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
+import com.cardinfolink.yunshouyin.core.QuickPayService;
 import com.cardinfolink.yunshouyin.data.SessonData;
-import com.cardinfolink.yunshouyin.data.User;
-import com.cardinfolink.yunshouyin.util.CommunicationListener;
-import com.cardinfolink.yunshouyin.util.ErrorUtil;
-import com.cardinfolink.yunshouyin.util.HttpCommunicationUtil;
-import com.cardinfolink.yunshouyin.util.JsonUtil;
-import com.cardinfolink.yunshouyin.util.ParamsUtil;
 import com.cardinfolink.yunshouyin.util.ShowMoneyApp;
 import com.cardinfolink.yunshouyin.util.VerifyUtil;
 
@@ -29,105 +25,78 @@ public class PasswordUpdateView extends LinearLayout {
     private EditText mNewPwdEdit;
     private EditText mQrPwdEdit;
     private BaseActivity mBaseActivity;
+    private Button updatePasswordButton;
+
+    private QuickPayService quickPayService;
 
     public PasswordUpdateView(Context context) {
         super(context);
         mContext = context;
         mBaseActivity = (BaseActivity) mContext;
-        View contentView = LayoutInflater.from(context).inflate(
-                R.layout.password_update_view, null);
-        LinearLayout.LayoutParams layoutParams = new LayoutParams(
-                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+
+        quickPayService = ShowMoneyApp.getInstance().getQuickPayService();
+
+        View contentView = LayoutInflater.from(context).inflate(R.layout.password_update_view, null);
+        LinearLayout.LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         contentView.setLayoutParams(layoutParams);
         addView(contentView);
+
         mOldPwdEdit = (EditText) contentView.findViewById(R.id.update_password_oldpwd);
         VerifyUtil.addEmailLimit(mOldPwdEdit);
+
         mNewPwdEdit = (EditText) contentView.findViewById(R.id.update_password_newpwd);
         VerifyUtil.addEmailLimit(mNewPwdEdit);
+
         mQrPwdEdit = (EditText) contentView.findViewById(R.id.update_password_qr_newpwd);
         VerifyUtil.addEmailLimit(mQrPwdEdit);
 
 
-        contentView.findViewById(R.id.btn_update_password).setOnClickListener(new OnClickListener() {
+        updatePasswordButton = (Button) contentView.findViewById(R.id.btn_update_password);
+        updatePasswordButton.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
-                if (validate()) {
-                    mBaseActivity.startLoading();
-                    final String oldpwd = mOldPwdEdit.getText().toString().replace(" ", "");
-                    final String newpwd = mNewPwdEdit.getText().toString().replace(" ", "");
-                    ;
-                    String qrpwd = mQrPwdEdit.getText().toString().replace(" ", "");
-                    ;
-                    HttpCommunicationUtil.sendDataToServer(ParamsUtil.getUpdate(SessonData.loginUser.getUsername(), oldpwd, newpwd), new CommunicationListener() {
-
-                        @Override
-                        public void onResult(String result) {
-                            String state = JsonUtil.getParam(result, "state");
-                            if (state.equals("success")) {
-                                User user = SaveData.getUser(mContext);
-                                if (user.isAutoLogin()) {
-                                    user.setPassword(newpwd);
-                                }
-                                SaveData.setUser(mContext, user);
-                                SessonData.loginUser.setPassword(newpwd);
-                                ((Activity) mContext).runOnUiThread(new Runnable() {
-
-                                    @Override
-                                    public void run() {
-                                        //更新UI
-                                        mBaseActivity.endLoading();
-                                        AlertDialog alert_Dialog = new AlertDialog(mContext, null, ((Activity) mContext).findViewById(R.id.alert_dialog), getResources().getString(R.string.alert_update_success), BitmapFactory.decodeResource(mContext.getResources(), R.drawable.right));
-                                        alert_Dialog.show();
-                                        mOldPwdEdit.setText("");
-                                        mNewPwdEdit.setText("");
-                                        mQrPwdEdit.setText("");
-                                    }
-
-                                });
-
-                            } else {
-                                final String error = JsonUtil.getParam(result, "error");
-                                ((Activity) mContext).runOnUiThread(new Runnable() {
-
-                                    @Override
-                                    public void run() {
-                                        mBaseActivity.endLoading();
-                                        AlertDialog alert_Dialog = new AlertDialog(mContext, null, ((Activity) mContext).findViewById(R.id.alert_dialog), ErrorUtil.getErrorString(error), BitmapFactory.decodeResource(mContext.getResources(), R.drawable.wrong));
-                                        alert_Dialog.show();
-
-                                    }
-
-                                });
-
-                            }
-
-                        }
-
-                        @Override
-                        public void onError(final String error) {
-                            ((Activity) mContext).runOnUiThread(new Runnable() {
-
-                                @Override
-                                public void run() {
-                                    mBaseActivity.endLoading();
-                                    AlertDialog alert_Dialog = new AlertDialog(mContext, null, ((Activity) mContext).findViewById(R.id.alert_dialog), ErrorUtil.getErrorString(error), BitmapFactory.decodeResource(mContext.getResources(), R.drawable.wrong));
-                                    alert_Dialog.show();
-
-                                }
-
-                            });
-
-                        }
-                    });
-                }
-
+                updatePasswordOnClick(v);
             }
         });
 
     }
 
+    public void updatePasswordOnClick(View v) {
+        if (!validate()) {
+            return;
+        }
+
+        mBaseActivity.startLoading();
+        final String oldpwd = mOldPwdEdit.getText().toString().replace(" ", "");
+        final String newpwd = mNewPwdEdit.getText().toString().replace(" ", "");
+
+        quickPayService.updatePasswordAsync(SessonData.loginUser.getUsername(), oldpwd, newpwd, new QuickPayCallbackListener<Void>() {
+            @Override
+            public void onSuccess(Void data) {
+                //更新一下UI
+                mBaseActivity.endLoading();
+                View alertView = mBaseActivity.findViewById(R.id.alert_dialog);
+                String alertMsg = getResources().getString(R.string.alert_update_success);
+                Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.right);
+                AlertDialog alertDialog = new AlertDialog(mContext, null, alertView, alertMsg, bitmap);
+                alertDialog.show();
+                mOldPwdEdit.setText("");
+                mNewPwdEdit.setText("");
+                mQrPwdEdit.setText("");
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+                mBaseActivity.endLoading();
+                String error = ex.getErrorMsg();
+                View alertView = mBaseActivity.findViewById(R.id.alert_dialog);
+                Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(), R.drawable.wrong);
+                AlertDialog alertDialog = new AlertDialog(mContext, null, alertView, error, bitmap);
+                alertDialog.show();
+            }
+        });
+    }
 
     private boolean validate() {
         String oldpwd = mOldPwdEdit.getText().toString().replace(" ", "");//注意这里把所有的空格都删除了
