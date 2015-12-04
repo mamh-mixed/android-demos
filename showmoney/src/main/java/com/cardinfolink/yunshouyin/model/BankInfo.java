d7dfcc180bd74f01a566da8ea95f60a52c90f4f2
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-04 16:21:56 +0800

    showmoney: add new member for json key in BankInfo
    在BankInfo类里添加了新的成员变量
    对应与json字符串中的key
    {
     "state": "success",
     "count": 0,
     "size": 0,
     "refdcount": 0,
     "info": {   这里对应BankInfo类
        "bank_open": "中国工商银行",
        "payee": "姓名",
        "payee_card": "6222021001090585214",
        "phone_num": "13821936284",
        "province": "天津市",
        "city": "天津市",
        "branch_bank": "中国工商银行天津市金街支行",
        "bankNo": "102110000076|102100099996"
        }
    }
    
    Change-Id: I027da160aeef0b17fb05fd58f31e0bead92a6537
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/model/BankInfo.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/model/BankInfo.java
@@ -3,13 +3,48 @@ package com.cardinfolink.yunshouyin.model;
 import com.google.gson.annotations.SerializedName;
 
 public class BankInfo {
+    /**
+     * {
+     * "state": "success",
+     * "count": 0,
+     * "size": 0,
+     * "refdcount": 0,
+     * "info": {这里对应BankInfo类
+     * "bank_open": "中国工商银行",
+     * "payee": "司瑞华",
+     * "payee_card": "6222021001090585214",
+     * "phone_num": "13821936284",
+     * "province": "天津市",
+     * "city": "天津市",
+     * "branch_bank": "中国工商银行天津市金街支行",
+     * "bankNo": "102110000076|102100099996"
+     * }
+     * }
+     */
+
+
     @SerializedName("bank_open")
     private String bankOpen;
+
     private String payee;
+
     @SerializedName("payee_card")
     private String payeeCard;
+
     @SerializedName("phone_num")
     private String phoneNum;
+
+    @SerializedName("province")
+    private String province;
+
+    @SerializedName("city")
+    private String city;
+
+    @SerializedName("branch_bank")
+    private String branchBank;
+
+    @SerializedName("bankNo")
+    private String bankNo;
 
     public String getBankOpen() {
         return bankOpen;
@@ -41,5 +76,37 @@ public class BankInfo {
 
     public void setPhoneNum(String phoneNum) {
         this.phoneNum = phoneNum;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getBranchBank() {
+        return branchBank;
+    }
+
+    public void setBranchBank(String branchBank) {
+        this.branchBank = branchBank;
+    }
+
+    public String getBankNo() {
+        return bankNo;
+    }
+
+    public void setBankNo(String bankNo) {
+        this.bankNo = bankNo;
     }
 }
