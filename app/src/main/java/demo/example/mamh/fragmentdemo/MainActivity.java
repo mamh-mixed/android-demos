d459cb7ef5a27fbf3606aaf4f7ab1c8f2b4c9529
Minghui Ma <mmh19891113@qq.com>
2015-12-06 17:08:40 +0800

    fixed when rotate screen fragment  overlap
    修复旋转屏幕fragment会重叠的问题
    在onCreate()方法里加入了通过tag来初
    始化fragment,
    messageFragment = (MessageFragment)
        fragmentManager.findFragmentByTag("message");
    contactsFragment = (ContactsFragment)
        fragmentManager.findFragmentByTag("contacts");
    newsFragment = (NewsFragment)
        fragmentManager.findFragmentByTag("news");
    settingFragment = (SettingFragment)
        fragmentManager.findFragmentByTag("setting");
    在private void setTabSelection(int index)
        里面new的时候判断是否是null，是null的话再去
    new，不是就直接重复使用了。
    
    Signed-off-by: Minghui Ma <mmh19891113@qq.com>

--- app/src/main/java/demo/example/mamh/fragmentdemo/MainActivity.java
+++ app/src/main/java/demo/example/mamh/fragmentdemo/MainActivity.java
@@ -126,6 +126,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // 初始化布局元素
         initViews();
         fragmentManager = getFragmentManager();
+        messageFragment = (MessageFragment) fragmentManager.findFragmentByTag("message");
+        contactsFragment = (ContactsFragment) fragmentManager.findFragmentByTag("contacts");
+        newsFragment = (NewsFragment) fragmentManager.findFragmentByTag("news");
+        settingFragment = (SettingFragment) fragmentManager.findFragmentByTag("setting");
         // 第一次启动时选中第0个tab
         setTabSelection(0);
     }
@@ -196,7 +200,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 if (messageFragment == null) {
                     // 如果MessageFragment为空，则创建一个并添加到界面上
                     messageFragment = new MessageFragment();
-                    transaction.add(R.id.content, messageFragment);
+                    transaction.add(R.id.content, messageFragment, "message");
                 } else {
                     // 如果MessageFragment不为空，则直接将它显示出来
                     transaction.show(messageFragment);
@@ -209,7 +213,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 if (contactsFragment == null) {
                     // 如果ContactsFragment为空，则创建一个并添加到界面上
                     contactsFragment = new ContactsFragment();
-                    transaction.add(R.id.content, contactsFragment);
+                    transaction.add(R.id.content, contactsFragment, "contacts");
                 } else {
                     // 如果ContactsFragment不为空，则直接将它显示出来
                     transaction.show(contactsFragment);
@@ -222,7 +226,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 if (newsFragment == null) {
                     // 如果NewsFragment为空，则创建一个并添加到界面上
                     newsFragment = new NewsFragment();
-                    transaction.add(R.id.content, newsFragment);
+                    transaction.add(R.id.content, newsFragment, "news");
                 } else {
                     // 如果NewsFragment不为空，则直接将它显示出来
                     transaction.show(newsFragment);
@@ -236,7 +240,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 if (settingFragment == null) {
                     // 如果SettingFragment为空，则创建一个并添加到界面上
                     settingFragment = new SettingFragment();
-                    transaction.add(R.id.content, settingFragment);
+                    transaction.add(R.id.content, settingFragment, "setting");
                 } else {
                     // 如果SettingFragment不为空，则直接将它显示出来
                     transaction.show(settingFragment);
@@ -279,4 +283,5 @@ public class MainActivity extends Activity implements View.OnClickListener {
             transaction.hide(settingFragment);
         }
     }
+
 }
