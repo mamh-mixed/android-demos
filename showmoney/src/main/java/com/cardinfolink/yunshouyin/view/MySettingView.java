46efc774322d0af5d70f8b7d1abba244f1a52ae3
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-07 15:55:32 +0800

    showmoney: add onclick on my wap item in SettingView
    实现我的网页版 的跳转
    
    Change-Id: I97bd2fc0d4b7683de01459fb88b5713609d2e24e
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/MySettingView.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/MySettingView.java
@@ -1,20 +1,25 @@
 package com.cardinfolink.yunshouyin.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.activity.WapActivity;
+import com.cardinfolink.yunshouyin.ui.SettingClikcItem;
 
 /**
  * 第四个界面，就是设置界面
  * Created by mamh on 15-12-7.
  */
 public class MySettingView extends LinearLayout {
+    private static final String TAG = "MySettingView";
 
-    private static final String TAG = "TicketView";
     private Context mContext;
+
+    private SettingClikcItem mMyWap;
 
     public MySettingView(Context context) {
         super(context);
@@ -24,5 +29,13 @@ public class MySettingView extends LinearLayout {
         contentView.setLayoutParams(layoutParams);
         addView(contentView);
 
+        mMyWap = (SettingClikcItem) contentView.findViewById(R.id.my_wap);
+        mMyWap.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(mContext, WapActivity.class);
+                mContext.startActivity(intent);
+            }
+        });
     }
 }
