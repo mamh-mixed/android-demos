0a95f6bebe0257918375e34b4ec2c2db99b0719a
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-07 17:40:24 +0800

    showmoney: add new acitivity MyChannelActivity
    添加了我的渠道的activity界面
    1.显示我的渠道
    2.显示卡券渠道
    
    Change-Id: Idd0fb4072079fe5a12bb96c117a3e4208311602d
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/MyChannelActivity.java
@@ -0,0 +1,16 @@
+package com.cardinfolink.yunshouyin.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.cardinfolink.yunshouyin.R;
+
+public class MyChannelActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_channel);
+    }
+
+}
