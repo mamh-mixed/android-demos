e8163db0e1ede5878a86417c1ff1b9f5fbd766ae
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-07 17:00:39 +0800

    showmoney: add new activity AccountInfoActivity
    账户信息的activity.
    
    Change-Id: Iffc60681e56cb2eac34502bbde14e519f0d34568
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/AccountInfoActivity.java
@@ -0,0 +1,16 @@
+package com.cardinfolink.yunshouyin.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.cardinfolink.yunshouyin.R;
+
+public class AccountInfoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_info);
+    }
+
+}
