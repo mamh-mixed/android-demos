3c8f1bf64e660db3ae5c2c416c0c8b8038004e6a
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-07 14:11:47 +0800

    showmoney: add define view SettingClikcView
    添加了自定义的视图
    
    Change-Id: Ic536d1087fc764a4982c26dbb45d0c602904d3f6
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/ui/SettingClikcView.java
@@ -0,0 +1,59 @@
+package com.cardinfolink.yunshouyin.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.cardinfolink.yunshouyin.R;
+
+
+/**
+ * Created by mamh on 15-11-1.
+ * 自定义的组合控件
+ */
+public class SettingClikcView extends RelativeLayout {
+    private ImageView mImageView;
+    private TextView mTitle;
+
+
+    public SettingClikcView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public SettingClikcView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+        TypedArray typeArray = context.obtainStyledAttributes(attrs, R.styleable.SettingClikcView);
+        String title = typeArray.getString(R.styleable.SettingClikcView_title);
+        typeArray.recycle();
+        mTitle.setText(title);
+    }
+
+    public SettingClikcView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        initView(context);
+    }
+
+    private void initView(Context context) {
+        View.inflate(context, R.layout.setting_click_item, this);
+        mImageView = (ImageView) this.findViewById(R.id.iv_setting);
+        mTitle = (TextView) this.findViewById(R.id.tv_title);
+    }
+
+
+    public void setTitle(String title) {
+        if (mTitle != null) {
+            mTitle.setText(title);
+        }
+    }
+
+    public String getTitle() {
+        return mTitle.getText().toString();
+    }
+
+}
