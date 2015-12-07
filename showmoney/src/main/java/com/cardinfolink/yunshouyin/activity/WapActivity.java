226ebb2d87d6f893cd12acfb0af815ec8c9742ae
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-07 15:53:43 +0800

    showmoney: rename wapview to WapActivity
    Change-Id: Ie8f43773ebe1a722848b7f4821de74bb1fc1ab11
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/WapActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/WapActivity.java
@@ -1,34 +1,27 @@
-package com.cardinfolink.yunshouyin.view;
+package com.cardinfolink.yunshouyin.activity;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.LinearLayout;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.activity.BaseActivity;
 import com.cardinfolink.yunshouyin.constant.SystemConfig;
 import com.cardinfolink.yunshouyin.data.SessonData;
 
 
-public class WapView extends LinearLayout {
-    private Context mContext;
+public class WapActivity extends BaseActivity {
+
     private WebView mWebView;
 
-    public WapView(Context context) {
-        super(context);
-        mContext = context;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.wap_bill_view);
 
-        View contentView = LayoutInflater.from(context).inflate(
-                R.layout.wap_bill_view, null);
-        LinearLayout.LayoutParams layoutParams = new LayoutParams(
-                LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
-        contentView.setLayoutParams(layoutParams);
-        addView(contentView);
-        mWebView = (WebView) contentView.findViewById(R.id.base_webview);
+
+        mWebView = (WebView) findViewById(R.id.base_webview);
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -45,17 +38,11 @@ public class WapView extends LinearLayout {
 
 
         mWebView.setWebChromeClient(new WebChromeClient() {
-                                        @Override
-                                        public boolean onJsAlert(WebView view, String url, String message, android.webkit.JsResult result) {
-
-                                            return false;
-
-
-                                        }
-
-                                        ;
-                                    }
-        );
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, android.webkit.JsResult result) {
+                return false;
+            }
+        });
 
 
         WebSettings webSettings = mWebView.getSettings();
@@ -75,10 +62,8 @@ public class WapView extends LinearLayout {
 
     public void initData() {
         if (SessonData.loginUser.getObjectId() != null && SessonData.loginUser.getObjectId().length() > 0) {
-
             mWebView.loadUrl(SystemConfig.WEB_BILL_URL + "?merchantCode=" + SessonData.loginUser.getObjectId());
         }
-
     }
 
 }
