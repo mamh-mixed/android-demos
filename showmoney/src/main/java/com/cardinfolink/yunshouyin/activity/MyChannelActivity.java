2630041557b034b4fb6124af78fe72a904d2799c
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-08 09:43:26 +0800

    showmoney: implements my channel items onclick event
    实现"我的渠道"界面上的item的点击事件
    注意是返回item的点击事件
    
    Change-Id: Iee5c567459d0b0f2b40f8b5ea1f6ccbacd150a4b
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/MyChannelActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/MyChannelActivity.java
@@ -2,15 +2,27 @@ package com.cardinfolink.yunshouyin.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 public class MyChannelActivity extends Activity {
+
+    private SettingActionBarItem mMyChannel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_channel);
+
+        mMyChannel = (SettingActionBarItem) findViewById(R.id.sabi_my_channel);
+        mMyChannel.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
