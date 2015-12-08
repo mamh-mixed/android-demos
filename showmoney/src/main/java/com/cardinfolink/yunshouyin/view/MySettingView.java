b7495e5ee2c5a47a267c85b5dcb39a3e0e44a464
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-08 09:18:50 +0800

    showmoney: implements setting view items onclick event
    实现第四个界面 上的item的点击事件，点击会跳转到相应的
    界面上。
    1.账户与安全
    2.支持的渠道
    3.我的网页版
    4.关于云收银
    
    5.安全退出 按钮的点击事件的实现.
    
    Change-Id: I94b4e8edafc73dc2589c6b0ee435bccebdd1b5d1
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/MySettingView.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/MySettingView.java
@@ -1,12 +1,18 @@
 package com.cardinfolink.yunshouyin.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.activity.AboutActivity;
+import com.cardinfolink.yunshouyin.activity.AccountSecurityActivity;
+import com.cardinfolink.yunshouyin.activity.LoginActivity;
+import com.cardinfolink.yunshouyin.activity.MyChannelActivity;
 import com.cardinfolink.yunshouyin.activity.WapActivity;
 import com.cardinfolink.yunshouyin.ui.SettingClikcItem;
 
@@ -14,12 +20,17 @@ import com.cardinfolink.yunshouyin.ui.SettingClikcItem;
  * 第四个界面，就是设置界面
  * Created by mamh on 15-12-7.
  */
-public class MySettingView extends LinearLayout {
+public class MySettingView extends LinearLayout implements View.OnClickListener {
     private static final String TAG = "MySettingView";
 
     private Context mContext;
 
-    private SettingClikcItem mMyWap;
+    private SettingClikcItem mAccountAndSecurity;//账户与安全
+    private SettingClikcItem mSupportChannel;//支持的渠道
+    private SettingClikcItem mMyWap;//我的网页版
+    private SettingClikcItem mAbout;//关于云收银
+
+    private Button mExit;
 
     public MySettingView(Context context) {
         super(context);
@@ -29,13 +40,53 @@ public class MySettingView extends LinearLayout {
         contentView.setLayoutParams(layoutParams);
         addView(contentView);
 
+        mExit = (Button) contentView.findViewById(R.id.btn_exit);
+
+        mAccountAndSecurity = (SettingClikcItem) contentView.findViewById(R.id.account_security);
+        mSupportChannel = (SettingClikcItem) contentView.findViewById(R.id.support_channel);
         mMyWap = (SettingClikcItem) contentView.findViewById(R.id.my_wap);
-        mMyWap.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, WapActivity.class);
-                mContext.startActivity(intent);
-            }
-        });
+        mAbout = (SettingClikcItem) contentView.findViewById(R.id.about);
+
+        mExit.setOnClickListener(this);
+        mAccountAndSecurity.setOnClickListener(this);
+        mSupportChannel.setOnClickListener(this);
+        mMyWap.setOnClickListener(this);
+        mAbout.setOnClickListener(this);
+
     }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.btn_exit:
+                //finish();
+                intent = new Intent(mContext, LoginActivity.class);
+                mContext.startActivity(intent);
+                ((Activity)mContext).finish();
+                break;
+            case R.id.account_security:
+                //账户与安全
+                intent = new Intent(mContext, AccountSecurityActivity.class);
+                mContext.startActivity(intent);
+                break;
+            case R.id.support_channel:
+                //支持的渠道
+                intent = new Intent(mContext, MyChannelActivity.class);
+                mContext.startActivity(intent);
+                break;
+            case R.id.my_wap:
+                //我的网页版
+                intent = new Intent(mContext, WapActivity.class);
+                mContext.startActivity(intent);
+                break;
+            case R.id.about:
+                //关于云收银
+                intent = new Intent(mContext, AboutActivity.class);
+                mContext.startActivity(intent);
+                break;
+        }
+    }
+
 }
