cf27ef3400e7a4e6fc6c542665a2c8b80fdfe0d0
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-08 09:34:02 +0800

    showmoney: implements account security items onclick event
    实现"账户与安全"界面上的item的点击事件，
    点击会跳转到相应的 界面上。
    1.跳转到账户信息的界面
    2.跳转到修改密码的界面
    
    Change-Id: Id54bf52101865804f39143187594a3269609926c
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/AccountSecurityActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/AccountSecurityActivity.java
@@ -1,16 +1,52 @@
 package com.cardinfolink.yunshouyin.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
+import com.cardinfolink.yunshouyin.ui.SettingClikcItem;
 
-public class AccountSecurityActivity extends Activity {
+public class AccountSecurityActivity extends Activity implements View.OnClickListener {
+
+    private SettingClikcItem mAccountInfo;
+    private SettingClikcItem mUpdatePassword;
+    private SettingActionBarItem mAccountSecurity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_security);
+
+        mAccountInfo = (SettingClikcItem) findViewById(R.id.account_info);
+        mUpdatePassword = (SettingClikcItem) findViewById(R.id.update_password);
+        mAccountInfo.setOnClickListener(this);
+        mUpdatePassword.setOnClickListener(this);
+
+        mAccountSecurity = (SettingActionBarItem) findViewById(R.id.sabi_account_security);
+        mAccountSecurity.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.account_info:
+                intent = new Intent(AccountSecurityActivity.this, AccountInfoActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.update_password:
+                intent = new Intent(AccountSecurityActivity.this, UpdatePasswordActivity.class);
+                startActivity(intent);
+                break;
+        }
+        finish();//结束当前的activity
+    }
 }
