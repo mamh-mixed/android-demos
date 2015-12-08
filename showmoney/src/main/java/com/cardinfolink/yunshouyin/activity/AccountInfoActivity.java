aff32cf9b44e85a10fb678d9d6e67af8e7f896ee
Deng Jia <charles.deng@cardinfolink.com>
2015-12-08 14:19:20 +0800

    showmoney:add account info view back onclick
    添加"账户信息"界面的返回键
    
    Change-Id: If84e3f0fef85ca4eae17078a8b99284cfddf64a7
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/AccountInfoActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/AccountInfoActivity.java
@@ -2,15 +2,27 @@ package com.cardinfolink.yunshouyin.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 public class AccountInfoActivity extends Activity {
+
+    private SettingActionBarItem mAccountInfo;//账户信息 界面的标题栏
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_info);
+
+        mAccountInfo = (SettingActionBarItem) findViewById(R.id.sabi_account_info);
+        mAccountInfo.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
