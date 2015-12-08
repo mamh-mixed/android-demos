0e9ab374bc01a34af41ce430fce0e915189ca5ba
Deng Jia <charles.deng@cardinfolink.com>
2015-12-08 14:28:08 +0800

    showmoney:add change password title return
    添加了"修改密码"界面的activity的返回功能。
    
    Change-Id: Ibf16773b6863f0a28a875b79a8850baf564ca05e
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/UpdatePasswordActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/UpdatePasswordActivity.java
@@ -2,15 +2,27 @@ package com.cardinfolink.yunshouyin.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 public class UpdatePasswordActivity extends Activity {
+
+    private SettingActionBarItem mUpdatePassword;//修改密码界面的 标题栏
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_password);
+
+        mUpdatePassword = (SettingActionBarItem) findViewById(R.id.sabi_update_password);
+        mUpdatePassword.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
