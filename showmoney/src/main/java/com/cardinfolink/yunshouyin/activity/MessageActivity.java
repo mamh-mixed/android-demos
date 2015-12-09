3cbc40db4e1bff2cc34972d0f9e5e1528ba1f483
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-09 11:42:41 +0800

    showmoney: all message button add onclick event
    "全面消息"界面添加了 左右文本的点击事件
    左边文本点击跳转到 返回的
    右边文本点击跳转到 "未读消息" 界面
    
    Change-Id: I25c7aecafd304c21479afee86af81706d71cd4b4
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/MessageActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/MessageActivity.java
@@ -1,16 +1,49 @@
 package com.cardinfolink.yunshouyin.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 public class MessageActivity extends Activity {
+
+    private SettingActionBarItem mAllMessage;
+
+    private Button mSetAllMessageRead;
+
+    private ListView mAllMessageListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        mAllMessage = (SettingActionBarItem) findViewById(R.id.sabi_all_message);
+        mAllMessage.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //返回
+                finish();
+            }
+        });
+        mAllMessage.setRightTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //进入未读消息界面
+                Intent intent = new Intent(MessageActivity.this, UnReadMessageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        mSetAllMessageRead = (Button) findViewById(R.id.set_all_message_read);
+        mAllMessageListView = (ListView) findViewById(R.id.all_message);
     }
 
 }
