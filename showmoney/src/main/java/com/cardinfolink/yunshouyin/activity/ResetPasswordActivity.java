4493f58964047faff4524bed65119f220401dfbe
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-09 17:52:46 +0800

    showmoney: add reset password activity view
    添加了重置密码的界面
    之后在重置密码对话框添加跳转到这个界面。
    
    Change-Id: I3f5858f337adbf8bda7b80b4c28008067533c18e
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ResetPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.cardinfolink.yunshouyin.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.cardinfolink.yunshouyin.R;
+
+public class ResetPasswordActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+
+}
