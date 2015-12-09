60b18782b80d12533ad4a54a6ad1853810147e66
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-09 10:50:57 +0800

    showmoney: init user infos to show on AccountInfoActivity
    在"账户信息"界面显示用户的相关信息
    例如银行，所在城市，手机号等。。
    
    Change-Id: I14b03cd44f5f66702c330bcb2a686a6b5ddad489
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/AccountInfoActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/AccountInfoActivity.java
@@ -1,15 +1,25 @@
 package com.cardinfolink.yunshouyin.activity;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.View;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.api.QuickPayException;
+import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
+import com.cardinfolink.yunshouyin.data.SessonData;
+import com.cardinfolink.yunshouyin.model.BankInfo;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
+import com.cardinfolink.yunshouyin.ui.SettingDetailItem;
 
-public class AccountInfoActivity extends Activity {
+public class AccountInfoActivity extends BaseActivity {
 
     private SettingActionBarItem mAccountInfo;//账户信息 界面的标题栏
+
+    private SettingDetailItem mName;//这里显示收款人
+    private SettingDetailItem mLocalCity;//所在城市
+    private SettingDetailItem mBankName;//清算银行名字
+    private SettingDetailItem mMerchantName;//商户名
+    private SettingDetailItem mPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +31,54 @@ public class AccountInfoActivity extends Activity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+        mName = (SettingDetailItem) findViewById(R.id.name);//这里显示收款人 payee的值
+        mLocalCity = (SettingDetailItem) findViewById(R.id.local_city);
+        mBankName = (SettingDetailItem) findViewById(R.id.bank_name);
+        mMerchantName = (SettingDetailItem) findViewById(R.id.merchant_name);
+        mPhoneNumber = (SettingDetailItem) findViewById(R.id.phone_number);
+
+        initUserInfo();//初始化用户的信息
+
+    }
+
+    /**
+     * 初始化用户的信息，例如显示用户手机号，银行，所在城市等。。。。
+     * <p/>
+     * {
+     * "state": "success",
+     * "count": 0,
+     * "size": 0,
+     * "refdcount": 0,
+     * "info": {
+     * "bank_open": "中国工商银行",
+     * "payee": "马明辉",//收款人
+     * "payee_card": "6222021001114863340",
+     * "phone_num": "13014625286",
+     * "province": "上海市",
+     * "city": "上海市",
+     * "branch_bank": "中国工商银行股份有限公司上海市漕宝路支行",
+     * "bankNo": "102290004911|102100099996"
+     * }
+     * }
+     */
+
+    private void initUserInfo() {
+        quickPayService.getBankInfoAsync(SessonData.loginUser, new QuickPayCallbackListener<BankInfo>() {
+            @Override
+            public void onSuccess(BankInfo bankInfo) {
+                mName.setDetail(bankInfo.getPayee());
+                mLocalCity.setDetail(bankInfo.getCity());
+                mBankName.setDetail(bankInfo.getBankOpen());
+                mPhoneNumber.setDetail(bankInfo.getPhoneNum());
+                //这里商户名没有获得到！！！！！！！！
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+
             }
         });
     }
