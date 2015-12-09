9a9a6cc2748d773450afe58c4494d62b8beca86c
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-09 18:03:57 +0800

    showmoney: add back onclick event on ResetPasswordActivity
    添加了标题栏的返回按钮的点击事件
    
    Change-Id: Icbab02a7ad261f4b3ef26499b763b4448acaf9b0
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ResetPasswordActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ResetPasswordActivity.java
@@ -2,15 +2,28 @@ package com.cardinfolink.yunshouyin.activity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 public class ResetPasswordActivity extends Activity {
+
+    private SettingActionBarItem mResetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reset_password);
+
+        mResetPassword = (SettingActionBarItem) findViewById(R.id.sabi_reset_password);
+        mResetPassword.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
 }
