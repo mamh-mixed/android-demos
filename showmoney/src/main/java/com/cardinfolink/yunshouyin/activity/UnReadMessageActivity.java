b0f7a27a15ffd659f9d61d077330937cc61651fe
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-09 11:23:37 +0800

    showmoney: add unread message button onclick event
    添加"未读消息"界面的标题栏的返回点击事件
    返回到"全部消息"的界面上。
    
    Change-Id: Icef4d22958dff7cef9bcceaf445e4827f9926792
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/UnReadMessageActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/UnReadMessageActivity.java
@@ -1,16 +1,39 @@
 package com.cardinfolink.yunshouyin.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 public class UnReadMessageActivity extends Activity {
+
+    private SettingActionBarItem mUnreadMessage;
+
+    private Button mSetAllMessageRead;
+
+    private ListView mUnreadMessageListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_un_read_message);
+
+        mUnreadMessage = (SettingActionBarItem) findViewById(R.id.sabi_unread_message);
+        mUnreadMessage.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(UnReadMessageActivity.this, MessageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        mSetAllMessageRead = (Button) findViewById(R.id.set_all_message_read);
     }
 
 }
