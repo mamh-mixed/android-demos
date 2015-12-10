65b709d170906c62c8494ed4da7d407f1e26e10d
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-10 17:01:45 +0800

    showmoney: add back/next button onclick event
    添加了 返回按钮 下一步 按钮的点击事件
    
    Change-Id: I4bfac3ba3fb101570a88a950437e3298abe984fb
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivateActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivateActivity.java
@@ -1,16 +1,40 @@
 package com.cardinfolink.yunshouyin.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
+
 
 public class RegisterActivateActivity extends Activity {
+    private SettingActionBarItem mActionBar;
+    private Button mNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_activate);
+
+        mActionBar = (SettingActionBarItem) findViewById(R.id.action_bar);
+        mActionBar.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mNext = (Button) findViewById(R.id.btnnext);
+        mNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivateActivity.this, RegisterNextActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
