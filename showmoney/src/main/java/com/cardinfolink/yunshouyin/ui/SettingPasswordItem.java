b56ac50e3abcbb40f2aa7d1844214622d03b33f1
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-10 09:14:38 +0800

    showmoney: add text watcher on password edittext
    在密码输入框上添加了 文本变化的监听事件
    密码变化了，而且是空的 右边的图片就不显示。
    
    Change-Id: Ied49b7ac5cf429d6f8b895bca551881f88df2399
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/ui/SettingPasswordItem.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/ui/SettingPasswordItem.java
@@ -2,7 +2,10 @@ package com.cardinfolink.yunshouyin.ui;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.text.Editable;
 import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
@@ -16,7 +19,7 @@ import com.cardinfolink.yunshouyin.R;
  * Created by mamh on 15-11-1.
  * 自定义的组合控件
  */
-public class SettingPasswordItem extends RelativeLayout implements View.OnClickListener {
+public class SettingPasswordItem extends RelativeLayout implements View.OnClickListener, TextWatcher {
     private ImageView mImageView;
     private TextView mTitle;
     private EditTextClear mPassword;
@@ -50,8 +53,11 @@ public class SettingPasswordItem extends RelativeLayout implements View.OnClickL
         mImageView = (ImageView) this.findViewById(R.id.iv_show);
         mTitle = (TextView) this.findViewById(R.id.tv_title);
         mPassword = (EditTextClear) this.findViewById(R.id.et_password);
-
+        mPassword.addTextChangedListener(this);//这里设置一些密码输入框变化的监听事件
         mImageView.setOnClickListener(this);//这里设置图片的点击事件
+        if (TextUtils.isEmpty(getPassword())) {
+            mImageView.setVisibility(View.INVISIBLE);
+        }
     }
 
 
@@ -108,4 +114,23 @@ public class SettingPasswordItem extends RelativeLayout implements View.OnClickL
     }
 
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        //密码框输入变化后调用，如果密码是空的就不显示此图片了。
+        if (TextUtils.isEmpty(getPassword())) {
+            mImageView.setVisibility(INVISIBLE);
+        } else {
+            mImageView.setVisibility(VISIBLE);
+        }
+    }
 }
