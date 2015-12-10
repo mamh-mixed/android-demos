39baf7dad25e628be557f33741fec17f34aed63a
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-10 15:06:36 +0800

    showmoney: add forget passwod button  on click event
    忘记密码 界面 按钮添加了点击事件，会发送请求给服务器
    
    Change-Id: Ie25551733cea2011a2a126c586d755860aa72dde
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ForgetPasswordActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ForgetPasswordActivity.java
@@ -2,9 +2,14 @@ package com.cardinfolink.yunshouyin.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.api.QuickPayException;
+import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
+import com.cardinfolink.yunshouyin.model.ServerPacket;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 import com.cardinfolink.yunshouyin.ui.SettingInputItem;
 import com.cardinfolink.yunshouyin.view.HintDialog;
@@ -12,18 +17,22 @@ import com.cardinfolink.yunshouyin.view.HintDialog;
 /**
  * 这个是忘记密码的界面
  */
-public class ForgetPasswordActivity extends Activity {
-
+public class ForgetPasswordActivity extends BaseActivity {
+    private static final String TAG = "ForgetPasswordActivity";
     private SettingActionBarItem mActionBar;//标题栏，自定义的标题栏
 
     private SettingInputItem mEmail;
 
     private HintDialog mHintDialog;
 
+    private Button mCnfirm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        mHintDialog = new HintDialog(this, findViewById(R.id.hint_dialog));
 
         mActionBar = (SettingActionBarItem) findViewById(R.id.action_bar);
         mActionBar.setLeftTextOnclickListner(new View.OnClickListener() {
@@ -32,6 +41,7 @@ public class ForgetPasswordActivity extends Activity {
                 finish();
             }
         });
+
         mEmail = (SettingInputItem) findViewById(R.id.email);
         mEmail.setImageViewOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,8 +53,32 @@ public class ForgetPasswordActivity extends Activity {
             }
         });
 
-        mHintDialog = new HintDialog(this, findViewById(R.id.hint_dialog));
+        mCnfirm = (Button) findViewById(R.id.confirm);
 
+        mCnfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                forgetPassowd(); //忘记密码了
+            }
+        });
+    }
+
+    /**
+     * 点击 按钮 调用这里， 提交忘记密码的请求
+     */
+    private void forgetPassowd() {
+        Log.e(TAG, "     * 点击 按钮 调用这里， 提交忘记密码的请求 ");
+        quickPayService.forgetPasswordAsync(mEmail.getText(), new QuickPayCallbackListener<ServerPacket>() {
+            @Override
+            public void onSuccess(ServerPacket data) {
+
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+
+            }
+        });
     }
 
 }
