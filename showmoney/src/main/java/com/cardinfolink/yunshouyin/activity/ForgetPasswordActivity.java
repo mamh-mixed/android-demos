9d24e8eb94fe16383b14678b772d559f84ca1426
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-10 12:48:47 +0800

    showmoney: rename ResetPasswordActivity.java -> ForgetPasswordActivity.java
    重命名：
    showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ResetPasswordActivity.java
    -> showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ForgetPasswordActivity.java
    
    Change-Id: I5218813415beb2f977007e9c77d5113817fb00de
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ForgetPasswordActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ForgetPasswordActivity.java
@@ -2,18 +2,19 @@ package com.cardinfolink.yunshouyin.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 import com.cardinfolink.yunshouyin.ui.SettingInputItem;
 import com.cardinfolink.yunshouyin.view.ResetDialog;
 
-public class ResetPasswordActivity extends Activity {
+/**
+ * 这个是忘记密码的界面
+ */
+public class ForgetPasswordActivity extends Activity {
 
-    private SettingActionBarItem mResetPassword;
+    private SettingActionBarItem mActionBar;//标题栏，自定义的标题栏
 
     private SettingInputItem mEmail;
 
@@ -22,22 +23,22 @@ public class ResetPasswordActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset_password);
+        setContentView(R.layout.activity_forget_password);
 
-        mResetPassword = (SettingActionBarItem) findViewById(R.id.sabi_reset_password);
-        mResetPassword.setLeftTextOnclickListner(new View.OnClickListener() {
+        mActionBar = (SettingActionBarItem) findViewById(R.id.action_bar);
+        mActionBar.setLeftTextOnclickListner(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-        mEmail = (SettingInputItem) findViewById(R.id.reset_email);
+        mEmail = (SettingInputItem) findViewById(R.id.email);
         mEmail.setImageViewOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String title = getResources().getString(R.string.reset_forget_account_message);
-                String ok = getResources().getString(R.string.reset_i_known);
-                String cancel = getResources().getString(R.string.reset_cancel);
+                String title = getResources().getString(R.string.forget_account_message);
+                String ok = getResources().getString(R.string.forget_i_known);
+                String cancel = getResources().getString(R.string.forget_cancel);
                 mResetDialog.show(title, ok, cancel);
             }
         });
