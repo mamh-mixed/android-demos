2ca229d50752651e2277594b91ac38178f9993c9
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-10 14:44:18 +0800

    showmoney: improve ActivateDialog add more methods
    优化了ActivateDialog对话框，添加了其他方法
    
    Change-Id: Id5e7b6a33435ef64a796c2809b8468bd078d3a5f
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/ActivateDialog.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/ActivateDialog.java
@@ -21,69 +21,167 @@ import com.cardinfolink.yunshouyin.util.ShowMoneyApp;
 public class ActivateDialog {
     private Context mContext;
     private View dialogView;
-    private String mEmali;
+    private String mEmail;
+
+    private TextView mTitle;//对话框标题，在上边。
+    private TextView mBodyText;//对话框中间显示的文本。
+    private TextView mOk;//默认右边是确认按钮当然 按钮的行为可以自定义
+    private TextView mCancel;//默认左边是取消按钮.当然 按钮的行为可以自定义
+
+    private OnClickListener mOkOnClickListener;
+    private OnClickListener mCancelOnClickListener;
+
 
     public ActivateDialog(Context context, View view, String email) {
         mContext = context;
         dialogView = view;
-        mEmali = email;
-    }
+        mEmail = email;
 
-    public void show() {
-        TextView textView = (TextView) dialogView.findViewById(R.id.email);
-        textView.setText(ShowMoneyApp.getResString(R.string.activate_sentto_email) + mEmali + "");
-        dialogView.setVisibility(View.VISIBLE);
+        //对话框中间显示的文本。
+        String body = ShowMoneyApp.getResString(R.string.activate_sentto_email) + mEmail + "";
+        mBodyText = (TextView) dialogView.findViewById(R.id.email);
+        setBodyText(body);//对话框中间显示的文本。
+
+        mOk = (TextView) dialogView.findViewById(R.id.activate_dialog_ok);
+        mCancel = (TextView) dialogView.findViewById(R.id.activate_dialog_cancel);
+
         dialogView.setOnTouchListener(new OnTouchListener() {
-
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 return true;
             }
         });
-        dialogView.findViewById(R.id.activate_dialog_cancel).setOnClickListener(new OnClickListener() {
 
+        //设置取消按钮的行为，取消的话就跳转到 登录界面 了
+        setCancelOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 enterLoginActivity();
             }
         });
 
-        dialogView.findViewById(R.id.activate_dialog_ok).setOnClickListener(new OnClickListener() {
-
+        //设置确认按钮的行为
+        setOkOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = SessonData.loginUser.getUsername();
-                String password = SessonData.loginUser.getPassword();
-                if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
-                    //sessondata如果没有保存的情况下会出现。在loginActivity里没设置的话会出现此问题。
-                    //强制停止应用，再次登录会出现此问题。因为此时
-                    //sessondata是空的没有设置username和password。
-                    enterLoginActivity();
-                    return;
-                }
-                QuickPayService quick = ShowMoneyApp.getInstance().getQuickPayService();
-                quick.activateAsync(username, password, new QuickPayCallbackListener<Void>() {
-                    @Override
-                    public void onSuccess(Void data) {
-                        enterLoginActivity();
-                    }
-
-                    @Override
-                    public void onFailure(QuickPayException ex) {
-
-                    }
-                });
-
+                activate();// 激活用户
             }
         });
     }
 
+    public void show() {
+        dialogView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 激活用户账户的
+     */
+    private void activate() {
+        String username = SessonData.loginUser.getUsername();
+        String password = SessonData.loginUser.getPassword();
+        if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
+            //sessondata如果没有保存的情况下会出现。在loginActivity里没设置的话会出现此问题。
+            //强制停止应用，再次登录会出现此问题。因为此时
+            //sessondata是空的没有设置username和password。
+            enterLoginActivity();//进入新的界面了
+            return;
+        }
+        QuickPayService quick = ShowMoneyApp.getInstance().getQuickPayService();
+        quick.activateAsync(username, password, new QuickPayCallbackListener<Void>() {
+            @Override
+            public void onSuccess(Void data) {
+                enterLoginActivity();//进入新的界面了
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+                hide();//隐藏对话框了
+            }
+        });
+
+    }
+
     private void enterLoginActivity() {
-        dialogView.setVisibility(View.GONE);
+        hide();
+
         Intent intent = new Intent(mContext, LoginActivity.class);
         mContext.startActivity(intent);
         if (!(mContext instanceof LoginActivity)) {
             ((Activity) mContext).finish();
         }
     }
+
+
+    public void hide() {
+        dialogView.setVisibility(View.GONE);
+    }
+
+    /**
+     * 设置对话框的 显示的文本
+     *
+     * @param body
+     * @param ok
+     * @param cancel
+     */
+    public void setText(String body, String ok, String cancel) {
+        setBodyText(body);
+        setOkText(ok);
+        setCancelText(cancel);
+    }
+
+    public void setText(String title, String body, String ok, String cancel) {
+        setTitle(title);
+        setText(body, ok, cancel);
+    }
+
+    /**
+     * 右边 一般显示 “确认”按钮
+     * 设置对话框中间上边显示的文本
+     *
+     * @param title
+     */
+    public void setTitle(String title) {
+        mTitle.setText(title);
+    }
+
+    /**
+     * 右边 一般显示 “确认”按钮
+     * 设置对话框中间显示的文本
+     *
+     * @param title
+     */
+    public void setBodyText(String title) {
+        mBodyText.setText(title);
+    }
+
+    /**
+     * 设置对话框右边按钮显示的文本
+     *
+     * @param ok
+     */
+    public void setOkText(String ok) {
+        mOk.setText(ok);
+    }
+
+    /**
+     * zuo边一般显示 “取消” 按钮
+     * 设置对话框左边按钮显示的文本
+     *
+     * @param cancelText
+     */
+    public void setCancelText(String cancelText) {
+        mCancel.setText(cancelText);
+    }
+
+    //设置ok按钮的点击事件
+    public void setOkOnClickListener(OnClickListener l) {
+        mOkOnClickListener = l;
+        mOk.setOnClickListener(l);
+    }
+
+    public void setCancelOnClickListener(OnClickListener l) {
+        mCancelOnClickListener = l;
+        mCancel.setOnClickListener(l);
+    }
+
 }
