be8b7c260d13271b2ba0510ab48c9c0c6eb3e4d0
Arthur.WANG <fnghwsj@qq.com>
2015-12-11 16:28:34 +0900

    yun-complete bug修复以及后端权限控制

--- master/list.go
+++ master/list.go
@@ -25,9 +25,11 @@ func kvListHandle(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// 从当前session中取得agentCode，subAgentCode，groupCode， merId
+	sid, err := r.Cookie(SessionKey)
 	keyword := r.FormValue("keyword")
 
-	ret := listFind(data, keyword)
+	ret := listFind(sid.Value, data, keyword)
 	rdata, err := json.Marshal(ret)
 	if err != nil {
 		w.Write([]byte("mashal data error"))
@@ -37,7 +39,7 @@ func kvListHandle(w http.ResponseWriter, r *http.Request) {
 	w.Write(rdata)
 }
 
-func listFind(data []byte, keyword string) (result *model.ResultBody) {
+func listFind(sessionId string, data []byte, keyword string) (result *model.ResultBody) {
 	cond := new(mongo.KVListCondition)
 	err := json.Unmarshal(data, cond)
 	if err != nil {
@@ -48,6 +50,32 @@ func listFind(data []byte, keyword string) (result *model.ResultBody) {
 	if cond.ColName == "" {
 		log.Error("missing collection name")
 		return model.NewResultBody(3, "COLLECTION_NAME_IS_REQUIRED")
+	}
+
+	log.Debugf("sessionId is %s", sessionId)
+	// 查找session信息
+	session, err := mongo.SessionColl.Find(sessionId)
+	if err != nil {
+		log.Errorf("find session(%s) err: %s", sessionId, err)
+		return model.NewResultBody(1, "查询失败")
+	}
+	user := session.User
+
+	switch user.UserType {
+	case "admin":
+
+	case "genAdmin":
+
+	case "agent":
+		cond.FilterMap["agentCode"] = user.AgentCode
+	case "subAgent":
+		cond.FilterMap["subAgentCode"] = user.SubAgentCode
+	case "group":
+		cond.FilterMap["groupCode"] = user.GroupCode
+	case "merchant":
+		cond.FilterMap["merId"] = user.MerId
+	default:
+		return model.NewResultBody(3, "INVALID_USER_TYPE")
 	}
 
 	list, err := cond.Find(keyword)
