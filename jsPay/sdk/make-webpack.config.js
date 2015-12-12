b2517cba868521dbbdee83da26ea5ec4ddb912f8
Arthur.WANG <fnghwsj@qq.com>
2015-12-12 15:31:13 +0900

    微信支付sdk

---
+++ jsPay/sdk/make-webpack.config.js
@@ -0,0 +1,245 @@
+/*
+ * @Author: dmyang
+ * @Date:   2015-08-02 14:16:41
+ * @Last Modified by:   dmyang
+ * @Last Modified time: 2015-09-14 15:08:13
+ */
+
+'use strict';
+
+// @see http://christianalfoni.github.io/javascript/2014/12/13/did-you-know-webpack-and-react-is-awesome.html
+// @see https://github.com/webpack/react-starter/blob/master/make-webpack-config.js
+
+var path = require('path');
+var fs = require('fs');
+var yargs = require('yargs').argv;
+
+var webpack = require('webpack');
+var _ = require('lodash');
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+var StringReplacePlugin = require("string-replace-webpack-plugin");
+
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+
+var srcDir = path.resolve(process.cwd(), 'src');
+var build = 'assets/';
+var sourceMap = require('./src/sourcemap.json');
+
+var excludeFromStats = [
+	/node_modules[\\\/]/
+];
+
+var config = require('config'),
+	gatwayServer = '';
+
+if (process.env.NODE_ENV === 'production') {
+	gatwayServer = config['PRODUCT_REMOTE_URL'];
+} else {
+	gatwayServer = config['TEST_REMOTE_URL'];
+}
+
+function makeConf(options) {
+	options = options || {};
+
+	var urlMap = require('./config/default.json');
+
+	var debug = options.debug !== undefined ? options.debug : true;
+	var entries = genEntries();
+	var chunks = Object.keys(entries);
+
+	var serverUrl = '';
+	if (options.product) {
+		serverUrl = urlMap['PRODUCT_REMOTE_URL'];
+	} else {
+		serverUrl = urlMap['TEST_REMOTE_URL'];
+	}
+	var config = {
+		entry: entries,
+
+		output: {
+			// 在debug模式下，__build目录是虚拟的，webpack的dev server存储在内存里
+			path: path.resolve(debug ? '__build' : build),
+			filename: debug ? '[name].js' : 'js/[chunkhash:8].[name].min.js',
+			chunkFilename: debug ? '[chunkhash:8].chunk.js' : 'js/[chunkhash:8].chunk.min.js',
+			hotUpdateChunkFilename: debug ? '[id].[chunkhash:8].js' : 'js/[id].[chunkhash:8].min.js',
+			publicPath: debug ? '/__build/' : ''
+		},
+
+		resolve: {
+			root: [srcDir, './node_modules'],
+			// alias: sourceMap,
+			extensions: ['', '.js', '.css', '.scss', '.tpl', '.png', '.jpg']
+		},
+
+		resolveLoader: {
+			root: path.join(__dirname, 'node_modules')
+		},
+
+		module: {
+			// noParse: ['zepto'],
+			loaders: [{
+				test: /util\.js$/,
+				loader: StringReplacePlugin.replace({
+					replacements: [{
+						pattern: /IN_GOD_WE_TRUST/,
+						replacement: function(match, p1, offset, string) {
+							// console.log('******', match, '1', p1, '2', offset, '3', string);
+							return serverUrl;
+						}
+					}]
+				})
+			}, {
+				test: /\.(png|jpg)$/,
+				loader: 'url-loader?mimetype=image/png&limit=8192'
+			}, {
+				test: /\.handlebars$/,
+				loader: "handlebars-template-loader"
+			}, {
+				test: /\.(woff|eot|ttf)$/i,
+				loader: 'url?limit=10000&name=fonts/[hash:8].[name].[ext]'
+			}, {
+				test: /\.(tpl|ejs)$/,
+				loader: 'ejs'
+			}, {
+				test: /\.js$/,
+				exclude: /(node_modules|lib|util\.js)/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015']
+				}
+			}]
+		},
+
+		plugins: [
+			// new CommonsChunkPlugin({
+			// 	name: 'vendors',
+			// 	chunks: chunks,
+			// 	// Modules must be shared between all entries
+			// 	minChunks: chunks.length // 提取所有chunks共同依赖的模块
+			// }),
+			new OpenBrowserPlugin({
+				url: 'http://localhost:8080/webpack-dev-server/index.html'
+			}),
+			new webpack.DefinePlugin({
+				__GATEWATSERVER__: gatwayServer
+			}),
+			new StringReplacePlugin()
+			// new CommonsChunkPlugin({
+			// 	name: 'common-bc',
+			// 	chunks: ['vendors', 'b', 'c'],
+			// 	minChunks: 2
+			// })
+		],
+
+		devServer: {
+			historyApiFallback: true,
+			hot: true,
+			inline: true,
+			progress: true,
+			contentBase: build,
+			port: 8000
+		},
+	};
+
+	if (options.minify) {
+		// 发布到生产上的代码要压缩
+		config.plugins.push(
+			new webpack.optimize.UglifyJsPlugin()
+		);
+
+		config.devtool = 'source-map';
+	}
+
+	if (debug) {
+		// 开发阶段，css直接内嵌
+		var cssLoader = {
+			test: /\.css$/,
+			loader: 'style!css'
+		};
+		var sassLoader = {
+			test: /\.scss$/,
+			loader: 'style!css!sass'
+		};
+
+		config.module.loaders.push(cssLoader);
+		config.module.loaders.push(sassLoader);
+	} else {
+		// 编译阶段，css分离出来单独引入
+		var cssLoader = {
+			test: /\.css$/,
+			loader: ExtractTextPlugin.extract('style', 'css?minimize') // enable minimize
+		};
+		var sassLoader = {
+			test: /\.scss$/,
+			loader: ExtractTextPlugin.extract('style', 'css?minimize', 'sass')
+		};
+
+		config.module.loaders.push(cssLoader);
+		config.module.loaders.push(sassLoader);
+		config.plugins.push(
+			new ExtractTextPlugin('css/[contenthash:8].[name].min.css', {
+				// 当allChunks指定为false时，css loader必须指定怎么处理
+				// additional chunk所依赖的css，即指定`ExtractTextPlugin.extract()`
+				// 第一个参数`notExtractLoader`，一般是使用style-loader
+				// @see https://github.com/webpack/extract-text-webpack-plugin
+				allChunks: false
+			})
+		);
+
+		// 自动生成入口文件，入口js名必须和入口文件名相同
+		// 例如，a页的入口文件是a.html，那么在js目录下必须有一个a.js作为入口文件
+		var pages = fs.readdirSync(srcDir);
+
+		pages.forEach(function(filename) {
+			var m = filename.match(/(.+)\.html$/);
+
+			if (m) {
+				// @see https://github.com/kangax/html-minifier
+				var conf = {
+					template: path.resolve(srcDir, filename),
+					// @see https://github.com/kangax/html-minifier
+					// minify: {
+					//     collapseWhitespace: true,
+					//     removeComments: true
+					// },
+					filename: filename
+				};
+
+				if (m[1] in config.entry) {
+					conf.inject = 'body';
+					conf.chunks = ['vendors', m[1]];
+				}
+
+				config.plugins.push(new HtmlWebpackPlugin(conf));
+			}
+		});
+
+		// config.plugins.push(new UglifyJsPlugin());
+	}
+
+	return config;
+}
+
+function genEntries() {
+	var jsDir = path.resolve(srcDir, 'js');
+	var names = fs.readdirSync(jsDir);
+	var map = {};
+
+	names.forEach(function(name) {
+		// console.log(name);
+		var m = name.match(/(.+)\.js$/);
+		var entry = m ? m[1] : '';
+		var entryPath = entry ? path.resolve(jsDir, name) : '';
+
+		if (entry) map[entry] = entryPath;
+	});
+
+	// console.log(map);
+	return map;
+}
+
+module.exports = makeConf;
---
+++ jsPay/sdk/src/js/wxpayment.js
@@ -0,0 +1,220 @@
+'use strict';
+
+require('./vendor/util');
+var $ = require('webpack-zepto');
+
+const base64EncodeChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+const base64DecodeChars = new Array(　　-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 　　-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 　　-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63, 　　52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1, 　　-1, 　0, 　1, 　2, 　3, 4, 　5, 　6, 　7, 　8, 　9, 10, 11, 12, 13, 14, 　　15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, 　　-1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 　　41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+
+function base64decode(str) {　　
+	var c1, c2, c3, c4;　　
+	var i, len, out;　　
+	len = str.length;　　
+	i = 0;　　
+	out = '';　　
+	while (i < len) {
+		/* c1 */
+		do {　　
+			c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+		} while (i < len && c1 == -1);
+		if (c1 == -1)　　 break;
+		/* c2 */
+		do {　　
+			c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+		} while (i < len && c2 == -1);
+		if (c2 == -1)　　 break;
+		out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+		/* c3 */
+		do {　　
+			c3 = str.charCodeAt(i++) & 0xff;　　
+			if (c3 == 61)　 return out;　　
+			c3 = base64DecodeChars[c3];
+		} while (i < len && c3 == -1);
+		if (c3 == -1)　　 break;
+		out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+		/* c4 */
+		do {　　
+			c4 = str.charCodeAt(i++) & 0xff;　　
+			if (c4 == 61)　 return out;　　
+			c4 = base64DecodeChars[c4];
+		} while (i < len && c4 == -1);
+		if (c4 == -1)　　 break;
+		out += String.fromCharCode(((c3 & 0x03) << 6) | c4);　　
+	}　　
+	return out;
+}
+
+function utf8to16(str) {　　
+	var out, i, len, c;　　
+	var char2, char3;　　
+	out = '';　　
+	len = str.length;　　
+	i = 0;　　
+	while (i < len) {
+		c = str.charCodeAt(i++);
+		switch (c >> 4) {　
+			case 0:
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+				　　 // 0xxxxxxx
+				　　out += str.charAt(i - 1);　　
+				break;　
+			case 12:
+			case 13:
+				　　 // 110x xxxx　 10xx xxxx
+				　　char2 = str.charCodeAt(i++);　　
+				out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));　　
+				break;　
+			case 14:
+				　　 // 1110 xxxx　10xx xxxx　10xx xxxx
+				　　char2 = str.charCodeAt(i++);　　
+				char3 = str.charCodeAt(i++);　　
+				out += String.fromCharCode(((c & 0x0F) << 12) | 　　　　((char2 & 0x3F) << 6) | 　　　　((char3 & 0x3F) << 0));　　
+				break;
+		}　　
+	}　　
+	return out;
+}
+
+(function($) {
+	$(function() {
+
+		let code = Util.getUrlParam('code'),
+			orderData = Util.getUrlParam('data'),
+			url = Util.getServer() + '/scanpay/unified';
+
+		if (orderData === null) {
+			window.alert('没有订单信息');
+			return;
+		}
+
+		orderData = utf8to16(base64decode(orderData));
+		let order = JSON.parse(orderData),
+			frontUrl = order.frontUrl,
+			errorData =
+			'attach=' + order.attach +
+			'&txamt=' + order.txamt +
+			'&goodsInfo=' + order.goodsInfo +
+			'&orderCurrency=' + order.orderCurrency +
+			'&orderNum=' + order.orderNum +
+			'&state=1';
+
+
+		if (!code) {
+			errorData = errorData + '&errorDetail=URL拼接错误';
+			window.location.replace(frontUrl + '?' + errorData);
+			return;
+		}
+
+		let postData = {
+			orderNum: order.orderNum,
+			sign: order.sign,
+			txamt: order.txamt,
+			backUrl: order.backUrl,
+			mchntid: order.mchntid,
+			inscd: order.inscd,
+			txndir: 'Q',
+			goodsInfo: order.goodsInfo,
+			chcd: 'WXP',
+			busicd: 'JSZF',
+			needUserInfo: 'NO',
+			code: code,
+			attach: order.attach,
+			currency: order.orderCurrency
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: url,
+			async: false,
+			data: JSON.stringify(postData),
+			dataType: 'json',
+			success: (data) => {
+				let payJson = data.payjson;
+
+				if (!payJson) {
+					errorData += '&errorDetail=' + encodeURI(encodeURI(data.errorDetail));
+					window.location.replace(frontUrl + '?' + errorData);
+					return;
+				}
+
+				let aaa = payJson.config,
+					bbb = payJson.chooseWXPay;
+
+				let configAppId = aaa.appId,
+					configTimestamp = aaa.timestamp,
+					configNonceStr = aaa.nonceStr,
+					configSignature = aaa.signature;
+
+				let chooseWxPayTimestamp = bbb.timeStamp,
+					chooseWxPayNonceStr = bbb.nonceStr,
+					chooseWxPayPackage = bbb.package,
+					chooseWxPaySignType = bbb.signType,
+					chooseWxPayPaySign = bbb.paySign;
+
+				let post = [
+					'attach=' + data.attach,
+					'txamt=' + data.txamt,
+					'goodsInfo=' + orderData.goodsInfo,
+					'orderCurrency=' + orderData.orderCurrency,
+					'orderNum=' + data.orderNum
+				].join('&');
+
+				wx.config({
+					debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+					appId: configAppId, // 必填，公众号的唯一标识
+					timestamp: configTimestamp, // 必填，生成签名的时间戳
+					nonceStr: configNonceStr, // 必填，生成签名的随机串
+					signature: configSignature, // 必填，签名，见附录1
+					jsApiList: [
+							'checkJsApi',
+							'chooseWXPay'
+						] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+				});
+
+				wx.checkJsApi({
+					jsApiList: ['chooseWXPay'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+					success: function(res) {
+						alert(JSON.stringify(res));
+						// 以键值对的形式返回，可用的api值true，不可用为false
+						// 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+					}
+				});
+
+				wx.ready(() => {
+					wx.chooseWXPay({
+						timestamp: chooseWxPayTimestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+						nonceStr: chooseWxPayNonceStr, // 支付签名随机串，不长于 32 位
+						package: chooseWxPayPackage, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+						signType: chooseWxPaySignType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
+						paySign: chooseWxPayPaySign, // 支付签名
+						success: (res) => {
+							console.log('success');
+							post = post + '&state=0';
+							window.location.replace(frontUrl + '?' + post);
+						},
+						fail: () => {
+							console.log('fail');
+							post = post + '&state=1';
+							window.location.replace(frontUrl + '?' + post);
+						},
+						cancel: () => {
+							console.log('cancel');
+							post = post + '&state=-1';
+							window.location.replace(frontUrl + '?' + post);
+						}
+					});
+				});
+			},
+			error: (message) => {
+				console.log('error', JSON.stringify(message));
+				window.alert(JSON.stringify(message));
+			}
+		});
+	});
+})($);
