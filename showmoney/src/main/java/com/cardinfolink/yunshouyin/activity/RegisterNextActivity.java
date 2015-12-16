83d64c504d5e7e3044c6012ba24ee77297cd23a1
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-16 18:40:04 +0800

    showmoney: select province city bank use wheel view
    添加了选择省份 城市的功能，使用新的组件滚轮组件 wheel
    第一步： 滚动省份，然后城市会跟着更新。
    第二步： 添加银行 银行支行的滚轮
    
    Change-Id: I811d47324e681527652f362642f89dae36fcd66f
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterNextActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterNextActivity.java
@@ -1,15 +1,33 @@
 package com.cardinfolink.yunshouyin.activity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.api.QuickPayException;
+import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
+import com.cardinfolink.yunshouyin.model.Bank;
+import com.cardinfolink.yunshouyin.model.City;
+import com.cardinfolink.yunshouyin.model.Province;
+import com.cardinfolink.yunshouyin.model.SubBank;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 import com.cardinfolink.yunshouyin.ui.SettingClikcItem;
 import com.cardinfolink.yunshouyin.view.SelectDialog;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+import kankan.wheel.widget.OnWheelScrollListener;
+import kankan.wheel.widget.WheelView;
+import kankan.wheel.widget.adapters.AbstractWheelTextAdapter;
+
 
 public class RegisterNextActivity extends BaseActivity implements View.OnClickListener {
     private static final String TAG = "RegisterNextActivity";
@@ -23,6 +41,20 @@ public class RegisterNextActivity extends BaseActivity implements View.OnClickLi
 
     private SelectDialog selectDialog;
 
+    private List<Province> provinceList = new ArrayList<Province>();//省份的list
+    private List<City> cityList = new ArrayList<City>();//城市的list
+    private Map<Province, List<City>> provinceCityMap = new Hashtable<>();
+
+    private String mProvinceName;//保存设置的省份，调用银行时候会检查这个是否有值
+    private String mCityCode;
+
+    private List<Bank> bankList = new ArrayList<Bank>();//Bank的list
+    private List<SubBank> subbankList = new ArrayList<SubBank>();//SubBank 的list
+
+    private static final String SEPARATOR = "__";//分隔符
+
+    //这个maps的可以是 mCityCode + SEPARATOR + currentBank.getBankName()
+    private Map<String, List<SubBank>> bankSubBankMap = new Hashtable<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,14 +89,299 @@ public class RegisterNextActivity extends BaseActivity implements View.OnClickLi
                 break;
             case R.id.province_city:
                 Log.e(TAG, " onclick province_city");
-                selectDialog.show();
+                showProvinceCity();
                 break;
             case R.id.bank:
-
                 Log.e(TAG, " onclick bank");
+                showBankSubBank();
                 break;
         }
     }
 
+    /**
+     * 显示银行 银行支行 滚轮的界面
+     */
+    private void showBankSubBank() {
+        if (TextUtils.isEmpty(mProvinceName)) {
+            String msg = getResources().getString(R.string.alert_error_province_cannot_empty);
+            Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (TextUtils.isEmpty(mCityCode)) {
+            String msg = getResources().getString(R.string.alert_error_city_cannot_empty);
+            Toast.makeText(mContext, msg, Toast.LENGTH_SHORT).show();
+            return;
+        }
 
+        updateBankData();//去获取银行信息
+
+        selectDialog.addLeftScrollingListener(new BankOnWheelScrollListener());
+
+        selectDialog.setOkOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int bankIndex = selectDialog.getWheelLeftCurrentItem();
+                int subbankIndex = selectDialog.getWheelRightCurrentItem();
+
+                String bankName = bankList.get(bankIndex).getBankName();
+                mSetBank.setTitle(bankName);
+
+                List<SubBank> list = bankSubBankMap.get(mCityCode + SEPARATOR + bankName);
+                if (list != null && subbankIndex >= 0 && subbankIndex < list.size()) {
+                    String subbankName = list.get(subbankIndex).getBankName();
+                    mSetBank.setRightText(subbankName);
+                } else {
+                    mSetBank.setRightText("");
+                }
+
+                selectDialog.hide();
+            }
+        });
+
+        selectDialog.setCancelOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDialog.hide();
+            }
+        });
+
+        selectDialog.show();
+    }
+
+
+    /**
+     * 这里调用显示选择 省份城市 滚轮 的界面
+     */
+    private void showProvinceCity() {
+        updateProvinceData();
+
+        selectDialog.addLeftScrollingListener(new ProvinceOnWheelScrollListener());
+
+        //点击确定按钮 就把滚轮当前的 值设置到相应的位置
+        selectDialog.setOkOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int provinceIndex = selectDialog.getWheelLeftCurrentItem();
+                int cityIndex = selectDialog.getWheelRightCurrentItem();
+                String provinceName = provinceList.get(provinceIndex).getProvinceName();
+
+                //省份是肯定有的
+                mSetProvinceCity.setTitle(provinceName);
+                mProvinceName = provinceName;//再点击ok按钮的时候把省份名字保存一下
+
+                //城市码不一定刷新出来，然后用户就按确定了，这时候城市就是空的。存到一个hashtable里
+                List<City> list = provinceCityMap.get(provinceList.get(provinceIndex));
+                if (list != null && cityIndex >= 0 && cityIndex < list.size()) {
+                    String cityName = list.get(cityIndex).getCityName();
+                    mCityCode = list.get(cityIndex).getCityCode();//保存一下citycode，之后获取银行信息的时候会用到
+                    mSetProvinceCity.setRightText(cityName);
+                } else {
+                    mSetProvinceCity.setRightText("");
+                    mCityCode = null;//这里设置为空
+                }
+
+                mSetBank.setTitle(getResources().getString(R.string.register_bank_branch_bank));
+                mSetBank.setRightText("");
+                selectDialog.hide();
+            }
+        });
+
+        selectDialog.setCancelOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectDialog.hide();
+            }
+        });
+
+        //调用显示对话框
+        selectDialog.show();
+    }
+
+    private void updateProvinceData() {
+        //这里调用 获取省份的信息。成功之后调用获取城市的信息。
+        bankDataService.getProvince(new QuickPayCallbackListener<List<Province>>() {
+            @Override
+            public void onSuccess(List<Province> data) {
+                provinceList.clear();
+                provinceList.addAll(data);
+
+                selectDialog.setWheelLeftAdapter(new RegisterWheelAdapter<Province>(mContext, provinceList));
+
+                selectDialog.setWheelLeftCurrentItem(0);
+                Province currentProvince = data.get(0);
+
+                //调用获取城市信息，并或刷新相应的data和view
+                updateCityData(currentProvince);
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+
+            }
+        });
+    }
+
+
+    /**
+     * 调用更新城市的方法，传人一个省份，得到这个省份下面所有的城市
+     */
+    private void updateCityData(final Province currentProvince) {
+        bankDataService.getCity(currentProvince.getProvinceName(), new QuickPayCallbackListener<List<City>>() {
+            @Override
+            public void onSuccess(List<City> data) {
+                cityList.clear();
+                cityList.addAll(data);
+                provinceCityMap.put(currentProvince, data);
+                selectDialog.setWheelRigthAdapter(new RegisterWheelAdapter<City>(mContext, cityList));
+                selectDialog.setWheelRightCurrentItem(0);
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+
+            }
+        });
+    }
+
+
+    private void updateBankData() {
+        bankDataService.getBank(new QuickPayCallbackListener<List<Bank>>() {
+            @Override
+            public void onSuccess(List<Bank> data) {
+                bankList.clear();
+                bankList.addAll(data);
+                selectDialog.setWheelLeftAdapter(new RegisterWheelAdapter<Bank>(mContext, bankList));
+
+                selectDialog.setWheelLeftCurrentItem(0);
+                Bank currentBank = data.get(0);
+
+                //调用update subbank的方法
+                updateSubBankData(currentBank);
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+
+            }
+        });
+    }
+
+    private void updateSubBankData(final Bank currentBank) {
+        bankDataService.getBranchBank(mCityCode, currentBank.getId(), new QuickPayCallbackListener<List<SubBank>>() {
+            @Override
+            public void onSuccess(List<SubBank> data) {
+                subbankList.clear();
+                subbankList.addAll(data);
+                bankSubBankMap.put(mCityCode + SEPARATOR + currentBank.getBankName(), data);//存入map字典中
+                selectDialog.setWheelRigthAdapter(new RegisterWheelAdapter<SubBank>(mContext, subbankList));
+                selectDialog.setWheelRightCurrentItem(0);
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+
+            }
+        });
+    }
+
+    /**
+     * 左边Province 滚轮 滑动 事件 的监听类
+     */
+    private class ProvinceOnWheelScrollListener implements OnWheelScrollListener {
+        @Override
+        public void onScrollingStarted(WheelView wheel) {
+            cityList.clear();//开始滚动就清除城市列表
+        }
+
+        @Override
+        public void onScrollingFinished(WheelView wheel) {
+            //滚动结束调用跟新城市的方法
+            Province currentProvince = provinceList.get(wheel.getCurrentItem());
+
+            if (provinceCityMap != null && provinceCityMap.get(currentProvince) != null) {
+                selectDialog.setWheelRigthAdapter(new RegisterWheelAdapter<City>(mContext, provinceCityMap.get(currentProvince)));
+            } else {
+                updateCityData(currentProvince);
+            }
+        }
+    }
+
+    /**
+     * 当左边显示的是银行信息的时候会添加这个监听事件
+     * 左边bak 滚轮 滑动 事件 的监听类
+     */
+    private class BankOnWheelScrollListener implements OnWheelScrollListener {
+        @Override
+        public void onScrollingStarted(WheelView wheel) {
+            subbankList.clear();
+        }
+
+        @Override
+        public void onScrollingFinished(WheelView wheel) {
+            Bank currentBank = bankList.get(wheel.getCurrentItem());
+            String mapKey = mCityCode + SEPARATOR + currentBank.getBankName();
+            if (bankSubBankMap != null && bankSubBankMap.get(mapKey) != null) {
+                selectDialog.setWheelRigthAdapter(new RegisterWheelAdapter<SubBank>(mContext, bankSubBankMap.get(mapKey)));
+            } else {
+                updateSubBankData(currentBank);
+            }
+        }
+    }
+
+    /**
+     * 滚轮组件的适配器，泛型的，里面存了一个 list，构建方法的时候传人这个list
+     *
+     * @param <T>
+     */
+    private class RegisterWheelAdapter<T> extends AbstractWheelTextAdapter {
+
+        // items
+        private List<T> items;
+
+        /**
+         * Constructor
+         *
+         * @param context the current context
+         * @param items   the items
+         */
+        public RegisterWheelAdapter(Context context, List<T> items) {
+            super(context);
+            this.items = items;
+        }
+
+        @Override
+        public CharSequence getItemText(int index) {
+            if (index >= 0 && index < items.size()) {
+                T item = items.get(index);
+                if (item instanceof CharSequence) {
+                    return (CharSequence) item;
+                }
+                if (item instanceof Province) {
+                    return ((Province) item).getProvinceName();
+                }
+                if (item instanceof City) {
+                    return ((City) item).getCityName();
+                }
+                if (item instanceof Bank) {
+                    return ((Bank) item).getBankName();
+                }
+                if (item instanceof SubBank) {
+                    return ((SubBank) item).getBankName();
+                }
+                return item.toString();
+            }
+            return null;
+        }
+
+        @Override
+        public int getItemsCount() {
+            if (items != null) {
+                return items.size();
+            } else {
+                return 0;
+            }
+        }
+    }
 }
