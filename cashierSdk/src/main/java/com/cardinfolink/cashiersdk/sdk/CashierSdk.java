2347df5e2e85a7badeb01ce372abca74b6433021
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-18 10:54:31 +0800

    cashierSdk: add cancel order method in CashierSdk.java
    SDK里面添加了 取消订单 方法
    
    Change-Id: I01d8e6067ce99c09de5bf134e2fcab4623c6c151
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- cashierSdk/src/main/java/com/cardinfolink/cashiersdk/sdk/CashierSdk.java
+++ cashierSdk/src/main/java/com/cardinfolink/cashiersdk/sdk/CashierSdk.java
@@ -41,6 +41,14 @@ public class CashierSdk {
     }
 
 
+    /**
+     * 3.1. 下单支付
+     * 下单支付接口适用于获取支付宝客户端的“付款码”或微信“刷卡”的条码号，
+     * 并通过该接口上送此条码号（scanCodeId字段）进行支付。
+     *
+     * @param orderData
+     * @param listener
+     */
     public static void startPay(OrderData orderData, final CashierListener listener) {
 
         String str = orderData.txamt;
@@ -68,7 +76,7 @@ public class CashierSdk {
                     map.remove("sign");
 
                     String veriSign = ParamsUtil.getSign(MapUtil.getSignString(map), mInitData.signKey, "SHA-1");
-                    Log.i(TAG,"veriSign: " + veriSign);
+                    Log.i(TAG, "veriSign: " + veriSign);
                     if (sign.equals(veriSign)) {
                         ResultData resultData = MapUtil.getResultData(map);
                         listener.onResult(resultData);
@@ -90,6 +98,14 @@ public class CashierSdk {
     }
 
 
+    /**
+     * 3.2. 预下单
+     * 预下单接口为一笔指定金额的交易生成一个url（qrcode字段），将此url直接转换成二维码，
+     * 使用支付宝或微信的扫一扫功能即可在手机端完成支付。可通过查询订单接口确定此交易的交易状态。
+     *
+     * @param orderData
+     * @param listener
+     */
     public static void startPrePay(OrderData orderData, final CashierListener listener) {
         String str = orderData.txamt;
         if (str != null && orderData.currency != null) {
@@ -134,6 +150,15 @@ public class CashierSdk {
     }
 
 
+    /**
+     * 3.3. 查询订单
+     * 此接口用于查询下单支付和预下单交易的交易状态，当下单支付或预下单
+     * 支付返回09：交易处理中或98：交易超时的应答码或者接入方没有收到应答时
+     * （网络原因等），推荐需要调用此接口，以明确订单状态。
+     *
+     * @param orderData
+     * @param listener
+     */
     public static void startQy(OrderData orderData, final CashierListener listener) {
         String str = orderData.txamt;
         if (str != null && orderData.currency != null) {
@@ -177,6 +202,13 @@ public class CashierSdk {
     }
 
 
+    /**
+     * * 3.4. 撤销
+     * 撤销下单支付或预下单交易
+     *
+     * @param orderData
+     * @param listener
+     */
     public static void startVoid(OrderData orderData, final CashierListener listener) {
 
         String str = orderData.txamt;
@@ -221,6 +253,13 @@ public class CashierSdk {
         });
     }
 
+    /**
+     * * 3.5. 退款
+     * 撤销下单支付或预下单交易
+     *
+     * @param orderData
+     * @param listener
+     */
     public static void startRefd(OrderData orderData, final CashierListener listener) {
         String str = orderData.txamt;
         if (str != null && orderData.currency != null) {
@@ -265,6 +304,49 @@ public class CashierSdk {
         });
     }
 
+
+    /**
+     * 3.6  取消订单
+     * 对于未成功付款的订单进行取消，则关闭交易，使用户后期不能支付成功；
+     * 对于成功付款的订单进行取消，系统将订单金额返还给用户，相当于对此交易做撤销。
+     *
+     * @param orderData
+     * @param listener
+     */
+    public static void startCanc(OrderData orderData, final CashierListener listener) {
+        CommunicationUtil.sendDataToServer(ParamsUtil.getCanc(mInitData, orderData), new CommunicationListener() {
+
+            @Override
+            public void onResult(String result) {
+                Map<String, Object> map = MapUtil.getMapForJson(result);
+                String sign = (String) map.get("sign");
+                if (sign != null) {
+                    map.remove("sign");
+                    String veriSign = ParamsUtil.getSign(MapUtil.getSignString(map), mInitData.signKey, "SHA-1");
+                    Log.i(TAG, "veriSign: " + veriSign);
+                    if (sign.equals(veriSign)) {
+                        ResultData resultData = MapUtil.getResultData(map);
+                        listener.onResult(resultData);
+                    } else {
+                        listener.onError(3);
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int error) {
+                listener.onError(error);
+            }
+        });
+    }
+
+    /**
+     * 3.7卡券核销
+     * 对上送的卡券号进行核销
+     *
+     * @param orderData
+     * @param listener
+     */
     public static void startVeri(OrderData orderData, final CashierListener listener) {
 
         String str = orderData.txamt;
