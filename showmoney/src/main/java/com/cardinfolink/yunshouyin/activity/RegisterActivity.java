f0deef54ec55b14ad990435d31c89673beecbcdd
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-19 17:27:17 +0800

    showmoney: add register success goto RegisterActivate view
    这里重用了激活对话框
    这里重新实现右边确定按钮的点击事件。默认是进入login界面，这里要进入RegisterActivateActivity界面
    当点击注册按钮的时候弹出一个对话框
        左边显示 "稍后激活" 右边显示 "已激活"
    点击 "已激活" 跳转到 RegisterActivateActivity 界面
    在RegisterActivateActivity里面判断一下是否真的激活了。
    是的话就进入RegisterNext页面了。做进一步的完善信息。
    
    点击 "稍后激活" 会跳转到 登录 界面
    
    Change-Id: Ic13436b73538bc592682604ca1ed8915fde955bc
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.cardinfolink.yunshouyin.activity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -100,17 +101,32 @@ public class RegisterActivity extends BaseActivity {
                 User user = new User();
                 user.setUsername(username);
                 user.setPassword(password);
-                SaveData.setUser(mContext, user);
+                SaveData.setUser(mContext, user);//保存到本地文件
 
-                SessonData.loginUser.setUsername(username);
-                SessonData.loginUser.setPassword(password);
+                //这里保存到sessondata中，下面一个activity中会用到
+                SessonData.loginUser.setUsername(username);//保存到sessondata里面
+                SessonData.loginUser.setPassword(password);//保存到sessondata里面
 
                 mLoadingDialog.endLoading();
+
+                //走到这里是注册成功了 ，就是注册的第一步成功了，之后会进入完善信息的页面。
+                //如果注册成功 点击 了 已激活 按钮 就跳转到 注册的第二个页面。
                 View activateView = findViewById(R.id.activate_dialog);
                 ActivateDialog activateDialog = new ActivateDialog(mContext, activateView, username);
-                activateDialog.setBodyText(getResources().getString(R.string.activate_message)+ username);
+                activateDialog.setBodyText(getResources().getString(R.string.activate_message) + username);
                 activateDialog.setCancelText(getResources().getString(R.string.activate_after));
                 activateDialog.setOkText(getResources().getString(R.string.activate_had_activated));
+                //这里重新实现右边确定按钮的点击事件。默认是进入login界面，这里要进入RegisterActivateActivity界面
+                activateDialog.setOkOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(RegisterActivity.this, RegisterActivateActivity.class);
+                        startActivity(intent);//跳转到RegisterActivateActivity页面
+                        //在RegisterActivateActivity里面判断一下是否真的激活了。是的话就进入RegisterNext页面了。
+                        finish();//结束当前的页面
+                    }
+                });
+
                 activateDialog.show();//显示激活对话框，这里文本都是自定义的。
             }
 
