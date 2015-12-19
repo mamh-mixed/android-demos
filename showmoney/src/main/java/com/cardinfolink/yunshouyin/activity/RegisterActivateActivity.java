01b848f6fbaee4aa9be59caa97a6ada51d1b7a1a
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-19 18:43:57 +0800

    showmoney: add check account is activate methdod
    在RegisterActivateActivity.java里加入了检查账号是否激活状态
    的方法，没有激活就不会跳转 完善信息的页面
    激活了 才能跳转完善信息的 页面
    
    Change-Id: Icc76a8841b9096dc5b1356198db6b292c3bbb28b
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivateActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterActivateActivity.java
@@ -1,18 +1,28 @@
 package com.cardinfolink.yunshouyin.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.api.QuickPayException;
+import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
+import com.cardinfolink.yunshouyin.data.SessonData;
+import com.cardinfolink.yunshouyin.data.User;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 
-public class RegisterActivateActivity extends Activity {
+public class RegisterActivateActivity extends BaseActivity {
     private SettingActionBarItem mActionBar;
     private Button mNext;
+    private TextView mActivateMessage;//显示账号是否激活的一些提示信息的
+    private TextView mAccount;//显示账号
+
+    private boolean isActivate = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +37,64 @@ public class RegisterActivateActivity extends Activity {
             }
         });
 
+
+        mAccount = (TextView) findViewById(R.id.account);
+        //这里得到上一个activity中（register acitvity）设置的账号
+        final String username = SessonData.loginUser.getUsername();
+        final String password = SessonData.loginUser.getPassword();
+        mAccount.setText(username);
+
         mNext = (Button) findViewById(R.id.btnnext);
         mNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivateActivity.this, RegisterNextActivity.class);
-                startActivity(intent);
+                if (isActivate) {
+                    Intent intent = new Intent(RegisterActivateActivity.this, RegisterNextActivity.class);
+                    startActivity(intent);
+                } else {
+                    checkActivate(username, password);
+                }
+            }
+        });
+
+        mActivateMessage = (TextView) findViewById(R.id.activate_message);
+
+
+        checkActivate(username, password);
+    }
+
+    /**
+     * 检查用户是否是激活的
+     *
+     * @param username
+     * @param password
+     */
+    private void checkActivate(String username, String password) {
+        if (isActivate) {
+            //这里先判断一下是不是激活的，是话就不执行了
+            return;
+        }
+
+        quickPayService.loginAsync(username, password, new QuickPayCallbackListener<User>() {
+            @Override
+            public void onSuccess(User data) {
+                //这里不太可能调到这里吧，刚刚注册的一个账号不至于能登录成功的。信息没有完善应该是登录失败的。
+                //这里暂时不管。。。。。
+                if (TextUtils.isEmpty(SessonData.loginUser.getClientid())) {
+                    isActivate = true;
+                    // clientid为空,跳转到完善信息页面
+                    mNext.setText(getResources().getString(R.string.activate_i_had_activated));
+                    mActivateMessage.setText(getResources().getString(R.string.activate_your_had_activate));
+                }
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+                String errorCode = ex.getErrorCode();
+                String errorMsg = ex.getErrorMsg();
+                Toast.makeText(getApplicationContext(), errorMsg, Toast.LENGTH_SHORT).show();
+                //其他出错的状态怎么设置这个？？？
+                isActivate = false;
             }
         });
     }
