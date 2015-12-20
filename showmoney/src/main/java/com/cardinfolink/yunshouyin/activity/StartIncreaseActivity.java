814e3dcd00ee01ce0fdaa988229c943c4cb7ba90
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-20 15:15:25 +0800

    showmoney: add help image to show some message
    添加了 提示用户信息的一个帮助的图片
    点击图片会弹出一个对话框给用户一些
    提示信息
    个体工商户 所需材料
    法人身份证
    营业执照
    
    企业商户 所需材料
    法人身份证
    企业营业执照
    税务登记证
    组织机构代码证
    
    Change-Id: I1402e21af9a2808db582d944c2cf95bca8fae56a
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/StartIncreaseActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/StartIncreaseActivity.java
@@ -1,17 +1,21 @@
 package com.cardinfolink.yunshouyin.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
+import com.cardinfolink.yunshouyin.view.HintDialog;
 
 /**
  * 提升限额 页面，也是 免费升级页面
  */
-public class StartIncreaseActivity extends Activity {
+public class StartIncreaseActivity extends BaseActivity implements View.OnClickListener {
     private SettingActionBarItem mActionBar;// action bar
+    private ImageView mHelp;
+    private HintDialog mHintDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,24 @@ public class StartIncreaseActivity extends Activity {
                 finish();
             }
         });
+
+        mHelp = (ImageView) findViewById(R.id.iv_help);
+        mHelp.setOnClickListener(this);
+
+        mHintDialog = new HintDialog(this, findViewById(R.id.hint_dialog));
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_help:
+                //显示一个提示信息的对话框
+                mHintDialog.show();
+                mHintDialog.setTitle("个体工商户 所需材料\n法人身份证\n营业执照\n\n企业商户 所需材料\n法人身份证\n企业营业执照\n税务登记证\n组织机构代码证");
+                mHintDialog.setOkText("我知道了");
+                break;
+        }
 
 
     }
