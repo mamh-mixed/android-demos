63597906d43b42b61c4a5ea622be008ca80e0773
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-20 12:04:02 +0800

    showmoney: add new activity RegisterFinalActivity.java
    添加了一个新的activity界面，完善信息成功支行跳转到这个界面
    
    Change-Id: Ie3515e43178e815edc29d098348d130076179812
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/RegisterFinalActivity.java
@@ -0,0 +1,16 @@
+package com.cardinfolink.yunshouyin.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cardinfolink.yunshouyin.R;
+
+public class RegisterFinalActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_final);
+    }
+
+}
