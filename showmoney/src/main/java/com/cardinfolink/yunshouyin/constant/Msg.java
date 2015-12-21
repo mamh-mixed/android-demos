b5e5076bacaed49b1accb2e65e4ceaebca3ccf13
Deng Jia <charles.deng@cardinfolink.com>
2015-12-21 10:06:57 +0800

    showmoney:add MSG  three message for close bill
    Change-Id: I74e739daf75f621e61d8ae9f2319d3e5e9e48d2b
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/Msg.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/Msg.java
@@ -44,4 +44,9 @@ public class Msg {
 
     public static final int MSG_FROM_CLIENT_ALERT_OK = 471;
 
+    //关单用到的
+    public static final int MSG_FROM_SERVER_CLOSEBILL_SUCCESS = 500;
+    public static final int MSG_FROM_SERVER_CLOSEBILL_DOING = 501;
+    public static final int MSG_FROM_SERVER_CLOSEBILL_FAIL = 502;
+
 }
