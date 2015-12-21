823e9fce863d097596749f0ef7477d0639d9afb4
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-21 18:30:28 +0800

    showmoeny: add person and company show different view
    区分开来 个体商户 和 企业商户 界面显示不同的内容
    
    Change-Id: I82ab72a2daf93e722886e973c98dd5c3772daeaf
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/LimitIncreaseActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/LimitIncreaseActivity.java
@@ -1,20 +1,43 @@
 package com.cardinfolink.yunshouyin.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingClikcItem;
 
 
 /**
  * 提升限额的界面,用于上传图片，填写店铺名称
  */
 public class LimitIncreaseActivity extends BaseActivity {
+    private static final String TYPE = "type";
+    private static final int PERSON = 0;
+    private static final int COMPANY = 1;
+    private SettingClikcItem mTax;
+    private SettingClikcItem mOrganization;
+    private TextView mMessage;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_limit_increase);
 
+        mTax = (SettingClikcItem) findViewById(R.id.tax);
+        mOrganization = (SettingClikcItem) findViewById(R.id.organization);
+        mMessage = (TextView) findViewById(R.id.increase_message);
+
+        int type = getIntent().getIntExtra(TYPE, PERSON);
+        if (PERSON == type) {
+            mMessage.setText(getString(R.string.limit_increase_message));
+            mTax.setVisibility(View.GONE);
+            mOrganization.setVisibility(View.GONE);
+        } else if (COMPANY == type) {
+            mMessage.setText(getString(R.string.limit_increase_message1));
+            mTax.setVisibility(View.VISIBLE);
+            mOrganization.setVisibility(View.VISIBLE);
+        }
     }
 
 
