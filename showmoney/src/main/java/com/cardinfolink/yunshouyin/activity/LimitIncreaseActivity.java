9e993472dcc024d0eb853eb75a38416b937b1433
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-22 14:05:23 +0800

    showmoney: add select picture feature in LimitIncreaseActivity.java
    添加了选择照片的功能
    
    Change-Id: I9bfb3a1f4fc99bc7a670a8169b783ef6269586d4
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/LimitIncreaseActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/LimitIncreaseActivity.java
@@ -1,6 +1,10 @@
 package com.cardinfolink.yunshouyin.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,12 +13,15 @@ import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 import com.cardinfolink.yunshouyin.ui.SettingClikcItem;
 import com.cardinfolink.yunshouyin.ui.SettingInputItem;
+import com.cardinfolink.yunshouyin.view.SelectPicDialog;
 
 
 /**
  * 提升限额的界面,用于上传图片，填写店铺名称
  */
 public class LimitIncreaseActivity extends BaseActivity implements View.OnClickListener {
+    private static final String TAG = "LimitIncreaseActivity";
+
     private static final String TYPE = "type";
     private static final int PERSON = 0;
     private static final int COMPANY = 1;
@@ -35,11 +42,18 @@ public class LimitIncreaseActivity extends BaseActivity implements View.OnClickL
 
     private SettingActionBarItem mActionBar;
 
+    private SelectPicDialog selectPic;
+    private static final int PICK_ID_P_REQUEST = 1;//身份证 正面
+    private static final int PICK_ID_N_REQUEST = 2;//身份证 反面
+    private static final int PICK_B_REQUEST = 3;//营业执照
+    private static final int PICK_TAX_REQUEST = 4;//税务
+    private static final int PICK_O_REQUEST = 5;//组织机构
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_limit_increase);
 
-        mActionBar= (SettingActionBarItem) findViewById(R.id.action_bar);
+        mActionBar = (SettingActionBarItem) findViewById(R.id.action_bar);
         mActionBar.setLeftTextOnclickListner(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -81,30 +95,138 @@ public class LimitIncreaseActivity extends BaseActivity implements View.OnClickL
         mTax.setOnClickListener(this);//税务
         mOrganization.setOnClickListener(this);//组织机构
         mFinish.setOnClickListener(this);//完成按钮
+
+        selectPic = new SelectPicDialog(this, findViewById(R.id.select_pic_dialog));
+
+
     }
 
 
     @Override
     public void onClick(View v) {
+        final Intent intent = new Intent();
+        // 开启Pictures画面Type设定为image
+        intent.setType("image/*");
+        // 使用Intent.ACTION_GET_CONTENT这个Action
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
         switch (v.getId()) {
             case R.id.id_card_positive:
                 //身份证 正面
+                selectPic.setPickPhotoOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //取得相片后返回本画面
+                        String title = getResources().getString(R.string.limit_increase_card_positive);
+                        startActivityForResult(Intent.createChooser(intent, title), PICK_ID_P_REQUEST);
+                        selectPic.hide();
+                    }
+                });
+                selectPic.show();
                 break;
             case R.id.id_card_negaitive:
                 //身份证 反面
+                selectPic.setPickPhotoOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //取得相片后返回本画面
+                        String title = getResources().getString(R.string.limit_increase_card_negaitive);
+                        startActivityForResult(Intent.createChooser(intent, title), PICK_ID_N_REQUEST);
+                        selectPic.hide();
+                    }
+                });
+                selectPic.show();
                 break;
             case R.id.business:
                 //营业执照
+                selectPic.setPickPhotoOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //取得相片后返回本画面
+                        String title = getResources().getString(R.string.limit_increase_business_licence);
+                        startActivityForResult(Intent.createChooser(intent, title), PICK_B_REQUEST);
+                        selectPic.hide();
+                    }
+                });
+                selectPic.show();
                 break;
             case R.id.tax:
                 //税务
+                selectPic.setPickPhotoOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //取得相片后返回本画面
+                        String title = getResources().getString(R.string.limit_increase_tax);
+                        startActivityForResult(Intent.createChooser(intent, title), PICK_TAX_REQUEST);
+                        selectPic.hide();
+                    }
+                });
+                selectPic.show();
                 break;
             case R.id.organization:
                 //组织机构
+                selectPic.setPickPhotoOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //取得相片后返回本画面
+                        String title = getResources().getString(R.string.limit_increase_organization);
+                        startActivityForResult(Intent.createChooser(intent, title), PICK_O_REQUEST);
+                        selectPic.hide();
+                    }
+                });
+                selectPic.show();
                 break;
             case R.id.btnfinish:
                 break;
         }
 
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e(TAG, "===========================onActivityResult+++++++++++++++++++++++++++" + resultCode);
+        String selectedStr = getString(R.string.limit_increase_selected);//已选择
+        String unselectedStr = getString(R.string.limit_increase_update_licence);//上传证件,相当于提示 用户 未选择图片
+        switch (requestCode) {
+            case PICK_ID_P_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    mCardPositive.setRightText(selectedStr);
+                } else {
+                    mCardPositive.setRightText(unselectedStr);
+                }
+                break;
+            case PICK_ID_N_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    mCardNegative.setRightText(selectedStr);
+                } else {
+                    mCardNegative.setRightText(unselectedStr);
+                }
+                break;
+            case PICK_B_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    mBusiness.setRightText(selectedStr);
+                } else {
+                    mBusiness.setRightText(unselectedStr);
+                }
+                break;
+            case PICK_TAX_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    mTax.setRightText(selectedStr);
+                } else {
+                    mTax.setRightText(unselectedStr);
+                }
+                break;
+            case PICK_O_REQUEST:
+                if (resultCode == RESULT_OK) {
+                    mOrganization.setRightText(selectedStr);
+                } else {
+                    mOrganization.setRightText(unselectedStr);
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
