fc4c2278ce07530d78ddff9ca686925bb9848eac
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-23 17:28:09 +0800

    showmoney: add new acitvity FinalIncreaseActivity
    Change-Id: I6802ec18843546b812de4209a8e850011b7a0911
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/FinalIncreaseActivity.java
@@ -0,0 +1,16 @@
+package com.cardinfolink.yunshouyin.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.cardinfolink.yunshouyin.R;
+
+public class FinalIncreaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_increase);
+    }
+
+}
