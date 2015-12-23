049d24a3e063a4162c29f3bfe5461e2a9389d541
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-23 10:01:49 +0800

    showmoney: add onProgress when upload photo
    添加了上传照片时候 的 onProgress的方法
    
    Change-Id: I4e0dcda99237a3f9fd8b0533dce826aa64bba30d
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/core/QiniuCallbackListener.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/core/QiniuCallbackListener.java
@@ -1,7 +1,13 @@
 package com.cardinfolink.yunshouyin.core;
 
+import com.qiniu.android.http.ResponseInfo;
+
+import org.json.JSONObject;
+
 public interface QiniuCallbackListener {
-    void onComplete();
+    void onComplete(String key, ResponseInfo info, JSONObject response);
 
     void onFailure(Exception ex);
+
+    void onProgress(String key, double percent);
 }
