2f87630abedcc46be2fbaf19fa1c391db5195b46
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-22 23:12:39 -0800

    Revert "showmoney: when upload complete will return mQiniuKeyPattern"
    This reverts commit 4eb7edbaaf86c1204e682d4f7448345b0008dadf.
    
    Change-Id: I819cce9280d5d4f50cb7fa088518dca0d4678742

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/core/QiniuCallbackListener.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/core/QiniuCallbackListener.java
@@ -1,9 +1,12 @@
 package com.cardinfolink.yunshouyin.core;
 
 import com.cardinfolink.yunshouyin.api.QuickPayException;
+import com.qiniu.android.http.ResponseInfo;
+
+import org.json.JSONObject;
 
 public interface QiniuCallbackListener {
-    void onComplete(String pattern, int photoKey);
+    void onComplete(String key, ResponseInfo info, JSONObject response, int photoKey);
 
     void onFailure(QuickPayException ex, int photoKey);
 
