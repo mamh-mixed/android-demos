15f897ffbe68c724f28b292262bd06ccebcda7f1
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-24 11:27:59 +0800

    cashierSdk: remove log in sdk in CommunicationUtil.java
    Change-Id: Ic2606a4a818406d87a67ba6facaa637b17fabccf
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- cashierSdk/src/main/java/com/cardinfolink/cashiersdk/data/SaveData.java
+++ cashierSdk/src/main/java/com/cardinfolink/cashiersdk/data/SaveData.java
@@ -10,8 +10,7 @@ public class SaveData {
     private static Server mServer = new Server();
 
     public static Server getmServer(Context context) {
-        SharedPreferences mySharedPreferences = context.getSharedPreferences("savedata_sdk",
-                Activity.MODE_PRIVATE);
+        SharedPreferences mySharedPreferences = context.getSharedPreferences("savedata_sdk", Activity.MODE_PRIVATE);
         mServer.setHost(mySharedPreferences.getString("host", "211.147.72.70"));
         mServer.setPort(mySharedPreferences.getString("port", "10008"));
         return mServer;
@@ -19,8 +18,7 @@ public class SaveData {
 
     public static void setmServer(Context context, Server mServer) {
         SaveData.mServer = mServer;
-        SharedPreferences mySharedPreferences = context.getSharedPreferences("savedata_sdk",
-                Activity.MODE_PRIVATE);
+        SharedPreferences mySharedPreferences = context.getSharedPreferences("savedata_sdk", Activity.MODE_PRIVATE);
         SharedPreferences.Editor editor = mySharedPreferences.edit();
         editor.putString("host", mServer.getHost());
         editor.putString("port", mServer.getPort());
