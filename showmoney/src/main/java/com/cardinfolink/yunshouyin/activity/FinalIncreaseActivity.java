5f8f5881db7ec044f6de3dbc11a6fe25679587c1
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-24 10:27:19 +0800

    showmoney: add button onclick event in FinalIncreaseActivity.java
    添加了 按钮的 点击事件
    标题栏的点击事件
    
    Change-Id: Id64444fd19c4f2a510f34ac8c1df7faad4e041b4
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/FinalIncreaseActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/FinalIncreaseActivity.java
@@ -1,16 +1,37 @@
 package com.cardinfolink.yunshouyin.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 
 public class FinalIncreaseActivity extends Activity {
+    private SettingActionBarItem mActionBar;
+    private Button mFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_increase);
+
+        mActionBar = (SettingActionBarItem) findViewById(R.id.action_bar);
+        mActionBar.setLeftTextOnclickListner(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mFinish = (Button) findViewById(R.id.btnfinish);
+        mFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 }
