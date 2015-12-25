a18b2bc52e2a2ef3dc75205289d2932a458d153a
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-25 13:59:35 +0800

    cashiersdk: remove public keyword in interface in CommunicationListener.java
    删除关键字 public ，接口里面不需要关键字public，因为默认就是
    public的
    
    Change-Id: I0df645b93659de3d5cfc97d2524322b45dd6cdc2
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- cashierSdk/src/main/java/com/cardinfolink/cashiersdk/listener/CommunicationListener.java
+++ cashierSdk/src/main/java/com/cardinfolink/cashiersdk/listener/CommunicationListener.java
@@ -1,7 +1,7 @@
 package com.cardinfolink.cashiersdk.listener;
 
 public interface CommunicationListener {
-    public void onResult(String result);
+    void onResult(String result);
 
-    public void onError(int error);
+    void onError(int error);
 }
