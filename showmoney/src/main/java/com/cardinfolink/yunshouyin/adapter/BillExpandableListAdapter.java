72665de1a3353abd9c63b6572b70cfcbf3c8945d
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-26 16:04:53 +0800

    showmoney: add bill BillExpandableListAdapter.java
    Change-Id: Iac2bf7a04718d48cb251b3e3872e936c3b824c4a
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/adapter/BillExpandableListAdapter.java
@@ -0,0 +1,78 @@
+package com.cardinfolink.yunshouyin.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+
+import java.util.List;
+
+/**
+ * Created by mamh on 15-12-26.
+ */
+public class BillExpandableListAdapter extends BaseExpandableListAdapter {
+    private List<String> groupData;
+    private List<List<String>> childrenData;
+    private Context mContext;
+
+
+    public BillExpandableListAdapter(Context context, List<String> groupData, List<List<String>> childrenData) {
+        this.mContext = context;
+        this.groupData = groupData;
+        this.childrenData = childrenData;
+    }
+
+    @Override
+    public int getGroupCount() {
+        if (groupData != null) {
+            return groupData.size();
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return childrenData.get(groupPosition).size();
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return groupData.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return childrenData.get(groupPosition).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return 0;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
