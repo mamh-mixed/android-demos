3928010081feab5fcb7fb9c547819bcd328c5bdc
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-28 11:47:32 +0800

    showmoney: add get group/child view in BillExpandableListAdapter.java
    展开的子视图，添加了 listview
    
    Change-Id: I03de302375e71c91a5c64f52c938eff01f06b4ec
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/adapter/BillExpandableListAdapter.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/adapter/BillExpandableListAdapter.java
@@ -1,22 +1,36 @@
 package com.cardinfolink.yunshouyin.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.data.MonthBill;
+import com.cardinfolink.yunshouyin.data.TradeBill;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 
 /**
  * Created by mamh on 15-12-26.
  */
 public class BillExpandableListAdapter extends BaseExpandableListAdapter {
-    private List<String> groupData;
-    private List<List<String>> childrenData;
+    private String TAG = "BillExpandableListAdapter";
+
+    private List<MonthBill> groupData;
+    private List<Map<String, List<TradeBill>>> childrenData;
     private Context mContext;
 
 
-    public BillExpandableListAdapter(Context context, List<String> groupData, List<List<String>> childrenData) {
+    public BillExpandableListAdapter(Context context, List<MonthBill> groupData, List<Map<String, List<TradeBill>>> childrenData) {
         this.mContext = context;
         this.groupData = groupData;
         this.childrenData = childrenData;
@@ -43,7 +57,7 @@ public class BillExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return childrenData.get(groupPosition).get(childPosition);
+        return childrenData.get(groupPosition).get(String.valueOf(childPosition + 1));
     }
 
     @Override
@@ -63,16 +77,50 @@ public class BillExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = View.inflate(mContext, R.layout.expandablelistview_group, null);
+        }
+
+        //设置一下月份
+        TextView month = (TextView) convertView.findViewById(R.id.tv_month);
+        month.setText(groupData.get(groupPosition).getCurrentMonth());
+
+        TextView year = (TextView) convertView.findViewById(R.id.tv_year);
+        year.setText(groupData.get(groupPosition).getCurrentYear());
+
+        TextView total = (TextView) convertView.findViewById(R.id.tv_total);
+        total.setText(groupData.get(groupPosition).getTotal());
+
+        TextView count = (TextView) convertView.findViewById(R.id.tv_count);
+        count.setText(groupData.get(groupPosition).getCount() + "");
+
+        return convertView;
     }
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        if (convertView == null) {
+            convertView = View.inflate(mContext, R.layout.expandablelistview_child, null);
+        }
+
+        Map<String, List<TradeBill>> tradeBillMap = childrenData.get(groupPosition);
+        Object[] keyArray = tradeBillMap.keySet().toArray();
+        String dayStr = keyArray[childPosition].toString();
+
+        TextView day = (TextView) convertView.findViewById(R.id.tv_day);
+        day.setText(dayStr);
+
+        ListView listView = (ListView) convertView.findViewById(R.id.child_list_view);
+        listView.setAdapter(new BillAdapter(mContext, tradeBillMap.get(dayStr)));
+
+        return convertView;
     }
 
     @Override
     public boolean isChildSelectable(int groupPosition, int childPosition) {
-        return false;
+        return true;
     }
+
+
+
 }
