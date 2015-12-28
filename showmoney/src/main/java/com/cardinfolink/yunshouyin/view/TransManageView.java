2c6874fa84167dcaf95d72117bc159bee4c1a36a
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-28 12:47:49 +0800

    showmoney: add get bill from server then set to listview
    Change-Id: I66823e7231be7b4848e83f2fa2106399b486c50d
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/TransManageView.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/TransManageView.java
@@ -1,19 +1,33 @@
 package com.cardinfolink.yunshouyin.view;
 
 import android.content.Context;
-import android.os.AsyncTask;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
-import android.widget.SimpleExpandableListAdapter;
 
+import com.cardinfolink.cashiersdk.util.TxamtUtil;
 import com.cardinfolink.yunshouyin.R;
+import com.cardinfolink.yunshouyin.adapter.BillExpandableListAdapter;
+import com.cardinfolink.yunshouyin.api.QuickPayException;
+import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
+import com.cardinfolink.yunshouyin.core.QuickPayService;
+import com.cardinfolink.yunshouyin.data.MonthBill;
+import com.cardinfolink.yunshouyin.data.SessonData;
+import com.cardinfolink.yunshouyin.data.TradeBill;
+import com.cardinfolink.yunshouyin.model.QRequest;
+import com.cardinfolink.yunshouyin.model.ServerPacket;
+import com.cardinfolink.yunshouyin.model.Txn;
+import com.cardinfolink.yunshouyin.util.ShowMoneyApp;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshExpandableListView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -22,14 +36,11 @@ public class TransManageView extends LinearLayout {
     private static final String TAG = "TransManageView";
     private Context mContext;
     private PullToRefreshExpandableListView mPullRefreshListView;
-    private SimpleExpandableListAdapter mAdapter;
+    private BillExpandableListAdapter mAdapter;
 
-    private String[] mChildStrings = {"Child One", "Child Two", "Child Three", "Child Four", "Child Five", "Child Six"};
-
-    private String[] mGroupStrings = {"Group One", "Group Two", "Group Three"};
-    private static final String KEY = "key";
-    private List<Map<String, String>> groupData = new ArrayList<Map<String, String>>();
-    private List<List<Map<String, String>>> childData = new ArrayList<List<Map<String, String>>>();
+    private List<Map<String, List<TradeBill>>> mTradeBillList;//日账单，这个条目会很多的
+    private List<MonthBill> mMonthList;//月账单
+    private int monthAgo = 0;
 
 
     public TransManageView(Context context) {
@@ -41,6 +52,7 @@ public class TransManageView extends LinearLayout {
         addView(contentView);
 
         initLayout();
+
     }
 
     private void initLayout() {
@@ -54,72 +66,103 @@ public class TransManageView extends LinearLayout {
             public void onRefresh(PullToRefreshBase<ExpandableListView> refreshView) {
                 // Do work to refresh the list here.
                 Log.e(TAG, " pull to refersh");
-                new GetDataTask().execute();
+                mPullRefreshListView.onRefreshComplete();
+
             }
         });
-        for (String group : mGroupStrings) {
-            Map<String, String> groupMap1 = new HashMap<String, String>();
-            groupData.add(groupMap1);
-            groupMap1.put(KEY, group);
 
-            List<Map<String, String>> childList = new ArrayList<Map<String, String>>();
-            for (String string : mChildStrings) {
-                Map<String, String> childMap = new HashMap<String, String>();
-                childList.add(childMap);
-                childMap.put(KEY, string);
-            }
-            childData.add(childList);
-        }
+        mMonthList = new ArrayList<>();
 
-        mAdapter = new SimpleExpandableListAdapter(mContext,
-                groupData,
-                android.R.layout.simple_expandable_list_item_1,
-                new String[]{KEY}, new int[]{android.R.id.text1},
-                childData,
-                android.R.layout.simple_expandable_list_item_2, new String[]{KEY}, new int[]{android.R.id.text1});
+        mTradeBillList = new ArrayList<>();
 
-        ExpandableListView ActualView = mPullRefreshListView.getRefreshableView();
+        mAdapter = new BillExpandableListAdapter(mContext, mMonthList, mTradeBillList);
+
+        final ExpandableListView ActualView = mPullRefreshListView.getRefreshableView();
         ActualView.setAdapter(mAdapter);
+        ActualView.setGroupIndicator(null);
     }
 
 
     public void refresh() {
-
-
+        getTradeBill();
     }
-    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
 
-        @Override
-        protected String[] doInBackground(Void... params) {
-            // Simulates a background job.
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+
+    private void getTradeBill() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MONTH, 0 - monthAgo);    //得到前一个月
+        String year = String.valueOf(calendar.get(Calendar.YEAR));
+        String month = String.valueOf(calendar.get(Calendar.MONTH) + 1);
+
+
+        final MonthBill monthBill = new MonthBill(year, month);
+
+        QuickPayService quickPayService = ShowMoneyApp.getInstance().getQuickPayService();
+        quickPayService.getHistoryBillsAsync(SessonData.loginUser, year + month, "0", "all", new QuickPayCallbackListener<ServerPacket>() {
+            @Override
+            public void onSuccess(ServerPacket data) {
+                //这里可以在ui线程里执行的
+
+                final int count = data.getCount();//返回的条数
+                final String total = data.getTotal();
+                final int refdcount = data.getRefdcount();
+                final String refdtotal = data.getRefdtotal();
+                final int size = data.getSize();
+                Map<String, List<TradeBill>> billMap = new HashMap<String, List<TradeBill>>();
+                for (Txn txn : data.getTxn()) {
+                    TradeBill tradeBill = new TradeBill();
+                    tradeBill.response = txn.getResponse();
+                    tradeBill.tandeDate = txn.getSystemDate();
+                    tradeBill.consumerAccount = txn.getConsumerAccount();
+
+                    QRequest req = txn.getmRequest();
+                    if (req != null) {
+                        tradeBill.orderNum = req.getOrderNum();
+                        tradeBill.amount = TxamtUtil.getNormal(req.getTxamt());
+                        tradeBill.busicd = req.getBusicd();
+                        if (tradeBill.busicd.equals("REFD")) {
+                            tradeBill.amount = "-" + tradeBill.amount;
+                        }
+                        tradeBill.chcd = req.getChcd();
+                        tradeBill.tradeFrom = req.getTradeFrom();
+                        tradeBill.goodsInfo = req.getGoodsInfo();
+                    }
+
+                    //获取这个账单里面的日期,年月日 的 日
+                    String currentDay = tradeBill.tandeDate.substring(6, 8);
+                    Log.e(TAG, tradeBill.tandeDate + " === " + currentDay);
+
+                    if (!TextUtils.isEmpty(tradeBill.chcd)) {
+                        if (billMap.containsKey(currentDay)) {
+                            billMap.get(currentDay).add(tradeBill);
+                        } else {
+                            ArrayList<TradeBill> list = new ArrayList<TradeBill>();
+                            list.add(tradeBill);
+                            billMap.put(currentDay, list);//把这个list添加到map里面
+                        }
+                    }
+                }
+                // 更新UI
+                monthBill.setCount(count);
+                monthBill.setTotal(total);
+                monthBill.setRefdcount(refdcount);
+                monthBill.setRefdtotal(refdtotal);
+                monthBill.setSize(size);
+
+                mMonthList.add(monthBill);
+                mTradeBillList.add(billMap);
+                mAdapter.notifyDataSetChanged();
+                mPullRefreshListView.onRefreshComplete();
+
+                monthAgo++;//月份往前计数一个月
             }
-            return mChildStrings;
-        }
 
-        @Override
-        protected void onPostExecute(String[] result) {
-            Map<String, String> newMap = new HashMap<String, String>();
-            newMap.put(KEY, "Added after refresh...");
-            groupData.add(newMap);
-
-            List<Map<String, String>> childList = new ArrayList<Map<String, String>>();
-            for (String string : mChildStrings) {
-                Map<String, String> childMap = new HashMap<String, String>();
-                childMap.put(KEY, string);
-                childList.add(childMap);
+            @Override
+            public void onFailure(QuickPayException ex) {
+                Log.e(TAG, " get history bill fail"+ex.getErrorMsg());
+                mPullRefreshListView.onRefreshComplete();
             }
-            childData.add(childList);
-
-            mAdapter.notifyDataSetChanged();
-
-            // Call onRefreshComplete when the list has been refreshed.
-            mPullRefreshListView.onRefreshComplete();
-
-            super.onPostExecute(result);
-        }
+        });
     }
 
 }
