629122649030a7c6077f094678a1e5acb4c20bac
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-28 20:38:49 +0800

    showmoney: add get history trade bill in TransManageView.java
    Change-Id: I6c14696e87d0d96a480aa5ae888a62c395cca059
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/TransManageView.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/TransManageView.java
@@ -27,10 +27,12 @@ import com.handmark.pulltorefresh.library.PullToRefreshExpandableListView;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class TransManageView extends LinearLayout {
     private static final String TAG = "TransManageView";
@@ -38,10 +40,15 @@ public class TransManageView extends LinearLayout {
     private PullToRefreshExpandableListView mPullRefreshListView;
     private BillExpandableListAdapter mAdapter;
 
-    private List<Map<String, List<TradeBill>>> mTradeBillList;//日账单，这个条目会很多的
-    private List<MonthBill> mMonthList;//月账单
-    private int monthAgo = 0;
+    private Map<String, MonthBill> mMonthBillMap;
+    private List<MonthBill> mMonthBilList;//月账单
 
+    private List<List<TradeBill>> mTradeBillList;//日账单，这个条目会很多的
+    private Map<String, List<TradeBill>> mTradeBillMap;
+
+    private int billIndex;
+    private String mMonth;
+    private int mMonthAgo;
 
     public TransManageView(Context context) {
         super(context);
@@ -50,9 +57,11 @@ public class TransManageView extends LinearLayout {
         LinearLayout.LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         contentView.setLayoutParams(layoutParams);
         addView(contentView);
-
         initLayout();
 
+
+        SimpleDateFormat spf = new SimpleDateFormat("yyyyMM");
+        mMonth = spf.format(new Date());
     }
 
     private void initLayout() {
@@ -65,17 +74,30 @@ public class TransManageView extends LinearLayout {
             @Override
             public void onRefresh(PullToRefreshBase<ExpandableListView> refreshView) {
                 // Do work to refresh the list here.
-                Log.e(TAG, " pull to refersh");
-                mPullRefreshListView.onRefreshComplete();
+                if (refreshView.isHeaderShown()) {
+                    billIndex = 0;
+                    mTradeBillList.clear();
+                    mMonthBilList.clear();
+                    mMonthBillMap.clear();
+                    mTradeBillMap.clear();
+                    SimpleDateFormat spf = new SimpleDateFormat("yyyyMM");
+                    mMonth = spf.format(new Date());
+                    getTradeBill();
+                    Log.e(TAG, " billIndex == " + billIndex);
+                } else {
+                    getTradeBill();
+                }
 
             }
         });
 
-        mMonthList = new ArrayList<>();
-
+        mMonthBilList = new ArrayList<>();
         mTradeBillList = new ArrayList<>();
+        mMonthBillMap = new HashMap<>();
+        mTradeBillMap = new HashMap<>();
 
-        mAdapter = new BillExpandableListAdapter(mContext, mMonthList, mTradeBillList);
+
+        mAdapter = new BillExpandableListAdapter(mContext, mMonthBilList, mTradeBillList);
 
         final ExpandableListView ActualView = mPullRefreshListView.getRefreshableView();
         ActualView.setAdapter(mAdapter);
@@ -89,16 +111,8 @@ public class TransManageView extends LinearLayout {
 
 
     private void getTradeBill() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MONTH, 0 - monthAgo);    //得到前一个月
-        String year = String.valueOf(calendar.get(Calendar.YEAR));
-        String month = String.valueOf(calendar.get(Calendar.MONTH) + 1);
-
-
-        final MonthBill monthBill = new MonthBill(year, month);
-
         QuickPayService quickPayService = ShowMoneyApp.getInstance().getQuickPayService();
-        quickPayService.getHistoryBillsAsync(SessonData.loginUser, year + month, "0", "all", new QuickPayCallbackListener<ServerPacket>() {
+        quickPayService.getHistoryBillsAsync(SessonData.loginUser, mMonth, String.valueOf(billIndex), "100", "all", new QuickPayCallbackListener<ServerPacket>() {
             @Override
             public void onSuccess(ServerPacket data) {
                 //这里可以在ui线程里执行的
@@ -108,7 +122,6 @@ public class TransManageView extends LinearLayout {
                 final int refdcount = data.getRefdcount();
                 final String refdtotal = data.getRefdtotal();
                 final int size = data.getSize();
-                Map<String, List<TradeBill>> billMap = new HashMap<String, List<TradeBill>>();
                 for (Txn txn : data.getTxn()) {
                     TradeBill tradeBill = new TradeBill();
                     tradeBill.response = txn.getResponse();
@@ -130,39 +143,86 @@ public class TransManageView extends LinearLayout {
 
                     //获取这个账单里面的日期,年月日 的 日
                     String currentDay = tradeBill.tandeDate.substring(6, 8);
-                    Log.e(TAG, tradeBill.tandeDate + " === " + currentDay);
+                    String currentYear = tradeBill.tandeDate.substring(0, 4);
+                    String currentMonth = tradeBill.tandeDate.substring(4, 6);
+                    String currentYearMonth = tradeBill.tandeDate.substring(0, 6);
 
-                    if (!TextUtils.isEmpty(tradeBill.chcd)) {
-                        if (billMap.containsKey(currentDay)) {
-                            billMap.get(currentDay).add(tradeBill);
-                        } else {
-                            ArrayList<TradeBill> list = new ArrayList<TradeBill>();
-                            list.add(tradeBill);
-                            billMap.put(currentDay, list);//把这个list添加到map里面
-                        }
+                    if (TextUtils.isEmpty(tradeBill.chcd)) {
+                        continue;
+                    }
+
+                    //添加到相应的map中，最后在转换到list中，排序转换到list中
+                    if (mMonthBillMap.containsKey(currentYearMonth)) {
+                        mMonthBillMap.get(currentYearMonth).setCount(count);
+                        mMonthBillMap.get(currentYearMonth).setTotal(total);
+                        mMonthBillMap.get(currentYearMonth).setRefdcount(refdcount);
+                        mMonthBillMap.get(currentYearMonth).setRefdtotal(refdtotal);
+                        mMonthBillMap.get(currentYearMonth).setSize(billIndex);
+                    } else {
+                        MonthBill monthBill = new MonthBill(currentYear, currentMonth);
+                        monthBill.setCount(count);
+                        monthBill.setTotal(total);
+                        monthBill.setRefdcount(refdcount);
+                        monthBill.setRefdtotal(refdtotal);
+                        monthBill.setSize(billIndex);
+                        mMonthBillMap.put(currentYearMonth, monthBill);
+                    }
+                    if (mTradeBillMap.containsKey(currentYearMonth)) {
+                        mTradeBillMap.get(currentYearMonth).add(tradeBill);
+                    } else {
+                        List<TradeBill> list = new ArrayList<TradeBill>();
+                        list.add(tradeBill);
+                        mTradeBillMap.put(currentYearMonth, list);
                     }
                 }
-                // 更新UI
-                monthBill.setCount(count);
-                monthBill.setTotal(total);
-                monthBill.setRefdcount(refdcount);
-                monthBill.setRefdtotal(refdtotal);
-                monthBill.setSize(size);
 
-                mMonthList.add(monthBill);
-                mTradeBillList.add(billMap);
+                mapToMonthBillList(mMonthBillMap);
+                mapToTradeBillList(mTradeBillMap);
+
                 mAdapter.notifyDataSetChanged();
                 mPullRefreshListView.onRefreshComplete();
 
-                monthAgo++;//月份往前计数一个月
+                billIndex += size;
+                if (size == 0) {
+                    //size等于零 表示 加载到这个月的全部的了，这时候就要加载前一个月的数据了
+                    billIndex = 0;
+                    mMonthAgo += 1;
+                    Calendar calendar = Calendar.getInstance();
+                    calendar.add(Calendar.MONTH, 0 - mMonthAgo);    //得到前一个月
+                    String year = String.valueOf(calendar.get(Calendar.YEAR));
+                    String month = String.valueOf(calendar.get(Calendar.MONTH) + 1);
+                    mMonth = year + month;
+                }
             }
 
             @Override
             public void onFailure(QuickPayException ex) {
-                Log.e(TAG, " get history bill fail"+ex.getErrorMsg());
                 mPullRefreshListView.onRefreshComplete();
             }
         });
     }
 
+    private void mapToMonthBillList(Map<String, MonthBill> map) {
+        Set<String> keyset = map.keySet();
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(keyset);
+        Collections.reverse(list);
+
+        mMonthBilList.clear();
+        for (String key : list) {
+            mMonthBilList.add(map.get(key));
+        }
+    }
+
+    private void mapToTradeBillList(Map<String, List<TradeBill>> map) {
+        Set<String> keyset = map.keySet();
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(keyset);
+        Collections.reverse(list);//把 key 排序一下
+
+        mTradeBillList.clear();
+        for (String key : list) {
+            mTradeBillList.add(map.get(key));
+        }
+    }
 }
