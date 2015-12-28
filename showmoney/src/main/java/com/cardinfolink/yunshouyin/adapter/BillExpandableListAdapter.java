f05606901b2add97e00e794201e491c52e906d89
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-28 20:37:30 +0800

    showmoney: update child item and not use listview
    expandableListview中不再使用嵌套的listview了
    嵌套的listview太卡了
    
    Change-Id: Ib9cbcdb5b071d984bd44eb8162386429a4ef1294
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/adapter/BillExpandableListAdapter.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/adapter/BillExpandableListAdapter.java
@@ -1,25 +1,24 @@
 package com.cardinfolink.yunshouyin.adapter;
 
 import android.content.Context;
-import android.util.Log;
+import android.graphics.Color;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.ImageView;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.data.MonthBill;
 import com.cardinfolink.yunshouyin.data.TradeBill;
-import com.cardinfolink.yunshouyin.ui.SubListView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
 
 /**
  * Created by mamh on 15-12-26.
@@ -28,15 +27,16 @@ public class BillExpandableListAdapter extends BaseExpandableListAdapter {
     private String TAG = "BillExpandableListAdapter";
 
     private List<MonthBill> groupData;
-    private List<Map<String, List<TradeBill>>> childrenData;
+    private List<List<TradeBill>> childrenData;
     private Context mContext;
 
 
-    public BillExpandableListAdapter(Context context, List<MonthBill> groupData, List<Map<String, List<TradeBill>>> childrenData) {
+    public BillExpandableListAdapter(Context context, List<MonthBill> groupData, List<List<TradeBill>> childrenData) {
         this.mContext = context;
         this.groupData = groupData;
         this.childrenData = childrenData;
     }
+
 
     @Override
     public int getGroupCount() {
@@ -59,7 +59,7 @@ public class BillExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return childrenData.get(groupPosition).get(String.valueOf(childPosition + 1));
+        return childrenData.get(groupPosition).get(childPosition);
     }
 
     @Override
@@ -99,7 +99,8 @@ public class BillExpandableListAdapter extends BaseExpandableListAdapter {
         groupViewHolder.month.setText(groupData.get(groupPosition).getCurrentMonth());
         groupViewHolder.year.setText(groupData.get(groupPosition).getCurrentYear());
         groupViewHolder.total.setText(groupData.get(groupPosition).getTotal());
-        groupViewHolder.count.setText(groupData.get(groupPosition).getCount() + "");
+        int size = childrenData.get(groupPosition).size();
+        groupViewHolder.count.setText(size + "");
 
         if (isExpanded) {
             groupViewHolder.folder.setBackgroundResource(R.drawable.bill_pack);
@@ -118,22 +119,69 @@ public class BillExpandableListAdapter extends BaseExpandableListAdapter {
             childViewHolder = new ChildViewHolder();
             convertView = View.inflate(mContext, R.layout.expandablelistview_child, null);
             childViewHolder.day = (TextView) convertView.findViewById(R.id.tv_day);
-            childViewHolder.weekday= (TextView) convertView.findViewById(R.id.tv_weekday);
+            childViewHolder.weekday = (TextView) convertView.findViewById(R.id.tv_weekday);
 
-            childViewHolder.listView = (ListView) convertView.findViewById(R.id.child_list_view);
+            childViewHolder.paylogo = (ImageView) convertView.findViewById(R.id.paylogo);
+            childViewHolder.billTradeDate = (TextView) convertView.findViewById(R.id.bill_tradedate);
+            childViewHolder.billTradeFrom = (TextView) convertView.findViewById(R.id.bill_tradefrom);
+            childViewHolder.billTradeStatus = (TextView) convertView.findViewById(R.id.bill_tradestatus);
+            childViewHolder.billTradeAmount = (TextView) convertView.findViewById(R.id.bill_tradeamount);
 
             convertView.setTag(childViewHolder);
         } else {
             childViewHolder = (ChildViewHolder) convertView.getTag();
         }
 
-        Map<String, List<TradeBill>> tradeBillMap = childrenData.get(groupPosition);
-        Object[] keyArray = tradeBillMap.keySet().toArray();
-        String dayStr = keyArray[childPosition].toString();
+        TradeBill bill = childrenData.get(groupPosition).get(childPosition);
 
-        childViewHolder.day.setText(dayStr);
+        if (!TextUtils.isEmpty(bill.chcd)) {
+            //有chcd渠道的话,这里设置不同渠道的图片
+            if (bill.chcd.equals("WXP")) {
+                childViewHolder.paylogo.setImageResource(R.drawable.wpay);
+            } else {
+                childViewHolder.paylogo.setImageResource(R.drawable.apay);
+            }
+        } else {
+            childViewHolder.paylogo.setImageDrawable(null);
+        }
+        SimpleDateFormat spf1 = new SimpleDateFormat("yyyyMMddHHmmss");
+        SimpleDateFormat spf2 = new SimpleDateFormat("MM-dd HH:mm:ss");
+        SimpleDateFormat spf3 = new SimpleDateFormat("dd");
+        try {
+            Date tandeDate = spf1.parse(bill.tandeDate);
+            childViewHolder.billTradeDate.setText(spf2.format(tandeDate));
+            childViewHolder.day.setText(spf3.format(tandeDate));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        String tradeFrom = "PC";
+        if (!TextUtils.isEmpty(bill.tradeFrom)) {
+            tradeFrom = bill.tradeFrom;
+        }
 
-        childViewHolder.listView.setAdapter(new BillAdapter(mContext, tradeBillMap.get(dayStr)));
+        String busicd = mContext.getResources().getString(R.string.detail_activity_busicd_pay);
+        if (bill.busicd.equals("REFD")) {
+            //退款的
+            busicd = mContext.getResources().getString(R.string.detail_activity_busicd_refd);
+        } else if ("CANC".equals(bill.busicd)) {
+            //取消订单
+            busicd = mContext.getResources().getString(R.string.detail_activity_busicd_canc);
+        }
+
+        childViewHolder.billTradeFrom.setText(tradeFrom + busicd);
+        String tradeStatus;
+        if (bill.response.equals("00")) {
+            tradeStatus = mContext.getResources().getString(R.string.detail_activity_trade_status_success);
+            childViewHolder.billTradeStatus.setTextColor(Color.parseColor("#888888"));
+        } else if (bill.response.equals("09")) {
+            tradeStatus = mContext.getResources().getString(R.string.detail_activity_trade_status_nopay);
+            childViewHolder.billTradeStatus.setTextColor(Color.RED);
+        } else {
+            tradeStatus = mContext.getResources().getString(R.string.detail_activity_trade_status_fail);
+            childViewHolder.billTradeStatus.setTextColor(Color.RED);
+        }
+        childViewHolder.billTradeStatus.setText(tradeStatus);
+        childViewHolder.billTradeAmount.setText("￥" + bill.amount);
 
         return convertView;
     }
@@ -154,6 +202,10 @@ public class BillExpandableListAdapter extends BaseExpandableListAdapter {
     public final class ChildViewHolder {
         public TextView day;
         public TextView weekday;
-        public ListView listView;
+        public ImageView paylogo;
+        public TextView billTradeDate;
+        public TextView billTradeFrom;
+        public TextView billTradeStatus;
+        public TextView billTradeAmount;
     }
 }
