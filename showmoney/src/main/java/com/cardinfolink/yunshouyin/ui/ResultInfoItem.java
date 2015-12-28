eb1f71f40973e90a5cd688f74c7e6840321022c4
Deng Jia <charles.deng@cardinfolink.com>
2015-12-28 10:38:23 +0800

    showmoney:add new ui ResultInfoItem
    添加了一个新的自定义 组件
    
    Change-Id: I62521ca13d45ad61655a1c90d7cfa571c368cdbb
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

---
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/ui/ResultInfoItem.java
@@ -0,0 +1,49 @@
+package com.cardinfolink.yunshouyin.ui;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.cardinfolink.yunshouyin.R;
+
+/**
+ * Created by charles on 2015/12/25.
+ */
+public class ResultInfoItem extends RelativeLayout {
+    private TextView mLeftText;
+    private TextView mRightText;
+
+    public ResultInfoItem(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public ResultInfoItem(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+        TypedArray typeArray = context.obtainStyledAttributes(attrs, R.styleable.SettingItemView);
+        String left_text = typeArray.getString(R.styleable.SettingItemView_left_text);
+        String right_text = typeArray.getString(R.styleable.SettingItemView_right_text);
+        typeArray.recycle();
+        mLeftText.setText(left_text);
+        mRightText.setText(right_text);
+    }
+
+    public void initView(Context context) {
+        View.inflate(context, R.layout.result_info_item, this);
+        mLeftText = (TextView) this.findViewById(R.id.tv_left);
+        mRightText = (TextView) this.findViewById(R.id.tv_right);
+
+    }
+
+    public void setLeftText(String str1) {
+        mLeftText.setText(str1);
+    }
+
+    public void setRightText(String str2) {
+        mRightText.setText(str2);
+    }
+}
