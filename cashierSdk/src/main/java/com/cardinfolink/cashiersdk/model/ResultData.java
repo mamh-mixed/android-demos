5b83c6392b43f6f8a26671fd3185c5c0721a7632
Deng Jia <charles.deng@cardinfolink.com>
2015-12-29 15:02:37 +0800

    cashierSdk:add element member for ticket in  ResultData
    Change-Id: I991a56417ccc2c77d4f002a45b78c221a13297b9
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- cashierSdk/src/main/java/com/cardinfolink/cashiersdk/model/ResultData.java
+++ cashierSdk/src/main/java/com/cardinfolink/cashiersdk/model/ResultData.java
@@ -1,27 +1,34 @@
 package com.cardinfolink.cashiersdk.model;
 
 public class ResultData {
-    public String respcd;
-    public String busicd;
-    public String chcd;
-    public String txamt;
-    public String channelOrderNum;
+    public String respcd;//返回交易结果
+    public String busicd;//交易类型
+    public String chcd;//交易渠道
+    public String txamt;//交易原始金额
+    public String channelOrderNum;//
     public String consumerAccount;
     public String consumerId;
-    public String errorDetail;
-    public String orderNum;
+    public String errorDetail;//错误详情
+    public String orderNum;//订单号
     public String chcdDiscount;
     public String merDiscount;
     public String qrcode;
-    public String origOrderNum;
-    public String scanCodeId;
-    public String cardId;
-    public String cardInfo;
-    public String voucherType;
-    public String saleMinAmount;
-    public String saleDiscount;
-    public String actualPayAmount;
-    public String maxDiscountAmt;
+    public String origOrderNum;//原始订单号
+    public String scanCodeId;//扫码号
+    public String cardId;//卡券类型
+    public String cardInfo;//卡券详情
+    public String cardbin;//银行卡的bin 或 用户标识
+    public String mchntid;//商户号
+    public String veriTime;//核销次数
+    public String payType;//支付方式
+    public String availCount;//卡券剩余次数
+    public String expDate;//卡券有效期
+    public String voucherType;//卡券的类型
+    public String saleMinAmount;//满足优惠条件的最小金额
+    public String saleDiscount;//抵扣金额
+    public String actualPayAmount;//实际支付金额
+    public String maxDiscountAmt;//最大优惠金额
+
 
     @Override
     public String toString() {
@@ -42,6 +49,12 @@ public class ResultData {
                 ", scanCodeId='" + scanCodeId + '\'' +
                 ", cardId='" + cardId + '\'' +
                 ", cardInfo='" + cardInfo + '\'' +
+                ", cardbin='" + cardbin + '\'' +
+                ", mchntid='" + mchntid + '\'' +
+                ", veriTime='" + veriTime + '\'' +
+                ", payType='" + payType + '\'' +
+                ", availCount='" + availCount + '\'' +
+                ", expDate='" + expDate + '\'' +
                 ", voucherType='" + voucherType + '\'' +
                 ", saleMinAmount='" + saleMinAmount + '\'' +
                 ", saleDiscount='" + saleDiscount + '\'' +
