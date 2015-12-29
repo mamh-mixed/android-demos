c0fc5db7f727c081792b9c93d06570e764c54251
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-29 14:44:38 +0800

    showmony:use new detail view and  add detail activity
    Change-Id: If6adc6f216fb78deb4586a2d3d8f3d6fb6b6fb1c
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/DetailActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/DetailActivity.java
@@ -1,15 +1,14 @@
 package com.cardinfolink.yunshouyin.activity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -20,39 +19,35 @@ import com.cardinfolink.cashiersdk.model.ResultData;
 import com.cardinfolink.cashiersdk.sdk.CashierSdk;
 import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.api.QuickPayException;
-import com.cardinfolink.yunshouyin.constant.Msg;
 import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
 import com.cardinfolink.yunshouyin.data.SessonData;
 import com.cardinfolink.yunshouyin.data.TradeBill;
 import com.cardinfolink.yunshouyin.model.ServerPacket;
 import com.cardinfolink.yunshouyin.model.ServerPacketOrder;
+import com.cardinfolink.yunshouyin.ui.ResultInfoItem;
+import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 import com.cardinfolink.yunshouyin.view.RefdDialog;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-@SuppressLint("SimpleDateFormat")
 public class DetailActivity extends BaseActivity {
 
     private TradeBill mTradeBill;
-
-    private ImageView mPaylogoImage;
-
-    private TextView mTradeFromText;
-    private TextView mTradeDateText;
-    private TextView mTradeStatusText;
-    private TextView mConsumerAccount;
-    private TextView mTradeAmountText;
-    private TextView mOrderNumText;
-    private TextView mGoodInfoText;
-
-    private Button mRefdButton; //退款按钮
-
-    private Handler mHandler;
-
-    private ImageView mBack;    //返回，这个是iamgeview
-    private Button mRefreshButton;  //刷新按钮
+    private SettingActionBarItem mActionBar;
+    private TextView mPayResult;
+    private ImageView mPayResultImage;
+    private ResultInfoItem mRealPayMoney;
+    private ResultInfoItem mPayMoney;
+    private ResultInfoItem mRefdMoney;
+    private ResultInfoItem mPayChcd;
+    private ResultInfoItem mPayAccount;
+    private ResultInfoItem mPayDatetime;
+    private ResultInfoItem mPayOrder;
+    private ResultInfoItem mTradeFrom;
+    private Button mClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,106 +58,122 @@ public class DetailActivity extends BaseActivity {
         mTradeBill = (TradeBill) billBundle.get("TradeBill");
         initLayout();
         initData();
-        initHandler();
     }
 
     private void initLayout() {
-        mPaylogoImage = (ImageView) findViewById(R.id.paylogo);
-        mTradeFromText = (TextView) findViewById(R.id.tradefrom);
-        mTradeDateText = (TextView) findViewById(R.id.tradedate);
-        mTradeStatusText = (TextView) findViewById(R.id.tradestatus);
-        mConsumerAccount = (TextView) findViewById(R.id.consumer_account);
-        mTradeAmountText = (TextView) findViewById(R.id.tradeamount);
-        mOrderNumText = (TextView) findViewById(R.id.ordernum);
-        mGoodInfoText = (TextView) findViewById(R.id.goodinfo);
-
-        mRefdButton = (Button) findViewById(R.id.btn_refd);
-        mRefdButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btnRefdOnClick(v);//退款按钮的响应事件处理方法
-            }
-        });
-
-        mBack = (ImageView) findViewById(R.id.iv_back);
-        mBack.setOnClickListener(new View.OnClickListener() {
+        mActionBar = (SettingActionBarItem) findViewById(R.id.action_bar);
+        mActionBar.setLeftTextOnclickListner(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
-        mRefreshButton = (Button) findViewById(R.id.refresh);
-        mRefreshButton.setOnClickListener(new View.OnClickListener() {
+        mActionBar.setRightTextOnclickListner(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                btnRefreshOnClick(v);//刷新按钮点击事件处理方法
+                //退款按钮
+                if ("REFD".equals(mTradeBill.busicd) || "CANC".equals(mTradeBill.busicd) || !"00".equals(mTradeBill.response)) {
+                } else {
+                    refdOnClick(v);//退款按钮
+                }
+            }
+        });
+
+        mPayResult = (TextView) findViewById(R.id.tv_pay_result);
+        mPayResultImage = (ImageView) findViewById(R.id.iv_pay_result);
+        mRealPayMoney = (ResultInfoItem) findViewById(R.id.real_pay_money);
+        mPayMoney = (ResultInfoItem) findViewById(R.id.pay_money);
+        mRefdMoney = (ResultInfoItem) findViewById(R.id.refd_money);
+
+        mPayChcd = (ResultInfoItem) findViewById(R.id.pay_chcd);
+        mPayAccount = (ResultInfoItem) findViewById(R.id.pay_account);
+        mPayDatetime = (ResultInfoItem) findViewById(R.id.pay_datetime);
+        mPayOrder = (ResultInfoItem) findViewById(R.id.pay_order);
+        mTradeFrom = (ResultInfoItem) findViewById(R.id.pay_tradefrom);
+
+        mClose = (Button) findViewById(R.id.btnclose);
+        mClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
 
-    @SuppressLint("NewApi")
     private void initData() {
+        //这里是设置支付渠道
         if (!TextUtils.isEmpty(mTradeBill.chcd)) {
-            //有chcd渠道的话,这里设置不同渠道的图片
             if ("WXP".equals(mTradeBill.chcd)) {
-                mPaylogoImage.setImageResource(R.drawable.wpay);
+                mPayChcd.setRightText("微信");
             } else {
-                mPaylogoImage.setImageResource(R.drawable.apay);
+                mPayChcd.setRightText("支付宝");
             }
         } else {
-            //如果没有chcd渠道的话,这里设置为一个错误的图片
-            mPaylogoImage.setImageResource(R.drawable.wrong);
+            mPayChcd.setRightText("");
         }
+
         SimpleDateFormat spf1 = new SimpleDateFormat("yyyyMMddHHmmss");
         SimpleDateFormat spf2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         try {
             Date tandeDate = spf1.parse(mTradeBill.tandeDate);
-            mTradeDateText.setText(spf2.format(tandeDate));
+            mPayDatetime.setRightText(spf2.format(tandeDate));
         } catch (ParseException e) {
-            e.printStackTrace();
+            mPayDatetime.setRightText("");
         }
-        String tradeFrom = "PC";
+
+        mPayAccount.setRightText(SessonData.loginUser.getUsername());
+
+        String tradeFrom = "";
         if (!TextUtils.isEmpty(mTradeBill.tradeFrom)) {
             tradeFrom = mTradeBill.tradeFrom;
         }
-        String busicd = getResources().getString(R.string.detail_activity_busicd_pay);
-        if (mTradeBill.busicd.equals("REFD")) {
-            busicd = getResources().getString(R.string.detail_activity_busicd_refd);
-        } else if ("CANC".equals(mTradeBill.busicd)) {
-            //取消订单
-            busicd = getResources().getString(R.string.detail_activity_busicd_canc);
-        }
 
-        mTradeFromText.setText(tradeFrom + busicd);
+        mTradeFrom.setRightText(tradeFrom + " 收款");
+
+        mPayOrder.setRightText(mTradeBill.orderNum);
+
+        //设置交易的结果状态
         String tradeStatus = "";
-
-        if (mTradeBill.response.equals("00")) {
+        if ("00".equals(mTradeBill.response)) {
+            //成功的
             tradeStatus = getResources().getString(R.string.detail_activity_trade_status_success);
-            mTradeStatusText.setTextColor(Color.parseColor("#888888"));
-        } else if (mTradeBill.response.equals("09")) {
+            mPayResult.setTextColor(Color.parseColor("#00bbd3"));
+            mPayResultImage.setImageResource(R.drawable.pay_result_succeed);
+        } else if ("09".equals(mTradeBill.response)) {
+            //正在处理中的交易
             tradeStatus = getResources().getString(R.string.detail_activity_trade_status_nopay);
-            mTradeStatusText.setTextColor(Color.RED);
+            mPayResult.setTextColor(Color.BLACK);
+            mPayResultImage.setImageResource(R.drawable.refresh);
+            mPayResultImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    refreshOnclick(v);
+                }
+            });
         } else {
+            //其他
             tradeStatus = getResources().getString(R.string.detail_activity_trade_status_fail);
-            mTradeStatusText.setTextColor(Color.RED);
+            mPayResult.setTextColor(Color.RED);
+            mPayResultImage.setImageResource(R.drawable.pay_result_fail);
         }
-        mTradeStatusText.setText(tradeStatus);
-        mConsumerAccount.setText(mTradeBill.consumerAccount);
-        mTradeAmountText.setText("￥" + mTradeBill.amount);
-        mOrderNumText.setText(mTradeBill.orderNum);
-        mGoodInfoText.setText(mTradeBill.goodsInfo);
-        if (mTradeBill.busicd.equals("REFD") || mTradeBill.busicd.equals("CANC") || !mTradeBill.response.equals("00")) {
-            mRefdButton.setVisibility(View.INVISIBLE);
-        } else {
-            mRefdButton.setVisibility(View.VISIBLE);
-        }
+        mPayResult.setText(tradeStatus);
+
+        mPayMoney.setRightText(mTradeBill.amount);
+        mRefdMoney.setRightText(mTradeBill.refundAmt);
+        BigDecimal bgPay = new BigDecimal(mTradeBill.amount);
+        BigDecimal bgRefd = new BigDecimal(mTradeBill.refundAmt);
+        String realPay = bgPay.subtract(bgRefd).toString();
+        mRealPayMoney.setRightText(realPay);
+
+
     }
 
+
     //刷新按钮点击事件处理方法
-    public void btnRefreshOnClick(View view) {
+    public void refreshOnclick(View view) {
         OrderData orderData = new OrderData();
         orderData.origOrderNum = mTradeBill.orderNum;
-        startLoading();
+        startLoading(mPayResultImage);
         CashierSdk.startQy(orderData, new CashierListener() {
 
             @Override
@@ -171,13 +182,13 @@ public class DetailActivity extends BaseActivity {
                     @Override
                     public void onSuccess(ServerPacketOrder data) {
                         mTradeBill.response = data.getTxn().getResponse();
-                        endLoading();
+                        endLoading(mPayResultImage);
                         initData();
                     }
 
                     @Override
                     public void onFailure(QuickPayException ex) {
-                        endLoading();
+                        endLoading(mPayResultImage);
                         initData();
                     }
                 });
@@ -185,7 +196,7 @@ public class DetailActivity extends BaseActivity {
 
             @Override
             public void onError(int errorCode) {
-
+                endLoading(mPayResultImage);
             }
 
         });
@@ -194,8 +205,17 @@ public class DetailActivity extends BaseActivity {
     }
 
 
+    public void startLoading(View view) {
+        Animation loadingAnimation = AnimationUtils.loadAnimation(mContext, R.anim.loading_animation);
+        view.startAnimation(loadingAnimation);
+    }
+
+    public void endLoading(View view) {
+        view.clearAnimation();
+    }
+
     //退款按钮的响应事件处理方法
-    public void btnRefdOnClick(View view) {
+    public void refdOnClick(View view) {
         startLoading();
         final String orderNum = mTradeBill.orderNum;
         final String amount = mTradeBill.amount;
@@ -206,11 +226,9 @@ public class DetailActivity extends BaseActivity {
                 String refdtotal = data.getRefdtotal();
                 // 更新UI
                 endLoading();
-
-                //退款对话框
                 View refdView = findViewById(R.id.refd_dialog);
+                RefdDialog refdDialog = new RefdDialog(mContext, null, refdView, orderNum, refdtotal, amount);
 
-                RefdDialog refdDialog = new RefdDialog(mContext, mHandler, refdView, orderNum, refdtotal, amount);
                 refdDialog.show();
             }
 
@@ -225,20 +243,5 @@ public class DetailActivity extends BaseActivity {
         });
     }
 
-    private void initHandler() {
-        mHandler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                switch (msg.what) {
-                    case Msg.MSG_FROM_CLIENT_ALERT_OK: {
-                        SessonData.positionView = 1;
-                        setResult(101);
-                        finish();
-                    }
-                }
-                super.handleMessage(msg);
-            }
-        };
-    }
 
 }
