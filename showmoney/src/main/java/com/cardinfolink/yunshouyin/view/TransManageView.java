833c37b0c2bd8ea48722b4a9a9e61cd91c607709
Minghui Ma <mamh.ma@cardinfolink.com>
2015-12-31 15:27:59 +0800

    showmoney: add findorder to get collection bill
    添加了 获取收款码账单的方法
    
    Change-Id: Ie83a2466ebbd19af100626e9cbc3f51c8cef23e6
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/view/TransManageView.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/view/TransManageView.java
@@ -44,7 +44,9 @@ public class TransManageView extends LinearLayout {
     private static final String TAG = "TransManageView";
     private Context mContext;
 
-    //****************************************************************************************
+    private QuickPayService quickPayService;
+
+    //***普通的收款账单*************************************************************************************
     private PullToRefreshExpandableListView mBillPullRefreshListView;//第一个第一个账单的listview
     private BillExpandableListAdapter mBillAdapter;
 
@@ -56,13 +58,21 @@ public class TransManageView extends LinearLayout {
 
     private int billIndex;
     private int mMonthBillAgo;
-    //****************************************************************************************
 
-    //****************************************************************************************
+    //***卡券账单*************************************************************************************
     private PullToRefreshExpandableListView mTicketPullRefreshListView;//第2个第2个卡券账单的listview
 
-    //****************************************************************************************
+
+    //**收款码账单**************************************************************************************
     private PullToRefreshExpandableListView mCollectionPullRefreshListView;//第3个第3个收款码账单的listview
+
+    private Map<String, MonthBill> mMonthCollectionBillMap;
+    private List<MonthBill> mMonthCollectionBillList;//收款码的月账单
+
+    private List<List<TradeBill>> mCollectionBillList;//收款码的日账单，这个条目会很多的
+    private Map<String, List<TradeBill>> mCollectionBillMap;
+
+    private int collectionIndex;//收款码账单 使用到的 index索引值
     //****************************************************************************************
 
     private String mCurrentYearMonth;//当前年份+月份的一个字符串
@@ -95,6 +105,8 @@ public class TransManageView extends LinearLayout {
         LinearLayout.LayoutParams layoutParams = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         contentView.setLayoutParams(layoutParams);
         addView(contentView);
+
+        quickPayService = ShowMoneyApp.getInstance().getQuickPayService();
 
         initLayout();
     }
@@ -148,6 +160,10 @@ public class TransManageView extends LinearLayout {
         mCollectionPullRefreshListView = (PullToRefreshExpandableListView) findViewById(R.id.colloction_list_view);
         mCollectionPullRefreshListView.setVisibility(GONE);
 
+        mCollectionBillList = new ArrayList<>();
+        mMonthCollectionBillList = new ArrayList<>();
+        mCollectionBillMap = new HashMap<>();
+        mMonthCollectionBillMap = new HashMap<>();
 
         //******************************************************************************************
 
@@ -182,8 +198,9 @@ public class TransManageView extends LinearLayout {
                 mBillPullRefreshListView.setVisibility(VISIBLE);
                 mTicketPullRefreshListView.setVisibility(GONE);
                 mCollectionPullRefreshListView.setVisibility(GONE);
-
                 mTitle.setText(mRaidoBill.getText());//设置标题
+
+                getBill();
             }
         });
 
@@ -197,6 +214,8 @@ public class TransManageView extends LinearLayout {
                 mTicketPullRefreshListView.setVisibility(VISIBLE);
                 mCollectionPullRefreshListView.setVisibility(GONE);
                 mTitle.setText(mRadioTicket.getText());
+
+                getTicketBill();
             }
         });
 
@@ -210,6 +229,8 @@ public class TransManageView extends LinearLayout {
                 mTicketPullRefreshListView.setVisibility(GONE);
                 mCollectionPullRefreshListView.setVisibility(VISIBLE);
                 mTitle.setText(mRadioCollection.getText());
+
+                getCollectionBill();
             }
         });
     }
@@ -222,7 +243,6 @@ public class TransManageView extends LinearLayout {
 
     //获取收款的账单账单
     private void getBill() {
-        QuickPayService quickPayService = ShowMoneyApp.getInstance().getQuickPayService();
         quickPayService.getHistoryBillsAsync(SessonData.loginUser, mCurrentYearMonth, String.valueOf(billIndex), "100", "all", new QuickPayCallbackListener<ServerPacket>() {
             @Override
             public void onSuccess(ServerPacket data) {
@@ -297,8 +317,8 @@ public class TransManageView extends LinearLayout {
                 }
                 //**********************************************************************************
 
-                mapToMonthBillList(mMonthBillMap);
-                mapToTradeBillList(mTradeBillMap);
+                mapToMonthBillList(mMonthBillMap, mMonthBilList);
+                mapToBillList(mTradeBillMap, mTradeBillList);
 
                 mBillAdapter.notifyDataSetChanged();
                 mBillPullRefreshListView.onRefreshComplete();
@@ -332,10 +352,135 @@ public class TransManageView extends LinearLayout {
 
     //获取 收款码 账单
     public void getCollectionBill() {
+        String size = "100";
+        String index = String.valueOf(collectionIndex);
+        /**
+         * recType
+         * "收款方式：移动版 桌面版 收款码 开放接口
+         *移动版 1
+         *桌面版 2
+         *收款码 4
+         *开放接口 8
+         *移动版｜桌面版：1 | 2 = 3
+         *移动版 | 收款码:1 | 4 = 5
+         *全部：1 | 2 | 4 | 8 = 15 "
+         */
+        String recType = "4";
+
+        /**
+         * payType
+         *  "支付方式：支付宝 微信
+         *  支付宝 1
+         *  微信 2
+         *  全部：1 | 2 = 3"
+         */
+        String payType = "3";
+
+        /**
+         *txnStatus
+         *"交易状态：交易成功 部分退款 全额退款
+         *  交易成功 1
+         *  部分退款 2
+         *  全额退款 4
+         *  部分退款 ｜ 全额退款：2 | 4 = 6
+         *  全部：1 | 2 | 4 = 7"
+         */
+        String txnStatus = "7";
+
+        quickPayService.findOrderAsync(SessonData.loginUser, index, size, recType, payType, txnStatus, new QuickPayCallbackListener<ServerPacket>() {
+            @Override
+            public void onSuccess(ServerPacket data) {
+                final int totalRecord = data.getTotalRecord();//这个字段表示当月的总条数
+                final int count = data.getCount();//返回的条数
+                final String total = data.getTotal();
+                final int refdcount = data.getRefdcount();
+                final String refdtotal = data.getRefdtotal();
+                final int size = data.getSize();
+
+                //**********************************************************************************
+                for (Txn txn : data.getTxn()) {
+                    TradeBill tradeBill = new TradeBill();
+                    tradeBill.response = txn.getResponse();
+                    tradeBill.tandeDate = txn.getSystemDate();
+                    tradeBill.consumerAccount = txn.getConsumerAccount();
+                    tradeBill.refundAmt = TxamtUtil.getNormal(txn.getRefundAmt());
+
+                    QRequest req = txn.getmRequest();
+                    if (req != null) {
+                        tradeBill.orderNum = req.getOrderNum();
+                        tradeBill.amount = TxamtUtil.getNormal(req.getTxamt());
+                        tradeBill.busicd = req.getBusicd();
+
+                        //使用/v3/bill接口 退款的好像也没有拉取到
+                        if (tradeBill.busicd.equals("REFD")) {
+                            tradeBill.amount = "-" + tradeBill.amount;
+                        }
+                        tradeBill.chcd = req.getChcd();
+                        tradeBill.tradeFrom = req.getTradeFrom();
+                        tradeBill.goodsInfo = req.getGoodsInfo();
+                    }
+
+                    //获取这个账单里面的日期,年月日 的 日
+                    String currentDay = tradeBill.tandeDate.substring(6, 8);
+                    String currentYear = tradeBill.tandeDate.substring(0, 4);
+                    String currentMonth = tradeBill.tandeDate.substring(4, 6);
+                    String currentYearMonth = tradeBill.tandeDate.substring(0, 6);
+
+                    //渠道为空的 不列入统计，这样totalRecord和实际的list的size可能不一样
+                    if (TextUtils.isEmpty(tradeBill.chcd)) {
+                        continue;
+                    }
+
+                    //添加到收款码账单相应的map中，最后再转换到list中，按照月份的先后排序转换到list中
+                    if (mMonthCollectionBillMap.containsKey(currentYearMonth)) {
+                        mMonthCollectionBillMap.get(currentYearMonth).setCount(count);
+                        mMonthCollectionBillMap.get(currentYearMonth).setTotal(total);
+                        mMonthCollectionBillMap.get(currentYearMonth).setRefdcount(refdcount);
+                        mMonthCollectionBillMap.get(currentYearMonth).setRefdtotal(refdtotal);
+                        mMonthCollectionBillMap.get(currentYearMonth).setSize(size);
+                        mMonthCollectionBillMap.get(currentYearMonth).setTotalRecord(totalRecord);
+                    } else {
+                        MonthBill monthBill = new MonthBill(currentYear, currentMonth);
+                        monthBill.setCount(count);
+                        monthBill.setTotal(total);
+                        monthBill.setRefdcount(refdcount);
+                        monthBill.setRefdtotal(refdtotal);
+                        monthBill.setSize(size);
+                        monthBill.setTotalRecord(totalRecord);
+                        mMonthCollectionBillMap.put(currentYearMonth, monthBill);
+                    }
+
+                    //收款码账单对应的map
+                    if (mCollectionBillMap.containsKey(currentYearMonth)) {
+                        mCollectionBillMap.get(currentYearMonth).add(tradeBill);
+                    } else {
+                        List<TradeBill> list = new ArrayList<TradeBill>();
+                        list.add(tradeBill);
+                        mCollectionBillMap.put(currentYearMonth, list);
+                    }
+                }//end for()
+                //**********************************************************************************
+
+                //这里通过这个两个方法，把map类型转换为list类型
+                mapToMonthBillList(mMonthCollectionBillMap, mMonthCollectionBillList);
+                mapToBillList(mCollectionBillMap, mCollectionBillList);
+
+                mCollectionPullRefreshListView.onRefreshComplete();
+
+                collectionIndex += size;
+            }
+
+            @Override
+            public void onFailure(QuickPayException ex) {
+
+                mCollectionPullRefreshListView.onRefreshComplete();
+            }
+        });
+
 
     }
 
-    private void mapToMonthBillList(Map<String, MonthBill> map) {
+    private void mapToMonthBillList(Map<String, MonthBill> map, List<MonthBill> monthList) {
         Set<String> keyset = map.keySet();
         ArrayList<String> list = new ArrayList<>();
         list.addAll(keyset);
@@ -355,13 +500,13 @@ public class TransManageView extends LinearLayout {
 
         Collections.sort(list, com);
 
-        mMonthBilList.clear();
+        monthList.clear();
         for (String key : list) {
-            mMonthBilList.add(map.get(key));
+            monthList.add(map.get(key));
         }
     }
 
-    private void mapToTradeBillList(Map<String, List<TradeBill>> map) {
+    private void mapToBillList(Map<String, List<TradeBill>> map, List<List<TradeBill>> monthList) {
         Set<String> keyset = map.keySet();
         ArrayList<String> list = new ArrayList<>();
         list.addAll(keyset);
@@ -380,9 +525,9 @@ public class TransManageView extends LinearLayout {
         };
         Collections.sort(list, com);//把 key 排序一下
 
-        mTradeBillList.clear();
+        monthList.clear();
         for (String key : list) {
-            mTradeBillList.add(map.get(key));
+            monthList.add(map.get(key));
         }
     }
 }
