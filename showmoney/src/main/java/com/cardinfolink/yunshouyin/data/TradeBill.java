86c5f5559f5db6a27961d0b93dfa62a282648a6f
Deng Jia <charles.deng@cardinfolink.com>
2016-01-03 10:11:32 +0800

    showmoney: add some member for pay result in TradeBill.java
    添加了几个成员变量
    在显示支付结果的时候用到的，在startActivity的时候bundle里面直接
    put一个TradeBill的对象即可。
        public String errorDetail;//错误信息
        public String total;//交易金额
        public String originalTotal;//消费金额
     在startActivity的时候这样设置一下intent：
             bun.putSerializable("TradeBill", tradeBill);
             intent.putExtra("BillBundle",bun);
    
    Change-Id: Ie028579dbd8da85bcffdce398fe0f81c935f69c7
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/data/TradeBill.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/data/TradeBill.java
@@ -5,16 +5,19 @@ import java.io.Serializable;
 public class TradeBill implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    public String chcd;
-    public String tradeFrom;
+    public String chcd;//交易渠道
+    public String tradeFrom;//交易来自哪里
     public String busicd;
-    public String orderNum;
-    public String tandeDate;
-    public String response;
+    public String orderNum;//订单号
+    public String tandeDate;//订单日期
+    public String response;//订单状态
     public String amount;
     public String consumerAccount;
     public String goodsInfo;
-
     public String refundAmt;
-    public String transStatus;
+    public String transStatus;//这个是新增的一个表示订单状态的字段
+
+    public String errorDetail;//错误信息
+    public String total;//交易金额
+    public String originalTotal;//消费金额
 }
