de0ca865a943098828098fe923011c19834c7b97
Deng Jia <charles.deng@cardinfolink.com>
2016-01-04 09:10:45 +0800

    showmoney:add coupon instance into sessiondata
    Change-Id: I3954e150eb582e7c4e2a7116b3619bd067554637
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/data/SessonData.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/data/SessonData.java
@@ -2,4 +2,7 @@ package com.cardinfolink.yunshouyin.data;
 
 public class SessonData {
     public static User loginUser = new User();
+
+    //建一个静态对象存放卡券优惠相关信息
+    public static Coupon coupon = new Coupon();
 }
