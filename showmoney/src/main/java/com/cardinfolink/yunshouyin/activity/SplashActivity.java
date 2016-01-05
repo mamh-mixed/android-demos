79b4a1965a86bc3ae17df98c1a27c75325f7953e
Deng Jia <charles.deng@cardinfolink.com>
2016-01-05 17:27:41 +0800

    showmoney:add welcome view logic in splashactivity
    Change-Id: I90b5283f09241bf7b0ce0d57448c9bb3312dbc94

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/SplashActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/SplashActivity.java
@@ -1,6 +1,8 @@
 package com.cardinfolink.yunshouyin.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -18,6 +20,9 @@ public class SplashActivity extends BaseActivity {
         setContentView(R.layout.activity_splash);
         MobclickAgent.updateOnlineConfig(mContext);
 
+        SharedPreferences sp = getSharedPreferences("savedata", Context.MODE_PRIVATE);
+        Boolean isFirst = sp.getBoolean("is_user_guide_show", true);
+
         //默认是登录，除非从其他地方传人了original的值
         String originalFromFlag = "login";
         try {
@@ -29,16 +34,25 @@ public class SplashActivity extends BaseActivity {
         }
 
         if ("login".equals(originalFromFlag)) {
-            new Handler().postDelayed(new Runnable() {
-
-                @Override
-                public void run() {
-                    Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
-                    startActivity(mainIntent);
-                    finish();
-                }
-
-            }, SPLASH_DISPLAY_LENGHT);
+            if (isFirst) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mainIntent = new Intent(SplashActivity.this, GuideActivity.class);
+                        startActivity(mainIntent);
+                        finish();
+                    }
+                }, SPLASH_DISPLAY_LENGHT);
+            } else {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                        startActivity(mainIntent);
+                        finish();
+                    }
+                }, SPLASH_DISPLAY_LENGHT);
+            }
         } else {
             new Handler().postDelayed(new Runnable() {
 
