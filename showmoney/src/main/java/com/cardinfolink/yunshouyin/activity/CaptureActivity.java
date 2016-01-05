360455ac304882b3e0d2929d9c60387921df603c
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-05 10:22:15 +0800

    showmoney: change currency type to TWD in taiwan-2.0
    更改了币种类型
    
    Change-Id: Ibc2ccf7fb0d22472d9e5f76150f97e95f8b3083b
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/CaptureActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/CaptureActivity.java
@@ -142,7 +142,7 @@ public class CaptureActivity extends BaseActivity implements Callback {
                         final OrderData orderData = new OrderData();
                         orderData.orderNum = mOrderNum;
                         orderData.txamt = total;
-                        orderData.currency = "156";
+                        orderData.currency = CashierSdk.SDK_CURRENCY_TWD;
                         orderData.chcd = chcd;
                         orderData.scanCodeId = (String) msg.obj;
                         // /orderData.scanCodeId="13241252555";
