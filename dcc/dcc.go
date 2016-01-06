9ef879bc4d8b82fb048fc3a070a3f28e31f4c26c
Rui <379630413@qq.com>
2016-01-06 15:42:54 +0800

    汇率理解相反

--- dcc/dcc.go
+++ dcc/dcc.go
@@ -58,6 +58,6 @@ func (d Dcc) Do(txamt int64) (amt int64, rate float64, err error) {
 		break
 	}
 	rate = er.Rate
-	amt = int64(math.Floor(float64(txamt)*rate + 0.5))
+	amt = int64(math.Floor(float64(txamt)/rate + 0.5))
 	return amt, rate, nil
 }
