a10dec19d44c9973b9baedcd4a4559ec5eb90a7d
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-06 10:46:20 +0800

    showmoney: add some coupon json key name in Txn.java
    添加了 卡券相关的
    
    Change-Id: Ie4424e7ba272ce12ff32ababeacb48ada0c8fe4d
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/model/Txn.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/model/Txn.java
@@ -2,19 +2,76 @@ package com.cardinfolink.yunshouyin.model;
 
 import com.google.gson.annotations.SerializedName;
 
+/**
+ * {
+ * response='09',
+ * systemDate='20160105154422',
+ * transStatus='40',
+ * refundAmt='0',
+ * nickName='null',
+ * avatarUrl='null',
+ * checkCode='null',
+ * couponName='null',
+ * couponChannel='WXP',
+ * couponOrderNo='null',
+ * couponDiscountAmt='null',
+ * consumerAccount='null',
+ * mRequest=com.cardinfolink.yunshouyin.model.QRequest@42dfbda0
+ * }
+ */
 public class Txn {
     private String response;
 
     @SerializedName("system_date")
     private String systemDate;
 
-    private String consumerAccount;
-    @SerializedName("m_request")
-    private QRequest mRequest;
+    private String transStatus;
 
     private String refundAmt;
 
-    private String transStatus;
+    /**
+     * 微信昵称，如果是使用微信扫描收款码支付需要有
+     */
+    private String nickName;
+
+    /**
+     * 微信头像，如果是使用微信扫描收款码支付需要有
+     */
+    private String avatarUrl;
+
+    /**
+     * 校验码，微信扫描固定码支付有这个数据
+     */
+    private String checkCode;
+
+    /**
+     * 卡券名称
+     */
+    private String couponName;
+
+
+    /**
+     * 卡券渠道
+     */
+    private String couponChannel;
+
+    /**
+     * 卡券核销订单号
+     */
+    private String couponOrderNo;
+
+    /**
+     * 卡券优惠金额
+     */
+    private String couponDiscountAmt;
+
+    /**
+     * 这个暂时没有了
+     */
+    private String consumerAccount;
+
+    @SerializedName("m_request")
+    private QRequest mRequest;
 
 
     public String getResponse() {
@@ -65,15 +122,78 @@ public class Txn {
         this.transStatus = transStatus;
     }
 
+    public String getNickName() {
+        return nickName;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getCheckCode() {
+        return checkCode;
+    }
+
+    public void setCheckCode(String checkCode) {
+        this.checkCode = checkCode;
+    }
+
+    public String getCouponName() {
+        return couponName;
+    }
+
+    public void setCouponName(String couponName) {
+        this.couponName = couponName;
+    }
+
+    public String getCouponChannel() {
+        return couponChannel;
+    }
+
+    public void setCouponChannel(String couponChannel) {
+        this.couponChannel = couponChannel;
+    }
+
+    public String getCouponOrderNo() {
+        return couponOrderNo;
+    }
+
+    public void setCouponOrderNo(String couponOrderNo) {
+        this.couponOrderNo = couponOrderNo;
+    }
+
+    public String getCouponDiscountAmt() {
+        return couponDiscountAmt;
+    }
+
+    public void setCouponDiscountAmt(String couponDiscountAmt) {
+        this.couponDiscountAmt = couponDiscountAmt;
+    }
+
     @Override
     public String toString() {
         return "Txn{" +
                 "response='" + response + '\'' +
                 ", systemDate='" + systemDate + '\'' +
+                ", transStatus='" + transStatus + '\'' +
+                ", refundAmt='" + refundAmt + '\'' +
+                ", nickName='" + nickName + '\'' +
+                ", avatarUrl='" + avatarUrl + '\'' +
+                ", checkCode='" + checkCode + '\'' +
+                ", couponName='" + couponName + '\'' +
+                ", couponChannel='" + couponChannel + '\'' +
+                ", couponOrderNo='" + couponOrderNo + '\'' +
+                ", couponDiscountAmt='" + couponDiscountAmt + '\'' +
                 ", consumerAccount='" + consumerAccount + '\'' +
                 ", mRequest=" + mRequest +
-                ", refundAmt='" + refundAmt + '\'' +
-                ", transStatus='" + transStatus + '\'' +
                 '}';
     }
 }
