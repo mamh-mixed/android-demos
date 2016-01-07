b938e808526a3d5b73d948fd3317723f489af888
Deng Jia <charles.deng@cardinfolink.com>
2016-01-07 10:25:20 +0800

    showmoney:add avoid CA certification in PostEngine.java
    绕过了CA验证
    
    Change-Id: I4a53f821de29a19ee4b30a415761222c3b442f67
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/api/PostEngine.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/api/PostEngine.java
@@ -9,7 +9,17 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.Proxy;
 import java.net.URL;
+
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Map;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class PostEngine {
     private Proxy httpProxy;
@@ -47,26 +57,31 @@ public class PostEngine {
 
         URL url = new URL(_url);
         HttpURLConnection conn = null;
-        if (httpProxy != null) {
-            conn = (HttpURLConnection) url.openConnection(httpProxy);
+        trustAllHosts();
+
+        HttpsURLConnection https = (HttpsURLConnection)url.openConnection();
+        if (url.getProtocol().toLowerCase().equals("https")) {
+            https.setHostnameVerifier(DO_NOT_VERIFY);
+            conn = https;
         } else {
-            conn = (HttpURLConnection) url.openConnection();
+            conn = (HttpURLConnection)url.openConnection();
         }
+
         try {
             conn.setDoOutput(true);
             conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
             conn.setRequestProperty("charset", "utf-8");
             conn.setRequestProperty("Content-Length", Integer.toString(postDataLength));
             conn.setRequestMethod("POST");
-
             DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
             wr.write(postData);
+
             if (conn.getResponseCode() == 200) {
                 InputStream in = new BufferedInputStream(conn.getInputStream());
                 String response = IOUtils.toString(in, "UTF-8");
                 return response;
             } else {
-                return null;
+                throw new IOException();
             }
 
         } finally {
@@ -74,6 +89,45 @@ public class PostEngine {
         }
     }
 
+    /**
+     *
+     */
+    final static HostnameVerifier DO_NOT_VERIFY = new HostnameVerifier() {
+
+        public boolean verify(String hostname, SSLSession session) {
+            return true;
+        }
+    };
+    /**
+     * Trust every server - dont check for any certificate
+     */
+    private static void trustAllHosts() {
+        final String TAG = "trustAllHosts";
+        // Create a trust manager that does not validate certificate chains
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return new java.security.cert.X509Certificate[]{};
+            }
+
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+            }
+
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+            }
+        }};
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("TLS");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public String get(String _url, Map<String, String> params) throws IOException {
         StringBuilder sb = new StringBuilder();
