0fe2119480cceca37fe75e89cb8c72fd00bbea4b
Deng Jia <charles.deng@cardinfolink.com>
2016-01-07 10:25:20 +0800

    showmoney:delete web url in ShowMoneyApp.java
    删除了 web bill url
    
    Change-Id: I7c3320780b13a57d1dcca799da97015e73f25db5
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ShowMoneyApp.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ShowMoneyApp.java
@@ -79,22 +79,18 @@ public class ShowMoneyApp extends Application {
         //default is pro
         SystemConfig.IS_PRODUCE = true;
         SystemConfig.Server = "https://api.shou.money/app";
-        SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/payment/trade.html";
         switch (ENVIRONMENT) {
             case "pro":
                 SystemConfig.IS_PRODUCE = true;
                 SystemConfig.Server = "https://api.shou.money/app";
-                SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/payment/trade.html";
                 break;
             case "test":
                 SystemConfig.IS_PRODUCE = false;
                 SystemConfig.Server = "http://test.quick.ipay.so/app";
-                SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/agent/trade.html";
                 break;
             case "dev":
                 SystemConfig.IS_PRODUCE = false;
                 SystemConfig.Server = "http://dev.quick.ipay.so/app";
-                SystemConfig.WEB_BILL_URL = "http://qrcode.cardinfolink.net/agent/trade.html";
                 break;
             default:
                 break;
