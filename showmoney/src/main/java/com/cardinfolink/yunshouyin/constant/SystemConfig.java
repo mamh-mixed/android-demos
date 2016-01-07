4b8d00515cbeb26bc05529c5163a4533932f4634
Deng Jia <charles.deng@cardinfolink.com>
2016-01-07 10:25:20 +0800

    showmoney: delete web bill url and app key
    删除了两个不用的 变量
    这个patch是在3.0分支上push错了，这里重用了一下
    
    Change-Id: I4a53f821de29a19ee4b30a415761222c3b442f67
    Signed-off-by: Deng Jia <charles.deng@cardinfolink.com>
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/SystemConfig.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/SystemConfig.java
@@ -4,15 +4,8 @@ import com.cardinfolink.yunshouyin.R;
 import com.cardinfolink.yunshouyin.util.ShowMoneyApp;
 
 public class SystemConfig {
-
-
-    public static String APP_KEY = ShowMoneyApp.getInstance().getResources().getString(R.string.app_key);//app用户系统交互key
-
     //用户系统服务器地址
     public static String Server;
-
-    //扫固定码支付网页订单支付
-    public static String WEB_BILL_URL;
 
     // SDK 环境
     public static boolean IS_PRODUCE;
@@ -21,5 +14,4 @@ public class SystemConfig {
 
     public static final String BANKBASE_KEY = "20e786206dcf4aae8a63fe34553fd274";
     public static final String BANKBASE_URL = "http://211.144.213.120:443/bdp";
-
 }
