c782f0a67cca32532035f6a4048f4dad22465efb
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-08 10:32:03 +0800

    showmoney: delete not used msg in Msg.java
    删除了一些不用的变量
    
    Change-Id: I7319b6df066e52edc3667b2ad21f188710ff020e
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/Msg.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/constant/Msg.java
@@ -1,58 +1,26 @@
 package com.cardinfolink.yunshouyin.constant;
 
 public class Msg {
-
-    public static final int MSG_FROM_FLASHLIGHT_CLOSE = 120;
-    public static final int MSG_FROM_FLASHLIGHT_OPEN = 121;
-
+    public static final int MSG_SCAN_CODE_VIEW_CLEAR_INPUT_OUTPUT = 120;
 
     public static final int MSG_FROM_SCANCODE_SUCCESS = 131;
-
 
     public static final int MSG_FROM_DIGLOG_SECOND = 141;
     public static final int MSG_FROM_DIGLOG_CLOSE = 142;
 
-    public static final int MSG_FROM_ACTIVATE_DIGLOG_CANCEL = 143;
-    public static final int MSG_FROM_ACTIVATE_DIGLOG_OK = 144;
-    public static final int MSG_FROM_SUCCESS_DIGLOG_HISTORY = 145;
-    public static final int MSG_FROM_CLIENT_LOAD_START = 146;
-    public static final int MSG_FROM_CLIENT_LOAD_END = 147;
-
     public static final int MSG_FROM_SERVER_TIMEOUT = 161;
-    public static final int MSG_FROM_SERVER_LIMIT_BEYOND = 162;
     public static final int MSG_FROM_SERVER_TRADE_SUCCESS = 163;
     public static final int MSG_FROM_SERVER_TRADE_FAIL = 164;
     public static final int MSG_FROM_SERVER_TRADE_NOPAY = 165;
-    public static final int MSG_FROM_TOAST = 166;
-    public static final int MSG_FROM_SERVER_UPDATEPASSWORD_SUCCESS = 167;
-    public static final int MSG_FROM_SERVER_LIMITINCREASE_SUCCESS = 168;
-    public static final int MSG_FROM_SERVER_QYREFD_SUCCESS = 169;
-    public static final int MSG_FROM_SERVER_REFD_SUCCESS = 170;
-    public static final int MSG_FROM_SERVER_REFD_FAIL = 171;
-
-
-    public static final int MSG_FROM_CLIENT_JUMP_LOGIN = 181;
-
-    public static final int MSG_FROM_CLIENT_JUMP_NEXT_REGISTER = 182;
-    public static final int MSG_FROM_CLIENT_JUMP_SCANCODE = 183;
-
-
-    public static final int MSG_FROM_SERVER_ERROR = 271;
-    public static final int MSG_FROM_CLIENT_REGISTER_SUCCESS = 272;
-
-    public static final int MSG_FROM_CLIENT_REMIND_UPDATE = 371;
-
-    public static final int MSG_FROM_CLIENT_ALERT_OK = 471;
 
     //关单用到的
     public static final int MSG_FROM_SERVER_CLOSEBILL_SUCCESS = 500;
     public static final int MSG_FROM_SERVER_CLOSEBILL_DOING = 501;
     public static final int MSG_FROM_SERVER_CLOSEBILL_FAIL = 502;
-    public static final int MSG_FROM_SEARCHING_POLLING=503;
+    public static final int MSG_FROM_SEARCHING_POLLING = 503;
 
     public static final int MSG_CREATE_QR_SUCCESS = 600;
     public static final int MSG_CREATE_QR_FAIL = 601;
-
 
     //卡券用到的
     public static final int MSG_FROM_SERVER_COUPON_SUCCESS = 602;
