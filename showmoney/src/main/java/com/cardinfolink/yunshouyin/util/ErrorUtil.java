60657397b21641da81fd37aa6bb776b93c62b168
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-09 14:15:34 +0800

    showmoney: imporve error code to error message in ErrorUtil.java
    优化 了 错误码 和 错误文本信息 直接的转换
    避免了null的异常
    
    Change-Id: I5e787720daf745aa8c78c020f584a79ee540aeaa
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ErrorUtil.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/util/ErrorUtil.java
@@ -5,39 +5,39 @@ import com.cardinfolink.yunshouyin.R;
 public class ErrorUtil {
 
     public static String getErrorString(String error) {
-        if (error.equals("user_no_activate")) {
+        if ("user_no_activate".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_user_no_activate);
         }
 
-        if (error.equals("username_password_error")) {
+        if ("username_password_error".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_username_password_error);
         }
 
-        if (error.equals("sign_fail")) {
+        if ("sign_fail".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_sign_fail);
         }
 
-        if (error.equals("username_no_exist")) {
+        if ("username_no_exist".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_username_no_exist);
         }
 
-        if (error.equals("username_exist")) {
+        if ("username_exist".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_username_exist);
         }
 
-        if (error.equals("system_error")) {
+        if ("system_error".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_system_error);
         }
-        if (error.equals("old_password_error")) {
+        if ("old_password_error".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_old_password_error);
         }
-        if (error.equals("user_has_three_times")) {
+        if ("user_has_three_times".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_user_has_three_times);
         }
-        if (error.equals("user_has_two_times")) {
+        if ("user_has_two_times".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_user_has_two_times);
         }
-        if (error.equals("user_has_one_times")) {
+        if ("user_has_one_times".equals(error)) {
             return ShowMoneyApp.getResString(R.string.alert_error_user_has_one_times);
         }
         return error;
