535ed9a8f49787c2cdb0aa21af5f55ae04a67fa1
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-11 12:59:14 +0800

    showmoney: add forget password support in ForgetPasswordActivity.java
    添加了 忘记密码的 功能
    输入邮箱 按钮 确认 弹出对话框，
    
    Change-Id: I2f991cfc9b64572d99c0eb3a56b58c86882b97f9
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ForgetPasswordActivity.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/activity/ForgetPasswordActivity.java
@@ -1,7 +1,10 @@
 package com.cardinfolink.yunshouyin.activity;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,8 @@ import com.cardinfolink.yunshouyin.core.QuickPayCallbackListener;
 import com.cardinfolink.yunshouyin.model.ServerPacket;
 import com.cardinfolink.yunshouyin.ui.SettingActionBarItem;
 import com.cardinfolink.yunshouyin.ui.SettingInputItem;
+import com.cardinfolink.yunshouyin.util.ShowMoneyApp;
+import com.cardinfolink.yunshouyin.util.VerifyUtil;
 import com.cardinfolink.yunshouyin.view.HintDialog;
 
 /**
@@ -67,18 +72,53 @@ public class ForgetPasswordActivity extends BaseActivity {
      * 点击 按钮 调用这里， 提交忘记密码的请求
      */
     private void forgetPassowd() {
-        Log.e(TAG, "     * 点击 按钮 调用这里， 提交忘记密码的请求 ");
-        quickPayService.forgetPasswordAsync(mEmail.getText(), new QuickPayCallbackListener<ServerPacket>() {
+        final String emailStr = mEmail.getText();
+        if (!validate(emailStr)) {
+            return;
+        }
+        quickPayService.forgetPasswordAsync(emailStr, new QuickPayCallbackListener<ServerPacket>() {
             @Override
             public void onSuccess(ServerPacket data) {
-
+                //重置密码将发送到此邮箱
+                String title = getResources().getString(R.string.forget_account_message1);
+                title += "\n" + emailStr;
+                String ok = getResources().getString(R.string.forget_i_known);
+                String cancel = getResources().getString(R.string.forget_cancel);
+                mHintDialog.setOkOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mHintDialog.hide();
+                        finish();
+                    }
+                });
+                mHintDialog.show(title, ok, cancel);
             }
 
             @Override
             public void onFailure(QuickPayException ex) {
+                String title = ex.getErrorMsg();
+                String ok = getResources().getString(R.string.forget_i_known);
+                String cancel = getResources().getString(R.string.forget_cancel);
+                mHintDialog.show(title, ok, cancel);
 
             }
         });
     }
 
+    private boolean validate(String email) {
+        String alertMsg = "";
+        Bitmap alertBitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.wrong);
+        if (TextUtils.isEmpty(email)) {
+            alertMsg = ShowMoneyApp.getResString(R.string.alert_error_email_cannot_empty);
+            mAlertDialog.show(alertMsg, alertBitmap);
+            return false;
+        }
+        if (!VerifyUtil.checkEmail(email)) {
+            alertMsg = ShowMoneyApp.getResString(R.string.alert_error_email_format_error);
+            mAlertDialog.show(alertMsg, alertBitmap);
+            return false;
+        }
+
+        return true;
+    }
 }
