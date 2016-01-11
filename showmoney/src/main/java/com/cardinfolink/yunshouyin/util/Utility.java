e8088e1723671e2f256bc10757d3038365690814
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-11 13:54:05 +0800

    showmoney: add new method cretaeBitmap() in Utility.java
    添加了一个新的工具方法，用来创建二维码图片 bitmap
    
    Change-Id: I5b5d56c74ba7ba6b0ef2f7b7a7957f6aa0953551
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/util/Utility.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/util/Utility.java
@@ -3,8 +3,16 @@ package com.cardinfolink.yunshouyin.util;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Hashtable;
 import java.util.Random;
 
 
@@ -12,6 +20,11 @@ import java.util.Random;
  * 工具类 方法，这里面存放一些通用的工具类方法
  */
 public class Utility {
+    private static final int IMAGE_HALFWIDTH = 40;
+    private static final int FOREGROUND_COLOR = 0xff000000;
+    private static final int BACKGROUND_COLOR = 0xffffffff;
+
+
     public static Bitmap zoomBitmap(Bitmap icon, int h) {
         Matrix m = new Matrix();
         float sx = (float) 2 * h / icon.getWidth();
@@ -38,4 +51,50 @@ public class Utility {
         return mOrderNum;
     }
 
+    /**
+     * //生成bitmap 二维码图片,生成一个固定长宽都是width和height的二维码图片
+     *
+     * @param str
+     * @param icon
+     * @param widthx
+     * @param heighty
+     * @return
+     * @throws WriterException
+     */
+    public static Bitmap cretaeBitmap(String str, Bitmap icon, int widthx, int heighty) throws WriterException {
+        icon = zoomBitmap(icon, IMAGE_HALFWIDTH);
+        Hashtable<EncodeHintType, Object> hints = new Hashtable<EncodeHintType, Object>();
+        hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+        hints.put(EncodeHintType.CHARACTER_SET, "utf-8");
+        hints.put(EncodeHintType.MARGIN, 1);
+        //调用com.google.zxing里面的生成二维码的方法
+        BitMatrix matrix = new MultiFormatWriter().encode(str, BarcodeFormat.QR_CODE, widthx, heighty, hints);
+
+        int width = matrix.getWidth();
+        int height = matrix.getHeight();
+
+        int halfW = width / 2;
+        int halfH = height / 2;
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (x > halfW - IMAGE_HALFWIDTH && x < halfW + IMAGE_HALFWIDTH
+                        && y > halfH - IMAGE_HALFWIDTH
+                        && y < halfH + IMAGE_HALFWIDTH) {
+                    pixels[y * width + x] = icon.getPixel(x - halfW + IMAGE_HALFWIDTH, y - halfH + IMAGE_HALFWIDTH);
+                } else {
+                    if (matrix.get(x, y)) {
+                        pixels[y * width + x] = FOREGROUND_COLOR;
+                    } else {
+                        pixels[y * width + x] = BACKGROUND_COLOR;
+                    }
+                }
+
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
+
+        return bitmap;
+    }
 }
