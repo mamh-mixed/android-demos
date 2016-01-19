6e2bc687a62ccbfc43f866b1a8c20794821f23ab
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-19 09:54:46 +0800

    showmoney: modify check mobile number Pattern regex
    修改了 检查 手机号 的正则表达式
    为什么 去掉了 17 开头的？？？？
    
    Change-Id: Ie46170097fa1c4d81e9502af0a0d0bf3490055dc
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/util/VerifyUtil.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/util/VerifyUtil.java
@@ -198,7 +198,7 @@ public class VerifyUtil {
      * 校验手机号
      */
     public static boolean isMobileNO(String mobiles) {
-        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0-9]))\\d{8}$");
+        Pattern p = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0-9])|(17[0-9]))\\d{8}$");
         Matcher m = p.matcher(mobiles);
         return m.matches();
     }
