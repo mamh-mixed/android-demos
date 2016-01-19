d52da2d4be89f4c0661140a8d1a6165c206cc1cb
Arthur.WANG <fnghwsj@qq.com>
2016-01-19 19:44:47 +0800

    fix(微信公众号支付):回调地址置空以及失败页面弹出信息

--- jsPay/payment/src/js/payresult.js
+++ jsPay/payment/src/js/payresult.js
@@ -8,5 +8,5 @@ let state = Util.getUrlParam('state');
 if (state === '0') {
 	window.location.replace('result.html?code=' + Util.getUrlParam('code') + '&orderAmount=' + Util.getNormalTxamt(Util.getUrlParam('txamt')));
 } else {
-	window.location.replace('fail.html');
+	window.location.replace('fail.html?errorDetail=' + Util.getUrlParam('errorDetail'));
 }
