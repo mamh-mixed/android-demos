9c9f6ca6ce14c0ff3c0a725808d293b8869d6b83
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-21 14:44:08 +0800

    cashierSdk:delete key word public in interface CashierListener.java
    删除 关键字 public
    在接口中 默认就是    public的 不必要加这个
    
    Change-Id: If8add2d603e972f7cdc0a13f311a7dad876f4df9
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- cashierSdk/src/main/java/com/cardinfolink/cashiersdk/listener/CashierListener.java
+++ cashierSdk/src/main/java/com/cardinfolink/cashiersdk/listener/CashierListener.java
@@ -3,7 +3,7 @@ package com.cardinfolink.cashiersdk.listener;
 import com.cardinfolink.cashiersdk.model.ResultData;
 
 public interface CashierListener {
-    public void onResult(ResultData resultData);
+    void onResult(ResultData resultData);
 
-    public void onError(int errorCode);
+    void onError(int errorCode);
 }
