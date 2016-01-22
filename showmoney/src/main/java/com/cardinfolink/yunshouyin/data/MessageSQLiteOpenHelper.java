f183bb2163051090bf94cec118635a9aa635c587
Minghui Ma <mamh.ma@cardinfolink.com>
2016-01-22 09:44:38 +0800

    showmoney: use static fianl constant in MessageSQLiteOpenHelper.java
    把 数据库 名称 版本号 建表语句 提取出来
    作为一个constant变量了
    
    Change-Id: Ib72a12b60c6b8824a3be56f1a00cc9cda24c2f3b
    Signed-off-by: Minghui Ma <mamh.ma@cardinfolink.com>

--- showmoney/src/main/java/com/cardinfolink/yunshouyin/data/MessageSQLiteOpenHelper.java
+++ showmoney/src/main/java/com/cardinfolink/yunshouyin/data/MessageSQLiteOpenHelper.java
@@ -5,32 +5,35 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
-import com.cardinfolink.yunshouyin.util.Log;
-
 public class MessageSQLiteOpenHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "message.db";
+    private static final int DB_VERSION = 1;
 
-    public MessageSQLiteOpenHelper(Context context, String name, CursorFactory factory, int version) {
-        super(context, name, factory, version);
-    }
-
-    public MessageSQLiteOpenHelper(Context context) {
-        this(context, "message.db", null, 1);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table message (" +
+    private static final String CREATE_MESSAGE_TABLE = "" +
+            "create table message (" +
                 "msgId VARCHAR(50)," +
                 "username VARCHAR(50)," +
                 "title VARCHAR(50)," +
                 "message VARCHAR(50)," +
                 "pushtime VARCHAR(50)," +
                 "updateTime VARCHAR(50)," +
-                "status VARCHAR(1))");
+                "status VARCHAR(1)" +
+            ")";
+
+    public MessageSQLiteOpenHelper(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public MessageSQLiteOpenHelper(Context context) {
+        this(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_MESSAGE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        Log.d("MessageSQLiteOpenHelper", "数据库版本已经更新，新版本是：" + newVersion);
     }
 }
